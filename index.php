986bfd58fa925fcf3f8c9e68cb79aa0ce120604c
Antoni Sanoguera <asanoguera@gmail.com>
2019-05-05 21:33:25 -0300

    Eliminacion de recursos externos

--- index.php
+++ index.php
@@ -1,10 +1,77 @@
-<html>
-	<head>
-		<title>Ieasa - Observatorio Ambiental</title>
-	</head>
+<!DOCTYPE html>
+<html lang="en">
 
-	<body>
-	
-	</body>
+<head>
+    <title>Ieasa - Observatorio Ambiental</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="stylesheet" href="./fontawesome-5.8.1/css/all.min.css" />
+    <link rel="stylesheet" href="./css/bootstrap.min.css" />
+    <link rel="stylesheet" href="./css/bootstrapfix.navbar.css" />
+    <link rel="stylesheet" href="./css/site.css" />
+
+    <script src="./js/jquery-3.2.1.min.js"></script>
+    <script src="./js/bootstrap.bundle.min.js"></script>
+    <script src="./js/popper-1.12.9.min.js"></script>
+
+    <script src="./js/openlayers/ol.js" type="text/javascript"></script>
+
+    <script src="./js/map.js" type="text/javascript"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+        });
+    </script>
+</head>
+
+<body>
+    <nav class="navbar navbar-dark bg-light" id="nav-1">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon">
+                <i class="fa fa-navicon"></i>
+            </span>
+        </button>
+
+        <a class="navbar-brand" href="#">
+            Observatorio
+            <img src="./images/ieasa_logo.png" id="logo_ieasa" height="30">
+        </a>
+
+        <div class="nav navbar-expand navbar-right" style="display: inline-block;" id="navIcons">
+            <ul class="navbar-nav">
+                <li class="nav-item nav-item-button active">
+                    <a class="nav-link nav-link-button" href="#">
+                        <i class="fa fa-search"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-item-button nav-link-button" href="#">
+                        <i class="fa fa-user"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+
+
+</body>
 </html>
