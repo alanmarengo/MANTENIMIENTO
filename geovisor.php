e53fbec224762a5e906b7cbeb7a2a07acfb434bf
Juan Pablo Rosset <rosset.juanpablo@gmail.com>
2019-05-07 20:17:08 -0300

    Popup maquetado contenido

--- geovisor.php
+++ geovisor.php
@@ -8,7 +8,7 @@
 	
 	<link href="https://fonts.googleapis.com/css?family=Raleway:400,500,600,700" rel="stylesheet">
 	
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"/>	
+	<link rel="stylesheet" href="./css/bootstrap.css"/>	
 
     <link rel="stylesheet" href="./fontawesome-5.8.1/css/all.min.css" />
 	
@@ -148,8 +148,9 @@
 				
 	</style>	
 
-	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="./js/jquery-3.2.1.min.js"></script>
+	<script src="./js/bootstrap.js"></script>
+	
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 		
 	<script src="./js/openlayers/ol.js" type="text/javascript"></script>
@@ -572,8 +573,17 @@
 					
 					<div class="popup-panel-content">
 					
-						COntenido
+						<img class="image" src="./images/popup.map.png" width="100%">
 					
+						<p class="title">Detallas de la Geografía del Lugar</p>
+						<p class="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						
+						<a href="#" class="popup-text-active">Link</a>
+						
+						<p class="mt-10">
+							<a href="#" class="button">AGREGAR AL MAPA</a>
+						</p>
+						
 					</div>
 					
 				</div>
