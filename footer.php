997868ebc01c17219a2e461deb5e2e6e63d34d83
Antoni Sanoguera <asanoguera@gmail.com>
2019-05-17 23:15:29 -0300

    Separacion de header y footer

---
+++ footer.php
@@ -0,0 +1,29 @@
+    <div class="row footer">
+        <div class="col-md-3" style="padding: 10px;">
+            <img src="./images/logo-footer.jpg" style="width: 100%; max-width: 250px; height: auto;" />
+        </div>
+        <div class="col-md-3" style="font-size: .9em; padding: 10px;">
+            <b>CONTACTO</b>
+            <br />
+            <br />
+            <b>TELEFONO</b><br />
+            (+54 11) 5276-4050 INT: 127<br />
+            <br />
+            <b>DIRECCION</b><br />
+            AV. PASEO COLON 505 PISO 6,<br />
+            CIUDAD DE BUENOS AIRES, ARGENTINA
+        </div>
+        <div class="col-md-3" style="font-size: .9em; padding: 10px;">
+            <b>ENLACES</b>
+            <br />
+            <br />
+        </div>
+        <div class="col-md-3 social-media" style="padding: 10px;">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-youtube"></a>
+            <a href="#" class="fab fa-twitter"></a>
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Ieasa - Observatorio Ambiental</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" type="text/css" href="./fontawesome-5.8.1/css/all.min.css" />
+    <link rel="stylesheet" type="text/css" href="./fonts/gotham/gotham.css"/>
+    <link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="./css/bootstrapfix.navbar.css" />
+    <link rel="stylesheet" type="text/css" href="./css/site.css" />
+
+    <script src="./js/jquery-3.2.1.min.js"></script>
+    <script src="./js/bootstrap.bundle.min.js"></script>
+    <script src="./js/popper-1.12.9.min.js"></script>
+    <script src="./js/openlayers/ol.js" type="text/javascript"></script>
+    <script src="./js/map.js" type="text/javascript"></script>
+</head>
+
+<body>
+    <nav class="navbar navbar-dark bg-light" id="nav-1">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon">
+                <i class="fa fa-navicon"></i>
+            </span>
+        </button>
+
+        <a class="navbar-brand" href="#">
+            Observatorio
+            <img src="./images/ieasa_logo.png" id="logo_ieasa" height="30">
+        </a>
+
+        <div class="nav navbar-expand navbar-right" style="display: inline-block;" id="navIcons">
+            <ul class="navbar-nav">
+                <li class="nav-item nav-item-search">
+                    <a class="nav-link nav-item-button nav-link-button">
+                        <i class="fa fa-search"></i>
+                        <input type="text" placeholder="Buscar en todo el sitio">
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-item-button nav-link-button" href="#">
+                        <i class="fa fa-question-circle"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-item-button nav-link-button" href="#">
+                        <i class="fa fa-user"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
