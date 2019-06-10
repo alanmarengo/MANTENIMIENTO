89421e95b5e50db95860039a1789835dce2cec8f
Juan Pablo <rosset.juanpablo@gmail.com>
2019-06-10 11:40:18 -0300

    Agrupamiento Popup

--- js/map.js
+++ js/map.js
@@ -200,7 +200,7 @@ function ol_map() {
 				var gid = $(v).attr("x");
 				var layer_name = $(v).attr("y");
 				
-				if (!results[layer_name]) { results[layer_name] = {}; result[layer_name]["points"] = []; }
+				if (!results[layer_name]) { results[layer_name] = {}; results[layer_name]["points"] = []; }
 				
 				layers.push(layer_name);
 				results[layer_name]["points"].push(gid);
