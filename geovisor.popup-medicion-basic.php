6d6d1c33626e3d4d2cc418f8ad54fb1db9fea158
Juan Pablo Rosset <rosset.juanpablo@gmail.com>
2019-06-10 02:44:39 -0300

    popup info

--- geovisor.popup-medicion-basic.php
+++ geovisor.popup-medicion-basic.php
@@ -2,7 +2,7 @@
 				
 	<div class="popup-col col col-sm-12 col-md-12 col-sm-12 col-xs-12 p-10 fs-12">
 		
-		<p class="popup-subtitle">MEDICI&OACUTE;N</p>
+		<p class="popup-subtitle">MEDICIÓN</p>
 				
 		<div id="info-medicion" class="mt-10 scrollbar-content">
 		
