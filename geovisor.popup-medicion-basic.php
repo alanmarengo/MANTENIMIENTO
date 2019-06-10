549bd36b110724d3f8c31f48157b883e105a64b2
Juan Pablo Rosset <rosset.juanpablo@gmail.com>
2019-06-10 02:46:17 -0300

    popup info

--- geovisor.popup-medicion-basic.php
+++ geovisor.popup-medicion-basic.php
@@ -2,7 +2,7 @@
 				
 	<div class="popup-col col col-sm-12 col-md-12 col-sm-12 col-xs-12 p-10 fs-12">
 		
-		<p class="popup-subtitle">MEDICIÓN</p>
+		<p class="popup-subtitle">MEDIDA DE TRAZO</p>
 				
 		<div id="info-medicion" class="mt-10 scrollbar-content">
 		
