21748f4ad996886e1e01a94a37d0bdd575a2670b
Juan Pablo <rosset.juanpablo@gmail.com>
2019-06-10 12:04:31 -0300

    Agrupamiento Popup

--- php/get-layer-info.php
+++ php/get-layer-info.php
@@ -25,81 +25,83 @@ for ($i=0; $i<sizeof($results); $i++) {
 
 $layer_names = array_unique($layer_names);
 
-var_dump($layer_names);
-var_dump($gids);
 
-/*$gid = $_POST["gid"];
-$layer_name = $_POST["layer_name"];
 
 $string_conn = "host=" . pg_server . " user=" . pg_user . " port=" . pg_portv . " password=" . pg_password . " dbname=" . pg_db;
 	
 $conn = pg_connect($string_conn);
 
-$query_string = "SELECT DISTINCT layer_schema,layer_table FROM mod_geovisores.vw_layers WHERE layer_wms_layer = '" . $layer_name . "' LIMIT 1";
+$html = "";
 
-$query = pg_query($conn,$query_string);
-
-$data = pg_fetch_assoc($query);
-
-$schema = $data["layer_schema"];
-$table= $data["layer_table"];
-
-$query_string = "SELECT * FROM \"$schema\".\"$table\" WHERE gid = $gid";
-
-$query = pg_query($conn,$query_string);
-
-$html = "<table class=\"popup-table\" id=\"gfi-info-table\" cellpadding=\"5\">";
-
-while($r = pg_fetch_assoc($query)) {
+for ($i=0; $i<sizeof($layer_names); $i++) {
 	
-	foreach ($r as $item => $value){
+	$query_string = "SELECT DISTINCT layer_schema,layer_table FROM mod_geovisores.vw_layers WHERE layer_wms_layer = '" . $layer_names[$i] . "' LIMIT 1";
+
+	$query = pg_query($conn,$query_string);
+
+	$data = pg_fetch_assoc($query);
+
+	$schema = $data["layer_schema"];
+	$table= $data["layer_table"];
+	
+	$query_string = "SELECT * FROM \"$schema\".\"$table\" WHERE gid IN (" . implode(",",$gids[$layer_names[$i]]) . ")";
+
+	$query = pg_query($conn,$query_string);
+
+	$html .= "<table class=\"popup-table\" class=\"gfi-info-table\" cellpadding=\"5\">";
+
+	while($r = pg_fetch_assoc($query)) {
 		
-		if(( strpos( $item, "geom" ) === false) && (strpos( $item, "id" ) === false)) {
-		
-			$html .= "<tr>";
-			$html .= "<td>" . $item . "</td>";
-			$html .= "<td>" . $value . "</td>";	
-			$html .= "</tr>";
+		foreach ($r as $item => $value){
+			
+			if(( strpos( $item, "geom" ) === false) && (strpos( $item, "id" ) === false)) {
+			
+				$html .= "<tr>";
+				$html .= "<td>" . $item . "</td>";
+				$html .= "<td>" . $value . "</td>";	
+				$html .= "</tr>";
+			
+			}
 		
 		}
-	
+		
 	}
 	
+	$html .= "</table>";
+
+	$html .= "<div style=\"text-align:center\" class=\"mt-20\">";
+
+	$html .= "<p>";
+	$html .= "<a ";
+	$html .= "class=\"popup-header-button popup-header-button-toggleable popup-header-button-active-fixed\" ";
+	$html .= "href=\"#\" ";
+	$html .= ">";
+	$html .= "<span>EXPORTAR TABLA</span>";
+	$html .= "</a>";
+	$html .= "</p>";
+
+	$html .= "<p>";
+	$html .= "<a ";
+	$html .= "class=\"popup-header-button popup-header-button-toggleable popup-header-button-active-fixed\" ";
+	$html .= "href=\"#\" ";
+	$html .= ">";
+	$html .= "<span>VER MODELO 3D</span>";
+	$html .= "</a>";
+	$html .= "</p>";
+
+	$html .= "<p>";
+	$html .= "<a ";
+	$html .= "class=\"popup-header-button popup-header-button-toggleable popup-header-button-active-fixed\" ";
+	$html .= "href=\"#\" ";
+	$html .= ">";
+	$html .= "<span>VER RECURSOS ASOCIADOS</span>";
+	$html .= "</a>";
+	$html .= "</p>";
+
+	$html .= "</div>";
+
 }
 
-$html .= "</table>";
-
-$html .= "<div style=\"text-align:center\" class=\"mt-20\">";
-
-$html .= "<p>";
-$html .= "<a ";
-$html .= "class=\"popup-header-button popup-header-button-toggleable popup-header-button-active-fixed\" ";
-$html .= "href=\"#\" ";
-$html .= ">";
-$html .= "<span>EXPORTAR TABLA</span>";
-$html .= "</a>";
-$html .= "</p>";
-
-$html .= "<p>";
-$html .= "<a ";
-$html .= "class=\"popup-header-button popup-header-button-toggleable popup-header-button-active-fixed\" ";
-$html .= "href=\"#\" ";
-$html .= ">";
-$html .= "<span>VER MODELO 3D</span>";
-$html .= "</a>";
-$html .= "</p>";
-
-$html .= "<p>";
-$html .= "<a ";
-$html .= "class=\"popup-header-button popup-header-button-toggleable popup-header-button-active-fixed\" ";
-$html .= "href=\"#\" ";
-$html .= ">";
-$html .= "<span>VER RECURSOS ASOCIADOS</span>";
-$html .= "</a>";
-$html .= "</p>";
-
-$html .= "</div>";
-
-echo $html;*/
+echo $html;
 
 ?>
