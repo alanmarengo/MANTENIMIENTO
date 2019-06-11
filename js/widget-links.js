4f7a6b2b689b1a0596de63c4238e62a723af5f58
Juan Pablo <rosset.juanpablo@gmail.com>
2019-06-11 15:03:13 -0300

    Widget Links

--- js/widget-links.js
+++ js/widget-links.js
@@ -4,11 +4,10 @@ window.addEventListener("load",function() {
 		function() {			
 			$(this).addClass("widget-link-hovered");
 			$(this).removeClass("widget-link-unhovered");
-		},700),		
+		},		
 		function() {			
 			$(this).addClass("widget-link-unhovered");
 			$(this).removeClass("widget-link-hovered");
-		},700)
-	);
+		})
 	
 }); 
