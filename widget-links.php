bf016df80d70a912908a876d5bd4e49b34d09352
Juan Pablo <rosset.juanpablo@gmail.com>
2019-06-11 14:31:51 -0300

    Capas Base

--- widget-links.php
+++ widget-links.php
@@ -1,21 +1,21 @@
 <div id="widget-links" class="widget-links">
-	<div class="widget-link" style="background-color:#ffa91b;">
-		<a href="#">
+	<div class="widget-link">
+		<a href="# style="background-color:#ffa91b;"">
 			
 		</a>
 	</div>
-	<div class="widget-link" style="background-color:#8e2f4f;">
-		<a href="#">
+	<div class="widget-link">
+		<a href="#" style="background-color:#8e2f4f;">
 			
 		</a>
 	</div>
-	<div class="widget-link" style="background-color:#585dc5;">
-		<a href="#">
+	<div class="widget-link">
+		<a href="#" style="background-color:#585dc5;">
 			
 		</a>
 	</div>
-	<div class="widget-link" style="background-color:#310133;">
-		<a href="#">
+	<div class="widget-link">
+		<a href="#" style="background-color:#310133;">
 			
 		</a>
 	</div>
