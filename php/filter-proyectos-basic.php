3c0c47b8f540eced0d7ee6b5d8ad39fc27789ab7
Juan Pablo <rosset.juanpablo@gmail.com>
2019-06-11 11:07:52 -0300

    Load Geovisor

--- php/filter-proyectos-basic.php
+++ php/filter-proyectos-basic.php
@@ -11,7 +11,7 @@ $conn = pg_connect($string_conn);
 
 if (isset($proyectos)>0) {
 	
-	if ( != -1) {
+	if ($geovisor != -1) {
 	
 		$get_layers_query_string = "SELECT string_agg(layer_id::text, ', ') AS layer_ids FROM mod_geovisores.layers_find('','','','".implode(",",$proyectos)."',-1,-1,-1,'',-1,-1) AND layer_id IN(SELECT layer_id FROM mod_geovisores.capa_inicial WHERE geovisor_id = " . $geovisor . ");";
 	
