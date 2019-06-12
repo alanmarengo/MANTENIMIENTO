3dfc9eda7f8d40370470823c2da884efcd38a3c8
Juan Pablo Rosset <rosset.juanpablo@gmail.com>
2019-06-12 00:56:40 -0300

    test filter

--- js/map.js
+++ js/map.js
@@ -528,7 +528,13 @@ function ol_map() {
 		
 
 		html2canvas(document.querySelector("#map")).then(canvas => {
-			document.body.appendChild(canvas)
+			
+			var a = document.createElement('a');
+			// toDataURL defaults to png, so we need to request a jpeg, then convert for file download.
+			a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
+			a.download = 'captura.jpg';
+			a.click();
+			
 		});
 
 
