b8a756969df99278ba88fcbf0ff17c2b718c6280
Juan Pablo Rosset <rosset.juanpablo@gmail.com>
2019-06-11 22:24:21 -0300

    Csv tool impprove

--- php/get-layer-info.php
+++ php/get-layer-info.php
@@ -77,8 +77,8 @@ for ($i=0; $i<sizeof($layer_names); $i++) {
 
 	$html .= "<p>";
 	$html .= "<a ";
-	$html .= "class=\"popup-header-button popup-header-button-toggleable popup-header-button-active-fixed\" onclick=\"geomap.map.exportCSV(this);\"";
-	$html .= "href=\"#\" data-q=\"".encrypt(str_replace("geom,","",$query_string2))."\"";
+	$html .= "class=\"popup-header-button popup-header-button-toggleable popup-header-button-active-fixed\"";
+	$html .= "href=\"./get-csv.php?q=\"".encrypt(str_replace("geom,","",$query_string2))."\" target=\"_blank\"";
 	$html .= ">";
 	$html .= "<span>EXPORTAR TABLA</span>";
 	$html .= "</a>";
