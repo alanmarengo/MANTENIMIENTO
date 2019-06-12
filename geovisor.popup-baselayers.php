739b5787d3269f55fd360df2e9590528a589183e
Juan Pablo <rosset.juanpablo@gmail.com>
2019-06-12 10:50:08 -0300

    Busqueda

--- geovisor.popup-baselayers.php
+++ geovisor.popup-baselayers.php
@@ -1,21 +1,29 @@
 <div class="popup popup-dark" id="popup-baselayers">
-
-	<div class="row popup-row popup-header">		
+	
+	<div class="row">
+	
+		<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+	
+			<div class="row popup-row popup-header">		
+						
+				<div class="col col-md-12 col-xs-12 popup-header">
 				
-		<div class="col col-md-12 col-xs-12 popup-header">
-		
-			<div class="nav navbar-expand popup-nav popup-nav-right">
+					<div class="nav navbar-expand popup-nav popup-nav-right">
+					
+						<ul class="navbar-nav">
+							<li class="nav-item nav-item-button popup-header-li active">
+								<a class="popup-header-button" href="#" onclick="$('#popup-baselayers').hide();">
+									<span>X</span>
+								</a>
+							</li>
+						</ul>
+						
+					</div>
+				
+				</div>
 			
-				<ul class="navbar-nav">
-					<li class="nav-item nav-item-button popup-header-li active">
-						<a class="popup-header-button" href="#" onclick="$('#popup-baselayers').hide();">
-							<span>X</span>
-						</a>
-					</li>
-				</ul>
-				
 			</div>
-		
+	
 		</div>
 	
 	</div>
