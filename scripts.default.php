5f488d84d9fdde5b3048a9e12e19e75ee2d481fe
Juan Pablo <rosset.juanpablo@gmail.com>
2019-06-13 15:52:16 -0300

    Jump Scripts

---
+++ scripts.default.php
@@ -0,0 +1,82 @@
+<!-- CSS -->
+<!-- FONTS -->
+
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="./fontawesome-5.8.1/css/all.min.css" />	
+
+<!-- BOOTSTRAP + LIBS -->
+
+<link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css" href="./css/bootstrapfix.navbar.css" />
+<link rel="stylesheet" type="text/css" href="./css/bootstrap-datepicker.min.css" />
+<link rel="stylesheet" type="text/css" href="./css/bootstrap-select.css" />
+<link rel="stylesheet" type="text/css" href="./css/site.css" />
+<link rel="stylesheet" type="text/css" href="./css/perfect-scrollbar.css" />
+<link rel="stylesheet" type="text/css" href="./css/scrollbars.css" />
+<link rel="stylesheet" type="text/css" href="./css/sidenav-index.css" />
+<link rel="stylesheet" type="text/css" href="./css/widget-links.css"/>
+
+<link rel="stylesheet" href="./js/jquery-ui/jquery-ui.min.css"/>
+
+<!-- PRETTY CHECKBOX -->
+
+<link rel="stylesheet" href="./css/pretty-checkbox.css"/>
+<link href="https://cdn.materialdesignicons.com/3.6.95/css/materialdesignicons.min.css" rel="stylesheet"> <!-- CHECKBOX FONTS -->
+
+<!-- JUMP CSS -->
+
+<link rel="stylesheet" href="./css/jump.align.css"/>
+<link rel="stylesheet" href="./css/jump.block.css"/>
+<link rel="stylesheet" href="./css/jump.button.css"/>
+<link rel="stylesheet" href="./css/jump.flotant.css"/>
+<link rel="stylesheet" href="./css/jump.hamburguer.css"/>
+<link rel="stylesheet" href="./css/jump.input.css"/>
+<link rel="stylesheet" href="./css/jump.list.css"/>
+<link rel="stylesheet" href="./css/jump.nav.css"/>
+<link rel="stylesheet" href="./css/jump.position.css"/>
+<link rel="stylesheet" href="./css/jump.scroll.css"/>
+<link rel="stylesheet" href="./css/jump.separator.css"/>
+<link rel="stylesheet" href="./css/jump.site.css"/>
+<link rel="stylesheet" href="./css/jump.window.css"/>
+
+<!-- JUMP CSS AUX -->
+
+<link rel="stylesheet" href="./css/jump.spacers.css"/>
+
+<!-- JUMP THEME -->
+
+<link rel="stylesheet" href="./css/jump.theme.default.css"/>
+
+<!-- SCRIPTS -->
+<!-- JQUERY + UI -->	
+
+<script type="text/javascript" src="./js/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="./js/jquery-ui/jquery-ui.min.js"></script>	
+
+<!-- POPPER -->	
+<script src="./js/popper-1.12.9.min.js"></script>
+
+<!-- BOOTSTRAP + LIBS -->	
+<script type="text/javascript" src="./js/bootstrap.min.js"></script>
+<script src="./js/bootstrap.bundle.min.js"></script>
+<script src="./js/bootstrap-datepicker.min.js"></script>
+<script src="./js/bootstrap-datepicker.es.min.js"></script>
+<script src="./js/bootstrap-select.js"></script>
+
+<script src="./js/perfect-scrollbar.js" type="text/javascript"></script>
+<script src="./js/scrollbars.js" type="text/javascript"></script>
+<script src="./js/moment.js"></script>
+<script src="./js/site.js" type="text/javascript"></script>
+<script src="./js/map.js" type="text/javascript"></script>
+<script src="./js/nav.js" type="text/javascript"></script>
+<script src="./js/widget-links.js" type="text/javascript"></script>
+
+<!-- JUMP JS -->
+
+<script src="./js/jump.js"></script>
+<script src="./js/jump.block.js"></script>
+<script src="./js/jump.flotant.js"></script>
+<script src="./js/jump.input.js"></script>
+<script src="./js/jump.nav.js"></script>
+<script src="./js/jump.scroll.js"></script>
+<script src="./js/jump.window.js"></script>
