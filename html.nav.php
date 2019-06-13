e9394b4338e52f7d8cda1ac75a7c5828f100c034
Juan Pablo <rosset.juanpablo@gmail.com>
2019-06-13 14:58:16 -0300

    Jump Test

---
+++ html.nav.php
@@ -0,0 +1,47 @@
+<ul>
+	<li>
+		<a href="./index.php"><span>Inicio</span></a>
+	</li>
+	<li>
+		<a href="#">Acerca de la plataforma de proyectos de IEASA</a>
+	</li>
+	<li>
+		<a href="./page.php">Observatorio Ambiental de los AHRSC</a>
+	</li>
+	<li>
+		<a href="javascript:void(0)" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onclick="flotant.toggle('#nav-geovisores',true);">
+			GeoVisores <span class="nav-item-count">(7)</span>
+		</a>
+	</li>
+	<li>
+		<a href="./mediateca.php">Mediateca</a>
+	</li>
+	<li>
+		<a href="javascript:void(0)" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onclick="flotant.toggle('#nav-vinculaciones-insterinstitucionales',true);">
+			Vinculaciones interinstitucionales <span class="nav-item-count">(5)</span>
+		</a>
+	</li>
+	<li>
+		<a href="#">Datos y Estadísticas</a>
+	</li>
+	<li>
+		<a href="#">Indicadores</a>
+	</li>
+	<li>
+		<a href="#">Territorio</a>
+	</li>
+	<li>
+		<a href="javascript:void(0)" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onclick="flotant.toggle('#nav-recursos-hidricos',true);">
+			Recursos Hídricos <span class="nav-item-count">(5)</span>
+		</a>
+	</li>
+	<li>
+		<a href="http://www.ieasa.com.ar" target="_blank">IEASA</a>
+	</li>
+	<li>
+		<a href="http://hidrosantacruz.com.ar" target="_blank">Hidro Santa Cruz</a>
+	</li>
+	<li>
+		<a href="./contacto.php">Contacto</a>
+	</li>
+</ul>
