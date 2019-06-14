f9675644e5637f9b166f5946a4d43e3f08ec23b4
Juan Pablo <rosset.juanpablo@gmail.com>
2019-06-14 09:58:45 -0300

    Jump Adapt

--- popup.buffer.php
+++ popup.buffer.php
@@ -1,4 +1,4 @@
-<div class="jump-window jump-align-right jump-flotant-heightfill  jump-flotant-heightfill-top col col-xs-12 col-sm-12-col-md-6 col-lg-6 geovisor-flotant" id="popup-buffer">
+<div class="jump-window jump-align-right jump-flotant-heightfill  jump-flotant-heightfill-top col col-xs-12 col-sm-12-col-md-3 col-lg-3 geovisor-flotant" id="popup-buffer">
 	
 	<div class="jump-window-inner p20">
 
