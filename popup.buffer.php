8e567def028093daa0b0f699fd8ec7734338182f
Juan Pablo Rosset <rosset.juanpablo@gmail.com>
2019-06-14 04:06:18 -0300

    jump height windows

--- popup.buffer.php
+++ popup.buffer.php
@@ -1,4 +1,4 @@
-<div class="jump-window jump-align-right jump-align-bottom col col-xs-12 col-sm-12-col-md-6 col-lg-6 geovisor-flotant" id="popup-buffer">
+<div class="jump-window jump-align-right jump-flotant-heightfill-top col col-xs-12 col-sm-12-col-md-6 col-lg-6 geovisor-flotant" id="popup-buffer">
 	
 	<div class="jump-window-inner p20">
 
