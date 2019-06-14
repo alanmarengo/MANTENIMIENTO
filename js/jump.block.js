d4bd1a36234ca0017d7119b54a36ffb67902dd8c
Juan Pablo <rosset.juanpablo@gmail.com>
2019-06-14 14:55:29 -0300

    Fix Search

--- js/jump.block.js
+++ js/jump.block.js
@@ -15,18 +15,37 @@ Jump.block = function() {
 		
 		$(".jump-block-inner-toolbar").each(function(i,v) {
 			
-			var innerElementHeight = $(v).height();			
-			var blockHeight = $(v).closest(".jump-block").height();			
+			if ($(v).closest(".jump-block").hasClass("jump-block-horizontal")) {
 			
-			var margin = ((blockHeight - innerElementHeight) / 2);
+				var innerElementWidth = $(v).height();			
+				var blockWidth = $(v).closest(".jump-block").width();			
+				
+				var margin = ((blockWidth - innerElementWidth) / 2);
+				
+				$(v).css({"margin":"0px " + margin + "px"});
+				
+				$(v).find(".button").css({ "width":innerElementWidth+"px"});
+				$(v).find(".button").css({"height":innerElementWidth+"px","line-height":innerElementWidth+"px" });
+				$(v).find(".button").not(".button-input").parent().css({ "width":innerElementWidth+"px"});
+				$(v).find(".button").parent().css({"height":innerElementWidth+"px","line-height":innerElementWidth+"px" });
+				$(v).find(".input").css({ "height":innerElementWidth,"line-height":innerElementWidth});
 			
-			$(v).css({"margin":margin + "px 0px"});
+			}else{
 			
-			$(v).find(".button").css({ "width":innerElementHeight+"px"});
-			$(v).find(".button").css({"height":innerElementHeight+"px","line-height":innerElementHeight+"px" });
-			$(v).find(".button").not(".button-input").parent().css({ "width":innerElementHeight+"px"});
-			$(v).find(".button").parent().css({"height":innerElementHeight+"px","line-height":innerElementHeight+"px" });
-			$(v).find(".input").css({ "height":innerElementHeight,"line-height":innerElementHeight});
+				var innerElementHeight = $(v).height();			
+				var blockHeight = $(v).closest(".jump-block").height();			
+				
+				var margin = ((blockHeight - innerElementHeight) / 2);
+				
+				$(v).css({"margin":margin + "px 0px"});
+				
+				$(v).find(".button").css({ "width":innerElementHeight+"px"});
+				$(v).find(".button").css({"height":innerElementHeight+"px","line-height":innerElementHeight+"px" });
+				$(v).find(".button").not(".button-input").parent().css({ "width":innerElementHeight+"px"});
+				$(v).find(".button").parent().css({"height":innerElementHeight+"px","line-height":innerElementHeight+"px" });
+				$(v).find(".input").css({ "height":innerElementHeight,"line-height":innerElementHeight});				
+				
+			}
 		
 		});
 		
