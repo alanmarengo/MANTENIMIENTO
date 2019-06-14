aa3da17420cae47496a7834896482ea790871c34
Juan Pablo <rosset.juanpablo@gmail.com>
2019-06-14 14:16:33 -0300

    Fix Search

---
+++ popup.preloader.php
@@ -0,0 +1,21 @@
+<div class="jump-window jump-modal col col-xs-12 col-sm-12-col-md-6 col-lg-6 geovisor-flotant" id="popup-preloader">
+	
+	<div class="jump-window-inner p20">
+
+		<div class="jump-window-header">
+			<span></span>
+			<a href="#" class="jump-window-close">
+				<i class="fas fa-times"></i>
+			</a>
+		</div>
+		
+		<div class="jump-window-body p20">
+			
+			<img src="./images/svg-loaders/oval.svg" alt="" width="50">
+			<p class="mt-10">Cargando...</p>
+			
+		</div>
+	
+	</div>
+
+</div>
