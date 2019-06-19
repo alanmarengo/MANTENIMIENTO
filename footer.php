61a2de2adf0b0db4360cd963d68f121859781324
Antoni Sanoguera <asanoguera@gmail.com>
2019-06-19 02:03:52 -0300

    Fixs

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
 		Av. Paseo Colón 505 Piso 6,<br />
 		Ciudad de Buenos Aires, Argentina
 	</div>
-	<div class="col-md-3" style="font-size: .9em; padding: 10px;">
+	<div class="col-md-6" style="font-size: .9em; padding: 10px;">
 		<b>ENLACES</b>
 		<br />
 		<br />
@@ -26,33 +26,33 @@
 			</ol>
 			<div class="carousel-inner">
 				<div class="carousel-item active">
-					<a href="http://www.ieasa.com.ar">Integración Energética Argentina SA</a><br />
-					<a href="http://hidrosantacruz.com.ar">Hidro Santa Cruz</a><br />
-					<a href="http://mapas.ambiente.gob.ar">Sec. de Gob. de Ambiente y Desarrollo Sustentable - IDE</a><br />
+					<a target="_blank" href="http://www.ieasa.com.ar">Integración Energética Argentina SA</a><br />
+					<a target="_blank" href="http://hidrosantacruz.com.ar">Hidro Santa Cruz</a><br />
+					<a target="_blank" href="http://mapas.ambiente.gob.ar">Sec. de Gob. de Ambiente y Desarrollo Sustentable - IDE</a><br />
 				</div>
 				<div class="carousel-item">
-					<a href="https://www.argentina.gob.ar/energia">Sec. Gob. Energía</a><br />
-					<a href="https://www.argentina.gob.ar/interior/infraestructura-y-politica-hidrica">Sec. Infraestructura y Política Hídrica</a><br />
-					<a href="https://www.argentina.gob.ar/orsep">Organismo Regulador de Seguridad de Presas (ORSEP)</a><br />
+					<a target="_blank" href="https://www.argentina.gob.ar/energia">Sec. Gob. Energía</a><br />
+					<a target="_blank" href="https://www.argentina.gob.ar/interior/infraestructura-y-politica-hidrica">Sec. Infraestructura y Política Hídrica</a><br />
+					<a target="_blank" href="https://www.argentina.gob.ar/orsep">Organismo Regulador de Seguridad de Presas (ORSEP)</a><br />
 				</div>
 				<div class="carousel-item">
-					<a href="http://www.ign.gob.ar/">Instituto Geográfico Nacional (IGN)</a><br />
-					<a href="https://www.argentina.gob.ar/ciencia/conae">Comisión Nacional de Actividades Espaciales (CONAE)</a><br />
-					<a href="http://www.segemar.gov.ar/">Servicio Geológico Minero Argentino (SegemAR)</a><br />
+					<a target="_blank" href="http://www.ign.gob.ar/">Instituto Geográfico Nacional (IGN)</a><br />
+					<a target="_blank" href="https://www.argentina.gob.ar/ciencia/conae">Comisión Nacional de Actividades Espaciales (CONAE)</a><br />
+					<a target="_blank" href="http://www.segemar.gov.ar/">Servicio Geológico Minero Argentino (SegemAR)</a><br />
 				</div>
 				<div class="carousel-item">
-					<a href="https://www.ina.gov.ar/">Instituto Nacional del Agua (INA)</a><br />
-					<a href="https://www.inpres.gob.ar/desktop/">Instituto Nacional de Prevención Sísmica (INPRES)</a><br />
-					<a href="https://www.idera.gob.ar/">Infraestructura de Datos Espaciales de la República Argentina (IDERA)</a><br />
+					<a target="_blank" href="https://www.ina.gov.ar/">Instituto Nacional del Agua (INA)</a><br />
+					<a target="_blank" href="https://www.inpres.gob.ar/desktop/">Instituto Nacional de Prevención Sísmica (INPRES)</a><br />
+					<a target="_blank" href="https://www.idera.gob.ar/">Infraestructura de Datos Espaciales de la República Argentina (IDERA)</a><br />
 				</div>
 				<div class="carousel-item">
-					<a href="https://www.conicet.gov.ar/">Consejo Nacional de Investigaciones Científicas y Técnicas (CONICET) </a><br />
-					<a href="https://www.mendoza.conicet.gov.ar/portal/ianigla/">Instituto Argentino de Nivología, Glaciología y Ciencias Ambientales (IANIGLA)</a><br />
-					<a href="http://www.glaciaresargentinos.gob.ar/">Inventario Nacional de Glaciares (ING)</a><br />
+					<a target="_blank" href="https://www.conicet.gov.ar/">Consejo Nacional de Investigaciones Científicas y Técnicas (CONICET) </a><br />
+					<a target="_blank" href="https://www.mendoza.conicet.gov.ar/portal/ianigla/">Instituto Argentino de Nivología, Glaciología y Ciencias Ambientales (IANIGLA)</a><br />
+					<a target="_blank" href="http://www.glaciaresargentinos.gob.ar/">Inventario Nacional de Glaciares (ING)</a><br />
 				</div>
 				<div class="carousel-item">
-					<a href="http://www.plahe.com.ar/">Plataforma Argentina de Hidroenergía (PLAHE)</a><br />
-					<a href="http://www.represaspatagonia.com.ar/index.php/es/">UTE Represas Patagonia</a><br />
+					<a target="_blank" href="http://www.plahe.com.ar/">Plataforma Argentina de Hidroenergía (PLAHE)</a><br />
+					<a target="_blank" href="http://www.represaspatagonia.com.ar/index.php/es/">UTE Represas Patagonia</a><br />
 				</div>
 			</div>
 		</div>
