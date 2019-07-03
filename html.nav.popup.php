26f9d5e621a32c2a3fb80ae781d7651e9111d89a
Juan Pablo <rosset.juanpablo@gmail.com>
2019-07-03 10:19:05 -0300

    new geovisor

--- html.nav.popup.php
+++ html.nav.popup.php
@@ -1,4 +1,4 @@
-<div id="nav-popup" data-visible="1" class="navmenu jump-flotant-nav jump-flotant-heightfill jump-flotant-heightfill-top jump-scroll nav-level-1 col col-nav col-xs-12 col-sm-12 col-md-4 col-lg-4">
+<div id="nav-popup" style="background-color:#FFFFFF;" data-visible="1" class="navmenu jump-flotant-nav jump-flotant-heightfill jump-flotant-heightfill-top jump-scroll nav-level-1 col col-nav col-xs-12 col-sm-12 col-md-4 col-lg-4">
 	<div id="nav-popup-filter">
 		a
 	</div>
