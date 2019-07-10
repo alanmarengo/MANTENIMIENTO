badd9b3cdf76c7f9e0d6c86dfbc9ac414c6892e2
Antoni Sanoguera <asanoguera@gmail.com>
2019-07-09 23:52:02 -0300

    Iconos en widget links

--- js/widget-links.js
+++ js/widget-links.js
@@ -1,13 +1,15 @@
-window.addEventListener("load",function() {
-	
-	$(".widget-link").hover(
-		function() {			
-			$(this).addClass("widget-link-hovered");
-			$(this).removeClass("widget-link-unhovered");
-		},		
-		function() {			
-			$(this).addClass("widget-link-unhovered");
-			$(this).removeClass("widget-link-hovered");
-		})
-	
-}); 
+window.addEventListener("load", function() {
+
+    $(".widget-link").hover(
+        function() {
+            $(this).addClass("widget-link-hovered");
+            $(this).removeClass("widget-link-unhovered");
+            $(this).find('a').show();
+        },
+        function() {
+            $(this).addClass("widget-link-unhovered");
+            $(this).removeClass("widget-link-hovered");
+            $(this).find('a').hide();
+        });
+
+});
--- widget-links.php
+++ widget-links.php
@@ -1,20 +1,20 @@
 <div class="widget-link" style="background-color:#ffa91b; right:0px; top:140px;">
 	<a href="./mediateca.php">
-			
+		<img src="./images/icono_mediateca.png" />
 	</a>
 </div>
 <div class="widget-link" style="background-color:#8e2f4f; right:0px; top:200px;">
 	<a href="./geovisor.php">
-		
+		<img src="./images/icono_geovisor.png" />
 	</a>
 </div>
 <div class="widget-link" style="background-color:#585dc5; right:0px; top:260px;">
 	<a href="#">
-		
+		<img src="./images/icono_indicadores.png" />
 	</a>
 </div>
 <div class="widget-link" style="background-color:#310133; right:0px; top:320px;">
 	<a href="#">
-		
+		<img src="./images/icono_estadisticas.png" />
 	</a>
 </div>
