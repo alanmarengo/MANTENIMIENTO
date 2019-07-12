0efada839ec9bdde3515f7362dd4df4b18909484
Juan Pablo <rosset.juanpablo@gmail.com>
2019-07-12 13:13:21 -0300

    resizer

--- html.navbar-tools.php
+++ html.navbar-tools.php
@@ -37,16 +37,16 @@
 					</a>
 					<div class="dropdown-menu dropdown-menu-right" id="dropdown-draw" aria-labelledby="navbarDropdown-draw" style="min-width:30px !Important; width:30px;">						
 					
-						<a href="#" onclick="geovisor.map.drawing('point')" class="dropdown-item">			
+						<a href="#" onclick="geomap.map.drawing('point')" class="dropdown-item">			
 							<img src="./images/geovisor/icons/drawing-bar-point.png">						
 						</a>
-						<a href="#" onclick="geovisor.map.drawing('line')" class="dropdown-item">			
+						<a href="#" onclick="geomap.map.drawing('line')" class="dropdown-item">			
 							<img src="./images/geovisor/icons/drawing-bar-line.png">						
 						</a>
-						<a href="#" onclick="geovisor.map.drawing('polygon')" class="dropdown-item">			
+						<a href="#" onclick="geomap.map.drawing('polygon')" class="dropdown-item">			
 							<img src="./images/geovisor/icons/drawing-bar-polygon.png">						
 						</a>
-						<a href="#" onclick="geovisor.map.drawing('circle')" class="dropdown-item">			
+						<a href="#" onclick="geomap.map.drawing('circle')" class="dropdown-item">			
 							<img src="./images/geovisor/icons/drawing-bar-circle.png">						
 						</a>
 						
