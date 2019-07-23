1b66e0ec7f6ca8a1478ed41814c4390016aaddfa
Juan Pablo <rosset.juanpablo@gmail.com>
2019-07-23 12:57:24 -0300

    contacto

--- html.nav.popup.php
+++ html.nav.popup.php
@@ -1,4 +1,4 @@
-<div id="nav-popup" style="background-color:#FFFFFF; display:none;" data-visible="1" class="navmenu jump-window-mode jump-flotant-nav jump-flotant-heightfill jump-flotant-heightfill-top jump-scroll nav-level-1 col col-nav col-xs-12 col-sm-12 col-md-4 col-lg-4">
+<div id="nav-popup" style="background-color:#FFFFFF; display:none;" data-visible="1" class="navmenu jump-window-mode jump-flotant-nav jump-flotant-heightfill jump-flotant-heightfill-top jump-scroll nav-level-1 col col-nav col-xs-12 col-sm-12 col-md-3 col-lg-3">
 	<div id="nav-popup-head" class="nav-popup-element">
 	</div>
 	<div id="nav-popup-filter" class="nav-popup-element">
