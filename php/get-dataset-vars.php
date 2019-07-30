34d57fa58f1827d928829ef7aabe4265708885ae
Juan Pablo <rosset.juanpablo@gmail.com>
2019-07-30 12:23:32 -0300

    list abr stats

--- php/get-dataset-vars.php
+++ php/get-dataset-vars.php
@@ -39,7 +39,7 @@ while ($r = pg_fetch_assoc($query)) {
 			
 			<div class="panel-dataset-group-list-header">
 				<span>SELECCIONAR VARIABLE DE ORIGEN: <?php echo $r["origen"]; ?></span>
-				<a href="#" class="toggeable-icon" onclick="$(this).children('i').switchClass('fa-minus-circle','fa-minus-circle'); $(this).parent().next().slideToggle('fast');">
+				<a href="#" class="toggeable-icon" onclick="$(this).children('i').switchClass('fa-plus-circle','fa-minus-circle'); $(this).parent().next().slideToggle('fast');">
 					<i class="fa fa-minus-circle"></i>
 				</a>
 			</div>
