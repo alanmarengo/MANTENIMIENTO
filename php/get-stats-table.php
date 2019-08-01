843de2c291efd594da9473ee59836ea71036851d
Juan Pablo Rosset <rosset.juanpablo@gmail.com>
2019-08-01 01:45:39 -0300

    mod estadistica footer

--- php/get-stats-table.php
+++ php/get-stats-table.php
@@ -68,7 +68,7 @@ if ($filter_str == "") {
 	$new_query_string = "SELECT $colstr_select FROM ($rquery_string) AS sub WHERE $filter_str $colstr_order";
 	
 }
-echo $new_query_string;
+
 ?>
 	
 <div class="dataset">
