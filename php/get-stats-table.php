09560df50cc44519114f02878297cbf49becdd27
Juan Pablo <rosset.juanpablo@gmail.com>
2019-08-01 11:07:50 -0300

    where stats mod

--- php/get-stats-table.php
+++ php/get-stats-table.php
@@ -99,7 +99,7 @@ if ($no_op) {
 		
 		for ($i=0; $i<sizeof($col); $i++) {
 			
-			$colstr_select .= "\"" . $col[$i] . "\"::" . $colType[$i] . ",";
+			$colstr_select .= $col[$i] . "::" . $colType[$i] . ",";
 			
 		}
 		echo $colstr_select;
