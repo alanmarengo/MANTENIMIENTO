8efa86ce6762cda083eb1388310d906c0c888bb3
Juan Pablo <rosset.juanpablo@gmail.com>
2019-08-04 17:56:37 -0300

    query agroup

--- php/get-stats-table.php
+++ php/get-stats-table.php
@@ -188,7 +188,7 @@ if ($filter_str == "") {
 }
 
 echo $_POST["groupbycol"] . " :: " . $groupbycol . " :: " . $groupindex;
-
+var_dump($_POST);
 if (($groupbycol == 1) && ($groupbycol_index == 0)) {
 
 	$gm_string = explode("ORDER",explode("SELECT",$new_query_string));
