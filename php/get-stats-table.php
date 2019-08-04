d243917f38b0819d5bf06e47745d28c3f14778db
Juan Pablo <rosset.juanpablo@gmail.com>
2019-08-04 16:32:00 -0300

    query agroup

--- php/get-stats-table.php
+++ php/get-stats-table.php
@@ -141,8 +141,24 @@ if ($no_op) {
 			$colstr_select .= $col[$i] . "::" . $colType[$i] . " AS " . $col[$i] . ",";
 			
 		}else{
-		
-			$colstr_select .= $operations[$i] . "(" . $col[$i] . ")::" . $colType[$i] . " AS " . $col[$i] . ",";
+			
+			if ($i<3) {				
+			
+				if ($groupbycol == 1) {
+				
+					$colstr_select .= $operations[$i] . "(" . $col[$i] . ")::" . $colType[$i] . " AS " . $col[$i] . ",";
+			
+				}else{
+					
+					$colstr_select .="MAX('')::TEXT AS " . $col[$i] . ",";
+					
+				}
+				
+			}else{
+				
+				$colstr_select .= $operations[$i] . "(" . $col[$i] . ")::" . $colType[$i] . " AS " . $col[$i] . ",";
+				
+			}
 		
 		}
 		
