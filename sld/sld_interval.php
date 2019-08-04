8f14501a2620ff5b0f165860d107197b60f5d597
Martín Flores <martinmaximilianoflores@gmail.com>
2019-08-03 22:53:59 -0300

    sld_intervals modifica query de origen

--- sld/sld_interval.php
+++ sld/sld_interval.php
@@ -8,7 +8,7 @@ $string_conn = "host=" . pg_server . " user=" . pg_user . " port=" . pg_portv . 
 	
 $conn = pg_connect($string_conn);
 
-$SQL = "SELECT max(valor)as maxx, min(valor) as minn,((max(valor)-min(valor))/5)AS salto FROM mod_estadistica.dt_layer_data WHERE id=$id;";
+$SQL = "SELECT max(_valor_)as maxx, min(_valor_) as minn,((max(_valor_)-min(_valor_))/5)AS salto FROM mod_estadistica.get_mapeo_value($id);";
 
 $recordset = pg_query($conn,$SQL);
 
@@ -20,28 +20,6 @@ $salto = $row[3];
 $acumulado = 0.0;
 
 pg_close($conn);
-
-/*
-const _POINT_   =0;
-const _LINE_    =1;
-const _POLYGON_ =2;
-*/
-
-//$type_geom	= $_REQUEST['type_geom'		];
-//$main_color 	= $_REQUEST['main_color'	];
-//$layer_name 	= $_REQUEST['layer_name'	];
-//$size			= $_REQUEST['size'			];
-//$border_color = $_REQUEST['border_color'	];
-//$border_size 	= $_REQUEST['border_size'	];
-
-/*
-switch ($type_geom) 
-{
-    case _POINT_	:$sld_file 	= file_get_contents("./point.sld");	$layer_name = '';	break;
-    case _LINE_		:$sld_file 	= file_get_contents("./line.sld");	$layer_name = '';	break;
-    case _POLYGON_	:$sld_file 	= file_get_contents("./intervalo_polygon.sld");	$layer_name = 'intervalos_polygons'; break;
-};
-*/
 
 $sld_file 	= file_get_contents("./intervalo_polygon.sld");	$layer_name = 'intervalos_polygons';
 
