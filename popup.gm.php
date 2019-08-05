e032d3f822aca067aa1b02450aba5b81094fe445
Juan Pablo <rosset.juanpablo@gmail.com>
2019-08-05 12:17:13 -0300

    table

--- popup.gm.php
+++ popup.gm.php
@@ -1,12 +1,25 @@
 <div class="popup-modal" id="popup-modal-gm"></div>
 <div class="popup-stats" id="popup-stats-gm">
 
-	<div class="row jump-row jus-between">
+	<div class="row jump-row jus-right">
 
-		<h3>Mapear Variable</h3>
 		<a href="#" onclick="$('#popup-modal-gm').hide(); $('#popup-stats-gm').hide();">
 			<i class="fa fa-times"></i>
 		</a>
+	
+	</div>
+
+	<div class="row jump-row jus-between">
+
+		<h3>Mapear Variable</h3>
+		<div class="icons">
+			<a href="#" onclick="$('#popup-modal-gm').hide(); $('#popup-stats-gm').hide();">
+				<i class="fa fa-print"></i>
+			</a>
+			<a href="#" onclick="$('#popup-modal-gm').hide(); $('#popup-stats-gm').hide();">
+				<i class="fa fa-download"></i>
+			</a>
+		</div>	
 	
 	</div>
 
