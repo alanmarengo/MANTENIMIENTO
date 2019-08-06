1d8b3461bca8332e4eabb29f3eded6616b10227e
Martín Flores <martinmaximilianoflores@gmail.com>
2019-08-06 10:43:51 -0300

    SLD Estatico

--- sld/sld_interval.php
+++ sld/sld_interval.php
@@ -52,7 +52,10 @@ $sld_file	= str_replace("[5_H]"	, $acumulado	,$sld_file);
 
 //echo $sld_file;
 
-file_put_contents("/$id.sld", $sld_file);
+if(!file_put_contents("/$id.sld", $sld_file))
+{
+	echo "NO se pudo generar el SLD";
+};
 
 echo "./$id.sld";
 
