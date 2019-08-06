fb5692415cf458bc9ece831e5f4c42bdc5a3635e
Juan Pablo <rosset.juanpablo@gmail.com>
2019-08-06 18:12:03 -0300

    GM

--- js/stats.js
+++ js/stats.js
@@ -594,7 +594,7 @@ function ol_stats() {
 			tooltip: {
 				headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
 				pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-					'<td style="padding:0"><b>{point.y:.1f}</b></td></tr>',q
+					'<td style="padding:0"><b>{point.y:.1f}</b></td></tr>',
 				footerFormat: '</table>',
 				shared: true,
 				useHTML: true
