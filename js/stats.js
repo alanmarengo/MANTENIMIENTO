755f8272b0fb18358b99452ec20dd7706891aa86
Juan Pablo <rosset.juanpablo@gmail.com>
2019-08-07 00:06:12 -0300

    graph types

--- js/stats.js
+++ js/stats.js
@@ -567,7 +567,7 @@ function ol_stats() {
 		}
 		
 		if ((!no_op) || (bypassOp)) {
-			alert(JSON.stringify(data));
+			
 			var req = $.ajax({
 				
 				async:false,
@@ -577,10 +577,27 @@ function ol_stats() {
 				success:function(d){}
 				
 			});
+			
+			var blob = new Blob([req.responseText], { type: 'text/csv;charset=utf-8;' });
+			if (navigator.msSaveBlob) { // IE 10+
+				navigator.msSaveBlob(blob, filename);
+			} else {
+				var link = document.createElement("a");
+				if (link.download !== undefined) { // feature detection
+					// Browsers that support HTML5 download attribute
+					var url = URL.createObjectURL(blob);
+					link.setAttribute("href", url);
+					link.setAttribute("download", filename);
+					link.style.visibility = 'hidden';
+					document.body.appendChild(link);
+					link.click();
+					document.body.removeChild(link);
+				}
+			}
 		
 		}else{
 			
-			alert("Faltan seleccionar funciones para poder actualizar la vista");
+			alert("Faltan seleccionar funciones para poder descargar la vista");
 			$("#update-view").prop("disabled",false);
 			
 		}
