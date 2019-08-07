bacdf9f4c0f02acc9beeeadb9183f2557c977eef
Juan Pablo <rosset.juanpablo@gmail.com>
2019-08-06 22:42:37 -0300

    graph types

--- js/stats.js
+++ js/stats.js
@@ -867,6 +867,68 @@ function ol_stats() {
 			
 			break;
 			
+			case 6:
+			
+			this.chart = Highcharts.chart('gm-stats-mediawrapper', {
+				chart: {
+					type: 'column',
+					height:350,
+					width:600
+				},
+				title: {
+					text: colagroup
+				},
+				subtitle: {
+					text: "por " + coldataset
+				},
+				xAxis: {
+					categories: labels
+				},
+				yAxis: {
+					min: 0,
+					title: {
+						text: coldataset
+					},
+					stackLabels: {
+						enabled: true,
+						style: {
+							fontWeight: 'bold',
+							color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+						}
+					}
+				},
+				legend: {
+					align: 'right',
+					x: -30,
+					verticalAlign: 'top',
+					y: 25,
+					floating: true,
+					backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+					borderColor: '#CCC',
+					borderWidth: 1,
+					shadow: false
+				},
+				tooltip: {
+					headerFormat: '<b>{point.x}</b><br/>',
+					pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+				},
+				plotOptions: {
+					column: {
+						stacking: 'normal',
+						dataLabels: {
+							enabled: true,
+							color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
+						}
+					}
+				},
+				series:[{
+					name:colagroup,
+					data: values
+				}]
+			});
+			
+			break;
+			
 		}
 			
 		
