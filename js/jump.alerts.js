1f39a577f8dca315d82f7620603f0da10f32dabd
Juan Pablo <rosset.juanpablo@gmail.com>
2019-08-08 10:25:14 -0300

    table

--- js/jump.alerts.js
+++ js/jump.alerts.js
@@ -1,7 +1,7 @@
 function jalert(msg,bs_class) {	
 	
 	$(".jump-alert-container .jump-alert-body").html(msg);
-	$(".jump-alert-container").attr("class","jump-alert jump-alert-"+bs_class);
+	$(".jump-alert-container").attr("class","jump-alert-container jump-alert-"+bs_class);
 	
 	$(".jump-alert-modal").show();
 	$(".jump-alert-container").show();	
