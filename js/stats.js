c39ef554af235c6d5c5af95cc3a1f24d090a5862
Juan Pablo <rosset.juanpablo@gmail.com>
2019-08-12 14:44:18 -0300

    table

--- js/stats.js
+++ js/stats.js
@@ -33,7 +33,7 @@ function ol_stats() {
 	
 	
 	this.panel.startSearch = function() {
-		
+		alert("INI PANEL");
 		$("#panel-seach-input-layers").val("");
 		
 		$("#panel-seach-input-layers").bind("keyup",function(e) {
