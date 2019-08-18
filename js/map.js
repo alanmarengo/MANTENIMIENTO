e71d94cbe7086a70380260f221d5bae365891035
Juan Pablo <rosset.juanpablo@gmail.com>
2019-08-18 17:34:30 -0300

    graph types

--- js/map.js
+++ js/map.js
@@ -1162,12 +1162,16 @@ function ol_map() {
 			if (this.getAttribute("data-active") == 0) {
 				
 				$(".panel-abr").attr("data-active","0");
-				$(".panel-abr").css("border-color","transparent");
-				$(".panel-abr").css("background-color","transparent");
+				//$(".panel-abr").css("border-color","transparent");
+				//$(".panel-abr").css("background-color","transparent");
+				$(".panel-abr").css("background-color","#F5F5F5");
+				$(".panel-abr").css("color","#666666");
 				
 				$(this).attr("data-active","1");
-				$(this).css("border-color",this.getAttribute("data-color"));
-				$(this).css("background-color",this.getAttribute("data-bgcolor"));
+				//$(this).css("border-color",this.getAttribute("data-color"));
+				//$(this).css("background-color",this.getAttribute("data-bgcolor"));
+				$(this).css("background-color","#31cbfd");
+				$(this).css("color","#FFFFFF");
 				
 				$(".layer-container").not(".layer-container[data-cid="+this.getAttribute("data-cid")+"]").hide();
 				$(".layer-container[data-cid="+this.getAttribute("data-cid")+"]").show();
