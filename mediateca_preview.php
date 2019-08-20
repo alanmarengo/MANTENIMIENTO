5db24462f4f95acc88871fbde66b2319c7412d42
Martín Flores <martinmaximilianoflores@gmail.com>
2019-08-19 23:02:20 -0300

    Agregar soporte para preview

--- mediateca_preview.php
+++ mediateca_preview.php
@@ -9,7 +9,7 @@ $string_conn = "host=" . pg_server . " user=" . pg_user . " port=" . pg_portv . 
 	
 $conn = pg_connect($string_conn);
 
-$SQL = "SELECT recurso_path_url,upper(right(recurso_path_url,3))AS extension FROM mod_mediateca.recurso R WHERE recurso_id=$recurso_id limit 1;";
+$SQL = "SELECT recurso_path_url,upper(right(recurso_path_url,3))AS extension,recurso_preview_path FROM mod_mediateca.recurso R WHERE recurso_id=$recurso_id limit 1;";
 
 $recordset = pg_query($conn,$SQL);
 
@@ -17,6 +17,12 @@ $row = pg_fetch_row($recordset);
 
 $recursor_path = $row[0];
 $recursos_extension = $row[1];
+$recurso_preview = $row[2];
+
+if ($recurso_preview==NULL)
+{
+	$recurso_preview='./images/3.jpg';
+};
 
 pg_close($conn);
 
@@ -32,7 +38,7 @@ if($recursos_extension=='PDF')
 else
 {
 	$imagick = new Imagick();
-	$imagick->readImage('./images/sin_data.jpg');
+	$imagick->readImage($recurso_preview);
 	$imagick->setImageFormat("jpg");
 
 	echo $imagick->getImagesBlob();
