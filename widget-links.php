ed627c21b3b9d45040c30cfd26de37ea873c02d7
Juan Pablo <rosset.juanpablo@gmail.com>
2019-08-20 09:23:25 -0300

    table

--- widget-links.php
+++ widget-links.php
@@ -1,20 +1,24 @@
 <div class="widget-link" style="background-color:#ffa91b; right:0px; top:140px;">
 	<a href="./mediateca.php">
-		<img src="./images/icono_mediateca.png" />
+		<!--<img src="./images/icono_mediateca.png" />-->
+		<span>MEDIATECA</span>
 	</a>
 </div>
 <div class="widget-link" style="background-color:#8e2f4f; right:0px; top:200px;">
 	<a href="./geovisor.php">
-		<img src="./images/icono_geovisores.png" />
+		<!--<img src="./images/icono_geovisores.png" />-->
+		<span>GEOVISORES</span>
 	</a>
 </div>
 <div class="widget-link" style="background-color:#585dc5; right:0px; top:260px;">
 	<a href="#">
-		<img src="./images/icono_indicadores.png" />
+		<!--<img src="./images/icono_indicadores.png" />-->
+		<span>INDICADORES</span>
 	</a>
 </div>
 <div class="widget-link" style="background-color:#310133; right:0px; top:320px;">
 	<a href="./estadisticas.php">
-		<img src="./images/icono_estadisticas.png" />
+		<!--<img src="./images/icono_estadisticas.png" />-->
+		<span>ESTADISTICAS</span>
 	</a>
 </div>
