2e4b02d302d9489124198a6f22003d8ac74bc7d6
Juan Pablo <rosset.juanpablo@gmail.com>
2019-08-21 11:39:22 -0300

    table

--- scripts.default.php
+++ scripts.default.php
@@ -48,6 +48,7 @@
 <!-- GEOVISOR -->
 
 <link rel="stylesheet" href="./css/geovisor/align.css"/>
+<link rel="stylesheet" href="./css/geovisor/buttons.css"/>
 <link rel="stylesheet" href="./css/geovisor/forms.css"/>
 <link rel="stylesheet" href="./css/geovisor/layers.css"/>
 <link rel="stylesheet" href="./css/geovisor/map.css"/>
