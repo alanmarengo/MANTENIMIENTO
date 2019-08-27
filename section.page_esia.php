82369e3c59af040c5d8243b3d31baab356f51fc7
Antoni Sanoguera <asanoguera@gmail.com>
2019-08-26 23:11:19 -0300

    cambios esia

--- section.page_esia.php
+++ section.page_esia.php
@@ -101,7 +101,7 @@
                         </video>
                     </div>
                 </div>
-                <div class="col-md-5">
+                <div class="col-md-5" style="padding-left: 40px; padding-right: 40px;">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
