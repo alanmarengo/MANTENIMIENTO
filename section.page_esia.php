6b48dd5a253faa47db013532144b225a08b63aae
Antoni Sanoguera <asanoguera@gmail.com>
2019-08-26 22:33:53 -0300

    page_esia

---
+++ section.page_esia.php
@@ -0,0 +1,133 @@
+<link rel="stylesheet" type="text/css" href="./css/page_esia.css" />
+
+
+<div id="page_esia" class="page page_template">
+    <div class="row">
+        <div class="col-md-12 page-title">
+            Evaluacion de impacto ambiental-EsIA
+        </div>
+
+        <div class="col-md-12 top-buttons">
+            <a href="./historia.php">HISTORIA</a>
+            <a href="./lineatiempo.php">LINEA DE TIEMPO</a>
+            <a href="./audienciapublica.php">AUDIENCIA PUBLICA</a>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 section-a historia">
+            <h3>HISTORIA DEL PROYECTO</h3>
+
+            <p>
+                El proyecto de los aprovechamientos hidroeléctricos sobre el río Santa Cruz cuenta con una larga historia. Sus inicios se remontan a la década del 20, cuando Agua y Energía Eléctrica tenía a cargo el desarrollo del proyecto. Entre 1948 y 1980 se realizaron una serie de estudios preliminares y los primeros anteproyectos.
+            </p>
+            <p>
+                Sin embargo, hubo que esperar hasta esta década para que el proyecto se retome y avance sobre una alternativa con dos aprovechamientos viables técnica, económica y ambientalmente.
+            </p>
+            <p>
+                En particular, el nuevo proyecto garantiza la no interferencia del embalse ubicado aguas arriba sobre el lago Argentino y la no afectación de la cuenca glaciaria que le da origen.
+            </p>
+
+            <a href="./informe_completo.php">Leer el informe completo</a>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 section-b linea-tiempo">
+            <h3>LÍNEA DE TIEMPO</h3>
+
+            <div class="cuadro-tiempo">
+                <div class="fila-1">
+                    <div class="w10 anio"></div>
+                    <div class="w10">30 días para análisis del proyecto</div>
+                    <div class="w10">Desición de revisar proyecto.</div>
+                    <div class="w30">
+                        <div class="w25">A. Lara confirma desacople</div>
+                        <div class="w25">Desarrollo nuevo PE y renegociación de contrato UTE</div>
+                        <div class="w25">Análisis del EsIA 2015 de la UTE</div>
+                        <div class="w25">MAyDS emite ETAs de 18 estudios</div>
+                    </div>
+                    <div class="w30">
+                        <div class="w25">Se firma ADENDA IV a contrato</div>
+                        <div class="w25">Firma ADENDA a convenio con Santa Cruz</div>
+                        <div class="w50">Se reinician obras temporarias y se comienzan campañas para estudios complementarios</div>
+                    </div>
+                    <div class="w10">21 Diciembre</div>
+                </div>
+                <div class="fila-2">
+                    <div class="w10 anio">2016</div>
+                    <div class="w10 bloque-1">10 Enero</div>
+                    <div class="w10 bloque-2">11 Febrero</div>
+                    <div class="w30 bloque-3">Abril</div>
+                    <div class="w30 bloque-1">2 Agosto</div>
+                    <div class="w10 bloque-2">21 Diciembre</div>
+                </div>
+                <div class="fila-3">.
+                </div>
+                <div class="fila-4">.
+                    <div class="w10 anio">2017</div>
+                    <div class="w20 bloque-2">28 Abril</div>
+                    <div class="w15 bloque-3">14 Junio</div>
+                    <div class="w15 bloque-1">20 Julio</div>
+                    <div class="w20 bloque-2">Agosto</div>
+                    <div class="w20 bloque-3">Octubre</div>
+                </div>
+                <div class="fila-5">
+                    <div class="w10 anio"></div>
+                    <div class="w20">
+                        <div class="w33">Se inician otros estudios especiales</div>
+                        <div class="w33">EBISA termina nuevo EsIA</div>
+                        <div class="w33">Revisión con MADS</div>
+                    </div>
+                    <div class="w15">Nuevo EsIA publicado</div>
+                    <div class="w15">Audiencia pública</div>
+                    <div class="w20">Aprovación EIA </div>
+                    <div class="w20">Órden de inicio</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 section-a audiencia-publica">
+            <h3>AUDIENCIA PÚBLICA</h3>
+
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="video-container">
+                        <video width="700" controls>
+                            <source src="http://observatorio.ieasa.com.ar/sga/recursos/recursos_audiovisual/00_audiencia_publica/AUDIENCIA PÚBLICA 20-07-17.mp4" type="video/mp4" />
+                            Tu navegador no soporta reproducir este video.
+                        </video>
+                    </div>
+                </div>
+                <div class="col-md-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 section-b temas-interes">
+            <h3>TEMAS DE INTERÉS EN LA AUDIENCIA PÚBLICA</h3>
+
+            <div class="row">
+
+            <a href="#" class="icono">
+                <img src="./images/iconos_audiencia/transparencia.png">
+                <div>SIA transparencia y divulgación</div>
+            </a>
+            <a href="#" class="icono">
+                <img src="./images/iconos_audiencia/especies_biologicas_interes.png">
+                <div>Especies  biológicas de interés</div>
+            </a>
+
+            </div>
+
+
+        </div>
+    </div>
+</div>
