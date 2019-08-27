ab99919d0e45ab5eba93626fa1c21fd3b9aa262e
Juan Pablo <rosset.juanpablo@gmail.com>
2019-08-27 13:27:32 -0300

    window fix

---
+++ template-indicadores-1.php
@@ -0,0 +1,79 @@
+<div class="template-indicador-container" id="template-indicadores-1">
+
+	<div class="row jump-row" >
+
+		<div class="col-md-4 col-lg-4 col-xs-12 col-sm-12" id="pos-1">
+		
+			
+		
+		</div>
+
+		<div class="col-md-4 col-lg-4 col-xs-12 col-sm-12" id="pos-2">
+		
+			
+		
+		</div>
+
+		<div class="col-md-4 col-lg-4 col-xs-12 col-sm-12" id="pos-3">
+		
+			
+		
+		</div>
+
+	</div>
+
+	<div class="row jump-row" >
+
+		<div class="col-md-12 col-lg-12 col-xs-12 col-sm-12" id="pos-4">
+		
+			
+		
+		</div>
+
+	</div>
+
+	<div class="row jump-row" >
+
+		<div class="col-md-6 col-lg-6 col-xs-12 col-sm-12" id="pos-5">
+		
+			
+		
+		</div>
+
+		<div class="col-md-6 col-lg-6 col-xs-12 col-sm-12" id="pos-6">
+		
+			
+		
+		</div>
+
+	</div>
+
+	<div class="row jump-row" >
+
+		<div class="col-md-3 col-lg-3 col-xs-12 col-sm-12" id="pos-7">
+		
+			
+		
+		</div>
+
+		<div class="col-md-3 col-lg-3 col-xs-12 col-sm-12" id="pos-8">
+		
+			
+		
+		</div>
+
+		<div class="col-md-3 col-lg-3 col-xs-12 col-sm-12" id="pos-9">
+		
+			
+		
+		</div>
+
+		<div class="col-md-3 col-lg-3 col-xs-12 col-sm-12" id="pos-10">
+		
+			
+		
+		</div>
+
+	</div>
+
+</div>
