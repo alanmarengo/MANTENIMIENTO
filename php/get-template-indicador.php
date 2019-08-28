e09e192d35b4ad242977f0f87d021c1624923660
Juan Pablo <rosset.juanpablo@gmail.com>
2019-08-28 09:50:17 -0300

    window fix

--- php/get-template-indicador.php
+++ php/get-template-indicador.php
@@ -1,7 +1,27 @@
 <?php
 
+include("../pgconfig.php");
+
 $ind_id = $_POST["ind_id"];
 
-echo "SERVER " . $ind_id;
+$string_conn = "host=" . pg_server . " user=" . pg_user . " port=" . pg_portv . " password=" . pg_password . " dbname=" . pg_db;
+	
+$conn = pg_connect($string_conn);
+
+$query_string = "SELECT template_id FROM mod_indicadores.ind_panel WHERE panel_id = " . $ind_id;
+
+$query = pg_query($conn,$query_string);
+
+$data = pg_fetch_assoc($query);
+
+$template_id = $data["template_id"];
+
+$query_string = "SELECT * FROM mod_indicadores.template WHERE template_id = " . $template_id;
+
+$query = pg_query($conn,$query_string);
+
+$data = pg_fetch_assoc($query);
+
+include($data["template_path"]);
 
 ?>
