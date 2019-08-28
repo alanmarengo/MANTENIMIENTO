6af7948b0eaadcf9cbe10715a8aa35387da9962d
Juan Pablo <rosset.juanpablo@gmail.com>
2019-08-28 12:16:10 -0300

    window fix

--- template-indicadores-1.php
+++ template-indicadores-1.php
@@ -1,81 +1,77 @@
 <div class="template-indicador-container" id="template-indicadores-1">
 
-	<div class="container">
+	<div class="row" >
 
-		<div class="row" >
-
-			<div class="col col-md-4 col-lg-4 col-xs-12 col-sm-12" id="pos-1">
-			
-				A1
-			
-			</div>
-
-			<div class="col col-md-4 col-lg-4 col-xs-12 col-sm-12" id="pos-2">
-			
-				A2
-			
-			</div>
-
-			<div class="col col-md-4 col-lg-4 col-xs-12 col-sm-12" id="pos-3">
-			
-				A3
-			
-			</div>
-
+		<div class="col col-md-4 col-lg-4 col-xs-12 col-sm-12" id="pos-1">
+		
+			A1
+		
 		</div>
 
-		<div class="row" >
-
-			<div class="col col-md-12 col-lg-12 col-xs-12 col-sm-12" id="pos-4">
-			
-				B1
-			
-			</div>
-
+		<div class="col col-md-4 col-lg-4 col-xs-12 col-sm-12" id="pos-2">
+		
+			A2
+		
 		</div>
 
-		<div class="row" >
-
-			<div class="col col-md-6 col-lg-6 col-xs-12 col-sm-12" id="pos-5">
-			
-				C1
-			
-			</div>
-
-			<div class="col col-md-6 col-lg-6 col-xs-12 col-sm-12" id="pos-6">
-			
-				C2
-			
-			</div>
-
+		<div class="col col-md-4 col-lg-4 col-xs-12 col-sm-12" id="pos-3">
+		
+			A3
+		
 		</div>
 
-		<div class="row" >
+	</div>
 
-			<div class="col col-md-3 col-lg-3 col-xs-12 col-sm-12" id="pos-7">
-			
-				D1
-			
-			</div>
+	<div class="row" >
 
-			<div class="col col-md-3 col-lg-3 col-xs-12 col-sm-12" id="pos-8">
-			
-				D2
-			
-			</div>
+		<div class="col col-md-12 col-lg-12 col-xs-12 col-sm-12" id="pos-4">
+		
+			B1
+		
+		</div>
 
-			<div class="col col-md-3 col-lg-3 col-xs-12 col-sm-12" id="pos-9">
-			
-				D3
-			
-			</div>
+	</div>
 
-			<div class="col col-md-3 col-lg-3 col-xs-12 col-sm-12" id="pos-10">
-			
-				D4
-			
-			</div>
+	<div class="row" >
 
+		<div class="col col-md-6 col-lg-6 col-xs-12 col-sm-12" id="pos-5">
+		
+			C1
+		
+		</div>
+
+		<div class="col col-md-6 col-lg-6 col-xs-12 col-sm-12" id="pos-6">
+		
+			C2
+		
+		</div>
+
+	</div>
+
+	<div class="row" >
+
+		<div class="col col-md-3 col-lg-3 col-xs-12 col-sm-12" id="pos-7">
+		
+			D1
+		
+		</div>
+
+		<div class="col col-md-3 col-lg-3 col-xs-12 col-sm-12" id="pos-8">
+		
+			D2
+		
+		</div>
+
+		<div class="col col-md-3 col-lg-3 col-xs-12 col-sm-12" id="pos-9">
+		
+			D3
+		
+		</div>
+
+		<div class="col col-md-3 col-lg-3 col-xs-12 col-sm-12" id="pos-10">
+		
+			D4
+		
 		</div>
 
 	</div>
