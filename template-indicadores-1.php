48f92bd44cbda08dff317287afc3f8f5fa20bcb5
Juan Pablo <rosset.juanpablo@gmail.com>
2019-08-28 12:12:08 -0300

    window fix

--- template-indicadores-1.php
+++ template-indicadores-1.php
@@ -2,29 +2,19 @@
 
 	<div class="row jump-row" >
 
-		<div class="col-md-4 col-lg-4 col-xs-12 col-sm-12" id="pos-1">
+		<div class="col col-md-4 col-lg-4 col-xs-12 col-sm-12" id="pos-1">
 		
 			
 		
 		</div>
 
-		<div class="col-md-4 col-lg-4 col-xs-12 col-sm-12" id="pos-2">
+		<div class="col col-md-4 col-lg-4 col-xs-12 col-sm-12" id="pos-2">
 		
 			
 		
 		</div>
 
-		<div class="col-md-4 col-lg-4 col-xs-12 col-sm-12" id="pos-3">
-		
-			
-		
-		</div>
-
-	</div>
-
-	<div class="row jump-row" >
-
-		<div class="col-md-12 col-lg-12 col-xs-12 col-sm-12" id="pos-4">
+		<div class="col col-md-4 col-lg-4 col-xs-12 col-sm-12" id="pos-3">
 		
 			
 		
@@ -34,13 +24,7 @@
 
 	<div class="row jump-row" >
 
-		<div class="col-md-6 col-lg-6 col-xs-12 col-sm-12" id="pos-5">
-		
-			
-		
-		</div>
-
-		<div class="col-md-6 col-lg-6 col-xs-12 col-sm-12" id="pos-6">
+		<div class="col col-md-12 col-lg-12 col-xs-12 col-sm-12" id="pos-4">
 		
 			
 		
@@ -50,25 +34,41 @@
 
 	<div class="row jump-row" >
 
-		<div class="col-md-3 col-lg-3 col-xs-12 col-sm-12" id="pos-7">
+		<div class="col col-md-6 col-lg-6 col-xs-12 col-sm-12" id="pos-5">
 		
 			
 		
 		</div>
 
-		<div class="col-md-3 col-lg-3 col-xs-12 col-sm-12" id="pos-8">
+		<div class="col col-md-6 col-lg-6 col-xs-12 col-sm-12" id="pos-6">
 		
 			
 		
 		</div>
 
-		<div class="col-md-3 col-lg-3 col-xs-12 col-sm-12" id="pos-9">
+	</div>
+
+	<div class="row jump-row" >
+
+		<div class="col col-md-3 col-lg-3 col-xs-12 col-sm-12" id="pos-7">
 		
 			
 		
 		</div>
 
-		<div class="col-md-3 col-lg-3 col-xs-12 col-sm-12" id="pos-10">
+		<div class="col col-md-3 col-lg-3 col-xs-12 col-sm-12" id="pos-8">
+		
+			
+		
+		</div>
+
+		<div class="col col-md-3 col-lg-3 col-xs-12 col-sm-12" id="pos-9">
+		
+			
+		
+		</div>
+
+		<div class="col col-md-3 col-lg-3 col-xs-12 col-sm-12" id="pos-10">
 		
 			
 		
