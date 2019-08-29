0dfedefb208d1148511634996fedd96f76c34841
Juan Pablo <rosset.juanpablo@gmail.com>
2019-08-29 13:08:44 -0300

    mpop

--- indicadores.get-recurso.php
+++ indicadores.get-recurso.php
@@ -9,8 +9,8 @@ $string_conn = "host=" . pg_server . " user=" . pg_user . " port=" . pg_portv . 
 	
 $conn = pg_connect($string_conn);
 
-//$query_string = "SELECT * FROM mod_indicadores.vw_recursos WHERE ind_id = $ind_id AND posicion = $pos";
-$query_string = "SELECT * FROM mod_indicadores.vw_recursos WHERE ind_id = 1 AND posicion = 1";
+$query_string = "SELECT * FROM mod_indicadores.vw_recursos WHERE ind_id = $ind_id AND posicion = $pos";
+//$query_string = "SELECT * FROM mod_indicadores.vw_recursos WHERE ind_id = 1 AND posicion = 1";
 
 $query = pg_query($conn,$query_string);
 
@@ -19,8 +19,6 @@ $layer_name = array();
 $layer_server = array();
 
 while($r = pg_fetch_assoc($query)) {
-	
-	echo "<br>e" . $r["resource_type"] . "<br>";
 	
 	switch($r["resource_type"]) {
 		
