71516316d129698d6438154192f20c65d20acff1
Juan Pablo <rosset.juanpablo@gmail.com>
2019-08-30 13:45:22 -0300

    grafico def

--- js/indicadores.js
+++ js/indicadores.js
@@ -130,7 +130,8 @@ function ol_indicadores() {
 			
 			var table = document.createElement("table");
 			var headRow = document.createElement("tr");
-			alert(js.columns.length);
+			alert("tabla");
+			console.log(js);
 			for (var i=0; i<js.columns.length; i++) {
 				
 				var td = document.createElement("td");
