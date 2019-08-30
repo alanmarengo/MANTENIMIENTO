a33560643b1bc235e3592eb067a81de43a2ca3e7
Juan Pablo <rosset.juanpablo@gmail.com>
2019-08-30 10:51:51 -0300

    grafico def

--- grafico-popup.php
+++ grafico-popup.php
@@ -54,10 +54,44 @@
 						}
 					}
 				},
-				series: {
+				/*series: {
 					name: js.etiqueta,
 					colorByPoint: true,
 					data: js.series
+				}*/
+				series: {
+					name: js.etiqueta,
+					colorByPoint: true,
+					data: [{
+						name: 'Chrome',
+						y: 61.41,
+						sliced: true,
+						selected: true
+					}, {
+						name: 'Internet Explorer',
+						y: 11.84
+					}, {
+						name: 'Firefox',
+						y: 10.85
+					}, {
+						name: 'Edge',
+						y: 4.67
+					}, {
+						name: 'Safari',
+						y: 4.18
+					}, {
+						name: 'Sogou Explorer',
+						y: 1.64
+					}, {
+						name: 'Opera',
+						y: 1.6
+					}, {
+						name: 'QQ',
+						y: 1.2
+					}, {
+						name: 'Other',
+						y: 2.61
+					}]
 				}
 			});
 		
