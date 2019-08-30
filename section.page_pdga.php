b0d05400a3863e090841201d3b50e29943356e79
Antoni Sanoguera <asanoguera@gmail.com>
2019-08-29 23:46:28 -0300

    pagina PDGA

---
+++ section.page_pdga.php
@@ -0,0 +1,135 @@
+<link rel="stylesheet" type="text/css" href="./css/page_template.css" />
+
+<div id="page_esia" class="page page_template">
+    <div class="row">
+        <div class="col-md-12 page-title">
+            Plan director de gestión ambiental - PDGA.
+        </div>
+
+        <div class="col-md-12 top-buttons section-sticky">
+            <a href="#" data-target="#descripcion">Descripción</a>
+            <a href="#" data-target="#estructura">Estructura del PDGA</a>
+            <a href="#" data-target="#temas">Temas claves</a>
+            <a href="#" data-target="#responsabilidades">Responsabilidades y seguimientos</a>
+            <a href="#" data-target="#articulacion">ARTICULACIÓN INSTITUCIONAL</a>
+        </div>
+
+        <div class="col-md-12">
+            <img src="./images/pdga_fondo.jpg" style="width: 100%; height: auto;" />
+        </div>
+
+        <div class="col-md-12 section-a" style="padding-left: 8em; padding-right: 8em;">
+            <a id="descripcion"></a>
+            <h3 style="color: #8e2f4f;">DESCRIPCIóN</h3>
+
+            <div class="row">
+                <div class="col-md-6">
+                    EL Plan Director de Gestión Ambiental - PDGA es el instrumento de gestión que permite organizar las medidas y las recomendaciones que surgen del Estudio de Impacto Ambiental - EsIA, para la mitigación de los impactos sobre el medio físico, biótico y socioeconómicos y materializar los objetivos propuestos en materia ambiental.                     <br />        
+                    <br />        
+                    <br />        
+                    El PDGA de los AHRSC (formulado en el capítulo 6 del EsIA 2017 EBISA) reúne el contenido del Plan de Gestión Ambiental - PGA comprometido por la UTE Contratista en el EsIA del 2015 y establece nuevos compromisos asumidos por el Comitente (Estado Nacional) para contribuir a la sostenibilidad de los AH sobre el rio Santa Cruz, pasando así a denominarse Plan Director.
+                </div>
+                <div class="col-md-6">
+                Su planificación desde la priorización de estudios de acuerdo al cronograma de obra (identificando temas críticos, abordaje en etapa y zonas de prioridad) permite una gestión adaptativa del Plan, una mejor administración de recursos y el cumplimiento de metas concretas.
+La ejecución del PGA involucra actores de diversos ámbitos, con distintos niveles de compromiso. Los compromisos ya asumidos en el EsIA, han sido levantados y ratificados por distintos actores de importancia en las Audiencias públicas, resoluciones ministeriales, en dictámenes particulares y constituyen obligaciones para el Comitente y la UTE. 
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-12 section-b estructura">
+            <a id="estructura"></a>
+            <h3>ESTRUCTURA DEL PDGA</h3>
+
+            <div class="row" style="padding-left: 2em; padding-right: 2em;">
+                <div class="col-md-12">
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-12 section-a temas">
+            <a id="temas"></a>
+            <h3 style="color: #666;">TEMAS CLAVE</h3>
+
+            <div class="row" style="padding-left: 2em; padding-right: 2em;">
+                <div class="col-md-12" style="margin-bottom: 50px; font-size: 16px;">
+                    EL PDGA al mismo tiempo está conformado por programas y subprogramas (Nro de programas), que abordan un conjunto de temas claves de las distintas etapas del proyecto y con diferente alcance territorial.
+                </div>
+
+                <img src="./images/pdga_temas.jpg" style="width: 100%; height: auto;" />
+
+            </div>
+        </div>
+        <div class="col-md-12 section-b responsabilidades">
+            <a id="responsabilidades"></a>
+            <h3 style="color: #666;">RESPONSABILIDADES Y SEGUIMIENTOS</h3>
+
+            <div class="row" style="padding-left: 2em; padding-right: 2em;">
+                <div class="col-md-6 parrafos">
+                    <p>
+                        Texto de discriminación de responsabilidades compromisos contractuales.
+                    </p>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </p>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </p>
+                </div>
+                <div class="col-md-6 parrafos">
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-12 section-a articulacion">
+            <a id="articulacion"></a>
+            <h3 style="color: #666;">Articulación institucional</h3>
+
+            <div class="row" style="padding-left: 2em; padding-right: 2em;">
+                <div class="col-md-12 parrafos">
+                    <p>
+                    Para llevar adelante el PDGA intervienen una diversidad de instituciones de investigación y profesionales especialistas  Instituciones involucradas en el PDGA  contratadas por el Estado Nacional o la UTE  según las responsabilidades asignadas en el ámbito del contrato.
+Convenios o acuerdos logrados , con qué instituciones (Comitente y UTE)  
+Especialidades involucradas,  asesores internacionales.
+                    </p>
+                </div>
+
+
+                <div class="section-footer">
+                    <div class="section-footer-buttons">
+                    <a href="#" class="section-footer-button">
+                            MEDIATECA
+                        </a>
+                        <a href="#" class="section-footer-button">
+                            GEOVISOR
+                        </a>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<script type='text/javascript'>
+$(document).ready(function() {
+    $('.section-sticky a').on('click', function() {
+        let selector = $(this).data('target');
+        $('html, body').animate({
+            scrollTop: $(selector).offset().top - 180
+        }, 500)
+    });
+
+    let target = $.urlParam('target');
+    if (target)
+        $('#link-' + target).trigger('click');
+
+    $('.section-footer-button2').hover( 
+        function () {
+        },
+        function () {
+        }
+    )
+
+});
+</script>
