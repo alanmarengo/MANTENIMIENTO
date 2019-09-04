7cc5ab6ff4488d3512df03f1c508813eec1d8e23
Juan Pablo <rosset.juanpablo@gmail.com>
2019-09-04 12:03:14 -0300

    ind grafico

--- js/indicadores.graficofn.js
+++ js/indicadores.graficofn.js
@@ -1,5 +1,31 @@
 function draw_grafico_11(container,config) { // PIE WITH DRILLDOWN
 
-	console.log(config);
+	Highcharts.chart('container', {
+		chart: {
+			type: 'pie'
+		},
+		title: {
+			text: config.titulo
+		},
+		subtitle: {
+			text: config.desc
+		},
+		plotOptions: {
+			series: {
+				dataLabels: {
+					enabled: true,
+					format: '{point.name}: {point.y:.1f}%'
+				}
+			}
+		},
+
+		tooltip: {
+			headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+			pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+		},
+
+		series:config.series
+		
+	});
 
 }
