8ec04ad5813a87848a6b26cb7667381a0884c249
Antoni Sanoguera <asanoguera@gmail.com>
2019-09-03 23:56:37 -0300

    test multi pagina

--- mediateca_find_pag.php
+++ mediateca_find_pag.php
@@ -68,109 +68,122 @@ $string_conn = "host=" . pg_server . " user=" . pg_user . " port=" . pg_portv . 
 	
 $conn = pg_connect($string_conn);
 
-$sol = 1;
+function getSQL($solapa) {
+	global $mode;
+	global $estudio_id;
+	global $qt;
+	global $desde;
+	global $hasta;
+	global $proyecto;
+	global $clase;
+	global $subclase;
+	global $tipo_doc;
+	global $ra;
 
-function getSQL($solap) {
-	$sol = $solap
-}
+	$SQL = "";
 
-if($mode==-1)
-{
-	getSQL(2)
-	if($sol == 2)
-		echo "// SARAZA"
-	else {
-		echo "// SARAZA NO FUNCIONO"
-	}
-
-
-	if (!IsSetVar($estudio_id))
+	if($mode==-1)
 	{
-
-	$SQL = "SELECT row_to_json(T)::text AS r FROM"
-        . "("
-        . "SELECT "
-        . "tipo_formato_solapa AS \"Solapa\","
-        . "origen_id,"
-        . "origen_id_especifico AS \"Id\","
-        . "recurso_titulo AS \"Titulo\","
-        . "recurso_desc AS \"Descripcion\","
-        . "recurso_path_url AS \"LinkImagen\","
-        . "recurso_categoria_desc AS \"MetaTag\","
-        . "CASE WHEN recurso_autores IS NULL THEN responsable::TEXT ELSE recurso_autores::TEXT END AS \"Autores\","
-        . "MAX(estudios_id) AS estudios_id,"
-        . "recurso_fecha AS Fecha,"
-        . "subclase_desc AS Tema"
-        . " FROM mod_mediateca.mediateca_find('$qt','$desde','$hasta','$proyecto','$clase','$subclase','$tipo_doc') "
-	. " WHERE tipo_formato_solapa=$solapa" 
-        . " GROUP BY recurso_fecha,subclase_desc,tipo_formato_solapa,origen_id,origen_id_especifico,recurso_titulo,recurso_desc,recurso_path_url,recurso_categoria_desc,CASE WHEN recurso_autores IS NULL THEN responsable::TEXT ELSE recurso_autores::TEXT END"
-	. " ORDER BY tipo_formato_solapa,recurso_titulo ASC"
-        . ")T";
+		if (!IsSetVar($estudio_id))
+		{
+	
+		$SQL = "SELECT row_to_json(T)::text AS r FROM"
+			. "("
+			. "SELECT "
+			. "tipo_formato_solapa AS \"Solapa\","
+			. "origen_id,"
+			. "origen_id_especifico AS \"Id\","
+			. "recurso_titulo AS \"Titulo\","
+			. "recurso_desc AS \"Descripcion\","
+			. "recurso_path_url AS \"LinkImagen\","
+			. "recurso_categoria_desc AS \"MetaTag\","
+			. "CASE WHEN recurso_autores IS NULL THEN responsable::TEXT ELSE recurso_autores::TEXT END AS \"Autores\","
+			. "MAX(estudios_id) AS estudios_id,"
+			. "recurso_fecha AS Fecha,"
+			. "subclase_desc AS Tema"
+			. " FROM mod_mediateca.mediateca_find('$qt','$desde','$hasta','$proyecto','$clase','$subclase','$tipo_doc') "
+		. " WHERE tipo_formato_solapa=$solapa" 
+			. " GROUP BY recurso_fecha,subclase_desc,tipo_formato_solapa,origen_id,origen_id_especifico,recurso_titulo,recurso_desc,recurso_path_url,recurso_categoria_desc,CASE WHEN recurso_autores IS NULL THEN responsable::TEXT ELSE recurso_autores::TEXT END"
+		. " ORDER BY tipo_formato_solapa,recurso_titulo ASC"
+			. ")T";
+		}
+		else
+		{
+			   if ((!IsSetVar($ra))&&($ra==1))
+			   {
+				$SQL = "SELECT row_to_json(T)::text AS r FROM"
+				. "("
+				. "SELECT "
+				. "tipo_formato_solapa AS \"Solapa\","
+				. "origen_id,"
+				. "origen_id_especifico AS \"Id\","
+				. "recurso_titulo AS \"Titulo\","
+				. "recurso_desc AS \"Descripcion\","
+				. "recurso_path_url AS \"LinkImagen\","
+				. "recurso_categoria_desc AS \"MetaTag\","
+				. "CASE WHEN recurso_autores IS NULL THEN responsable::TEXT ELSE recurso_autores::TEXT END AS \"Autores\","
+				. "estudios_id,"
+				. "recurso_fecha AS Fecha,"
+				. "subclase_desc AS Tema "
+				. " FROM mod_catalogo.vw_catalogo_data C WHERE "
+				. " C.estudios_id IN(SELECT sub_estudio_id FROM mod_catalogo.estudio_subestudio WHERE estudios_id=$estudio_id) "
+				. " AND C.estudios_id=$estudio_id  AND  tipo_formato_solapa=$solapa " /* Tambíen incluye el mismo estudio */ 
+				. " ORDER BY tipo_formato_solapa,recurso_titulo ASC "
+				. ")T";
+			   }
+			   else
+			   {
+			   $SQL = "SELECT row_to_json(T)::text AS r FROM"
+				. "("
+				. "SELECT "
+				. "tipo_formato_solapa AS \"Solapa\","
+				. "origen_id,"
+				. "origen_id_especifico AS \"Id\","
+				. "recurso_titulo AS \"Titulo\","
+				. "recurso_desc AS \"Descripcion\","
+				. "recurso_path_url AS \"LinkImagen\","
+				. "recurso_categoria_desc AS \"MetaTag\","
+				. "CASE WHEN recurso_autores IS NULL THEN responsable::TEXT ELSE recurso_autores::TEXT END AS \"Autores\","
+				. "estudios_id,"
+				. "recurso_fecha AS Fecha,"
+				. "subclase_desc AS Tema"
+				. " FROM mod_catalogo.vw_catalogo_data C WHERE estudios_id=$estudio_id AND tipo_formato_solapa=$solapa ORDER BY tipo_formato_solapa,recurso_titulo ASC"
+				. ")T";
+			   };
+		};
 	}
 	else
 	{
-   		if ((!IsSetVar($ra))&&($ra==1))
-   		{
-        	$SQL = "SELECT row_to_json(T)::text AS r FROM"
-        	. "("
-        	. "SELECT "
-        	. "tipo_formato_solapa AS \"Solapa\","
-        	. "origen_id,"
-        	. "origen_id_especifico AS \"Id\","
-        	. "recurso_titulo AS \"Titulo\","
-        	. "recurso_desc AS \"Descripcion\","
-        	. "recurso_path_url AS \"LinkImagen\","
-        	. "recurso_categoria_desc AS \"MetaTag\","
-        	. "CASE WHEN recurso_autores IS NULL THEN responsable::TEXT ELSE recurso_autores::TEXT END AS \"Autores\","
-        	. "estudios_id,"
-        	. "recurso_fecha AS Fecha,"
-        	. "subclase_desc AS Tema "
-        	. " FROM mod_catalogo.vw_catalogo_data C WHERE "
-        	. " C.estudios_id IN(SELECT sub_estudio_id FROM mod_catalogo.estudio_subestudio WHERE estudios_id=$estudio_id) "
-        	. " AND C.estudios_id=$estudio_id  AND  tipo_formato_solapa=$solapa " /* Tambíen incluye el mismo estudio */ 
-        	. " ORDER BY tipo_formato_solapa,recurso_titulo ASC "
-        	. ")T";
-   		}
-   		else
-   		{
-   		$SQL = "SELECT row_to_json(T)::text AS r FROM"
-        	. "("
-        	. "SELECT "
-        	. "tipo_formato_solapa AS \"Solapa\","
-        	. "origen_id,"
-        	. "origen_id_especifico AS \"Id\","
-        	. "recurso_titulo AS \"Titulo\","
-        	. "recurso_desc AS \"Descripcion\","
-        	. "recurso_path_url AS \"LinkImagen\","
-        	. "recurso_categoria_desc AS \"MetaTag\","
-        	. "CASE WHEN recurso_autores IS NULL THEN responsable::TEXT ELSE recurso_autores::TEXT END AS \"Autores\","
-        	. "estudios_id,"
-        	. "recurso_fecha AS Fecha,"
-        	. "subclase_desc AS Tema"
-        	. " FROM mod_catalogo.vw_catalogo_data C WHERE estudios_id=$estudio_id AND tipo_formato_solapa=$solapa ORDER BY tipo_formato_solapa,recurso_titulo ASC"
-        	. ")T";
-   		};
-	};
-}
-else
-{
-	switch ($mode)
-	{
-		case 0: 	$SQL = ""; die("Modo desconocido."); break;
-		case 1: 	$SQL = ""; die("Modo desconocido."); break;
-		case 2: 	$SQL = ""; die("Modo desconocido."); break;
-		default: 	$SQL = ""; die("Modo desconocido."); break;
+		switch ($mode)
+		{
+			case 0: 	$SQL = ""; die("Modo desconocido."); break;
+			case 1: 	$SQL = ""; die("Modo desconocido."); break;
+			case 2: 	$SQL = ""; die("Modo desconocido."); break;
+			default: 	$SQL = ""; die("Modo desconocido."); break;
+		};
 	};
 
-};
+	return $SQL;
+}
+
 
 /*
  * Data para paginador
  */
 
+ function getTotalRegistros($solapa) {
+	global $conn;
+
+	$data_query	= "SELECT COUNT(*) registros FROM (getSQL($solapa)) A;";
+	$recordset	= pg_query($conn,$data_query);
+	$row		= pg_fetch_row($recordset);
+	
+	return $row[0];  		
+ }
+
 //echo $SQL;
 
-$data_query	= "SELECT COUNT(*) registros FROM ($SQL) A;";
+$data_query	= "SELECT COUNT(*) registros FROM (getSQL($solapa)) A;";
 
 //echo $data_query;
 
@@ -187,17 +200,19 @@ $offset_pag 	= (($salto-1)*$pagina);
 $paginador_text = " LIMIT $salto OFFSET $offset_pag";
 
 //$SQL = $SQL." WHERE T.\"Solapa\"=$solapa";
-$SQL = $SQL.$paginador_text;
+//$SQL = $SQL.$paginador_text;
 
 echo "{"; // JSON - Inicio
 echo "	\"paginas\":$total_paginas,";
 echo "	\"solapa\": $solapa,";
 echo "	\"pagina\": $pagina,";
-echo "	\"registros_total\": $registros,";
+echo "	\"registros_total_0\": getTotalRegistros(0),";
+echo "	\"registros_total_1\": getTotalRegistros(1),";
+echo "	\"registros_total_2\": getTotalRegistros(2),";
 echo "	\"rec_per_page\": $salto,";
 echo "	\"recordset\":";
 
-$recordset = pg_query($conn,$SQL);
+$recordset = pg_query($conn,getSQL($solapa).$paginador_text);
 
 $fflag = false;
 $sflag = null; /* Solapa */
