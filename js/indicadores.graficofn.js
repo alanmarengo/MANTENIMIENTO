977f78dcdd57c5b11ad30a4aa184ddc289c006fa
Juan Pablo <rosset.juanpablo@gmail.com>
2019-09-05 10:43:48 -0300

    ind grafico

--- js/indicadores.graficofn.js
+++ js/indicadores.graficofn.js
@@ -1,3 +1,59 @@
+function draw_grafico_1(container,json) {
+	
+	Highcharts.chart('container', {
+		chart: {
+			type: 'area'
+		},
+		accessibility: {
+			description: config.desc
+		},
+		title: {
+			text: config.titulo
+		},
+		subtitle: {
+			text: config.desc
+		},
+		xAxis: {
+			allowDecimals: false,
+			labels: {
+				formatter: function () {
+					return this.value; // clean, unformatted number for year
+				}
+			}
+		},
+		yAxis: {
+			title: {
+				text: 'Nuclear weapon states'
+			},
+			labels: {
+				formatter: function () {
+					return this.value / 1000 + 'k';
+				}
+			}
+		},
+		tooltip: {
+			pointFormat: '{series.name} had stockpiled <b>{point.y:,.0f}</b><br/>warheads in {point.x}'
+		},
+		plotOptions: {
+			area: {
+				pointStart: 1940,
+				marker: {
+					enabled: false,
+					symbol: 'circle',
+					radius: 2,
+					states: {
+						hover: {
+							enabled: true
+						}
+					}
+				}
+			}
+		},
+		series:config.data
+	});
+	
+} //  BASIC AREA
+
 function draw_grafico_11(container,config) { // PIE WITH DRILLDOWN
 	
 	Highcharts.chart(container, {
