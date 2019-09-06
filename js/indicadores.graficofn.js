c23a5dd50e888a46cb2d5d1035432a70bcd938cc
Juan Pablo <rosset.juanpablo@gmail.com>
2019-09-06 10:44:44 -0300

    graficos

--- js/indicadores.graficofn.js
+++ js/indicadores.graficofn.js
@@ -69,7 +69,7 @@ function draw_grafico_2(container,config) {
 		yAxis: {
 			min: 0,
 			title: {
-				text: 'Total fruit consumption'
+				text: config.subtitle
 			}
 		},
 		legend: {
@@ -256,7 +256,86 @@ function draw_grafico_4(container,config) { // BUBBLE CHART
 		series:series
 	});
 
+}
 
+
+function draw_grafico_5(container,config) { // BUBBLE CHART
+	
+	var series = [];
+	var arrInd = -1;
+	var label = "";
+	var data = [];
+	
+	for (var i=0; i<config.etiquetas.length; i++) {
+		
+		if (label != config.etiquetas[i]) {
+			
+			arrInd++;
+			
+			series.push({
+				
+				name:config.etiquetas[i]
+				
+			});
+			
+			data[arrInd] = [];			
+			
+			label = config.etiquetas[i];
+			
+		}
+			
+		data[arrInd].push({
+			
+			name:config.data[i].name,
+			value:config.data[i].y
+			
+		});
+		
+	}
+	
+	for (var i=0; i<series.length; i++) {
+		
+		series[i].data = data[i];
+		
+	}
+	
+	
+	Highcharts.chart('container', {
+		chart: {
+			type: 'column'
+		},
+		title: {
+			text: 'Monthly Average Rainfall'
+		},
+		subtitle: {
+			text: 'Source: WorldClimate.com'
+		},
+		xAxis: {
+			categories: config.etiquetas,
+			crosshair: true
+		},
+		yAxis: {
+			min: 0,
+			title: {
+				text: 'Rainfall (mm)'
+			}
+		},
+		tooltip: {
+			headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+			pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+				'<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+			footerFormat: '</table>',
+			shared: true,
+			useHTML: true
+		},
+		plotOptions: {
+			column: {
+				pointPadding: 0.2,
+				borderWidth: 0
+			}
+		},
+		series:config.data
+	});
 }
 
 function draw_grafico_11(container,config) { // PIE WITH DRILLDOWN
