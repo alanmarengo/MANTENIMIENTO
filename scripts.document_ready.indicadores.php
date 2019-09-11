f00634e622aed6ff43ffb397af1457a6369974ab
Juan Pablo <rosset.juanpablo@gmail.com>
2019-09-11 14:08:11 -0300

    ficha botones

--- scripts.document_ready.indicadores.php
+++ scripts.document_ready.indicadores.php
@@ -1,6 +1,13 @@
 <script type="text/javascript">
 	
-	$(document).ready(function() {
+	$(document).ready(function() {		
+		
+		$("[title]").tooltipster({
+			animation: 'fade',
+			delay: 200,
+			theme: 'tooltipster-dark',
+			trigger: 'hover'
+		});
 		
 		indicadores = new ol_indicadores();
 		indicadores.panel.start();
@@ -40,8 +47,6 @@
 
 		window.addEventListener("resize",onresize);
 		onresize();
-		
-		$(".tooltip").tooltip();
 		
 	});
 			
