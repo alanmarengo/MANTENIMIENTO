56e482864d1fded118904f6635e8e6bec6e4a398
Juan Pablo <rosset.juanpablo@gmail.com>
2019-09-11 14:11:30 -0300

    ficha botones

--- scripts.indicadores.php
+++ scripts.indicadores.php
@@ -1,4 +1,14 @@
 <!-- STATS -->
 
+
+<link rel="stylesheet" href="./js/tooltipster/dist/css/tooltipster.bundle.min.css"/>
+<link rel="stylesheet" href="./js/tooltipster/dist/css/tooltipster-sideTip-shadow.min.css"/>
+
+
+
 <script src="./js/indicadores.js" type="text/javascript"></script>
 <script src="./js/indicadores.graficofn.js" type="text/javascript"></script>
+
+<!-- TOOLTIPSTER JS -->
+
+<script src="./js/tooltipster/dist/js/tooltipster.bundle.min.js" type="text/javascript"></script>
