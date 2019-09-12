4e15ea9c4f4681d881fd61170772a12f31a6d112
Juan Pablo <rosset.juanpablo@gmail.com>
2019-09-12 13:59:53 -0300

    ficha botones

--- php/get-stats-table-csv-indicador.php
+++ php/get-stats-table-csv-indicador.php
@@ -6,8 +6,11 @@ include("../tools.php");
 header("Content-Type: text/csv");
 header("Content-Disposition: attachment; filename=file.csv");
 
-$ind_id = $_POST["ind_id"];
-$pos = $_POST["pos"];
+//$ind_id = $_POST["ind_id"];
+//$pos = $_POST["pos"];
+
+$ind_id = 1;
+$pos = 4;
 
 $query_string = "SELECT tabla_fuente FROM mod_indicadores.vw_recursos WHERE ind_id = $ind_id AND posicion = $pos LIMIT 1";
 
@@ -16,7 +19,7 @@ $query = pg_query($conn,$query_string);
 $data = pg_fetch_assoc($query);
 
 $new_query_string = "SELECT * FROM " . $data["tabla_fuente"];
-
+echo $new_query_string;
 $query = pg_query($conn,$new_query_string);
 
 $csv = parseSQLToCSV($query);
