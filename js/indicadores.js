364be9b90100329896555c5837233d957cea4ce2
Juan Pablo <rosset.juanpablo@gmail.com>
2019-09-12 13:58:49 -0300

    ficha botones

--- js/indicadores.js
+++ js/indicadores.js
@@ -176,8 +176,36 @@ function ol_indicadores() {
 				fichaIcon.style.right = "45px";
 				fichaIcon.onclick = function() {
 					
-					jwindow.open("popup-fmetodologica");
-					this.loadFichaMetodologica(ind_id,pos);
+					var req = $.ajax({
+				
+						async:false,
+						data:{
+							ind_id:ind_id,
+							pos:pos
+						},
+						type:"POST",
+						url:"./php/get-stats-table-csv-indicador.php",
+						success:function(d){}
+						
+					});
+					
+					var blob = new Blob([req.responseText], { type: 'text/csv;charset=utf-8;' });
+					var filename = "webexport.csv";
+					if (navigator.msSaveBlob) { // IE 10+
+						navigator.msSaveBlob(blob, filename);
+					} else {
+						var link = document.createElement("a");
+						if (link.download !== undefined) { // feature detection
+							// Browsers that support HTML5 download attribute
+							var url = URL.createObjectURL(blob);
+							link.setAttribute("href", url);
+							link.setAttribute("download", filename);
+							link.style.visibility = 'hidden';
+							document.body.appendChild(link);
+							link.click();
+							document.body.removeChild(link);
+						}
+					}					
 					
 				}.bind(this);
 				
