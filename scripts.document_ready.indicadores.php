a91481f6d42b649ee939bddb78f0467379e42d59
Juan Pablo <rosset.juanpablo@gmail.com>
2019-09-12 21:38:35 -0300

    fixes}

--- scripts.document_ready.indicadores.php
+++ scripts.document_ready.indicadores.php
@@ -19,7 +19,7 @@
 		flotant.fitTopElement("#navbar-tools","#navbar-main");
 		flotant.fitTopElement(".page-container",".jump-navbar");
 		flotant.fit();		
-		flotant.fitHeightElement("#layers-wrapper",".bar-paneler");
+		flotant.fitHeightElement("#navbar-tools",".bar-paneler");
 		
 		jwindow = new Jump.window();
 		jwindow.initialize();
