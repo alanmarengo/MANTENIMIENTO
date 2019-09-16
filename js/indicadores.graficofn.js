6076bd44282987e42fe22f1e29b562fa0378ff2e
Juan Pablo <rosset.juanpablo@gmail.com>
2019-09-16 13:28:57 -0300

    fix print

--- js/indicadores.graficofn.js
+++ js/indicadores.graficofn.js
@@ -788,7 +788,8 @@ function draw_grafico_11(container,config) { // PIE WITH DRILLDOWN
 
 function draw_grafico_12(container,config) { // PIE WITH DRILLDOWN
 	
-	alert("GRAFICO 12");
 	container.innerHTML = "GRAFICO NUMERICO";
+	
+	console.log(container);
 
 }
