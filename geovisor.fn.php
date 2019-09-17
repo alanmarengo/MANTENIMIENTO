1ac80023883b86c983deaadf1d3be80340b26fa6
Juan Pablo <rosset.juanpablo@gmail.com>
2019-09-17 09:40:55 -0300

    fix print

--- geovisor.fn.php
+++ geovisor.fn.php
@@ -108,10 +108,10 @@ function DrawLayers($clase_id) {
 				<div class="pretty p-default p-curve p-toggle">
 					<input type="checkbox" class="layer-checkbox default-empty-checkbox" id="layer-checkbox-<?php echo $r["layer_id"]; ?>" data-lid="<?php echo $r["layer_id"]; ?>" data-cid="<?php echo $r["clase_id"]; ?>" data-added="0" data-layer="<?php echo $r["layer_wms_layer"]; ?>" data-wms="<?php echo $r["layer_wms_server"]; ?>" data-layer-type="<?php echo $r["tipo_layer_id"]; ?>"/>
 					<div class="state p-success p-on">
-						<i class="fa fa-eye"></i>
+						<i class="fa fa-eye-slash"></i>
 					</div>
 					<div class="state p-danger p-off">
-						<i class="fa fa-eye-slash"></i>
+						<i class="fa fa-eye"></i>
 					</div>
 				</div>
 				
