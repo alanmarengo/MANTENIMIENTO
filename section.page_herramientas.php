e3129514bf3b666491a402df8fddcfbb0ae70ce9
Antoni Sanoguera <asanoguera@gmail.com>
2019-09-17 23:13:46 -0300

    ajustes a herramientas

--- section.page_herramientas.php
+++ section.page_herramientas.php
@@ -21,29 +21,28 @@
                 <div class="col-md-4 page-box">
                     <a href="./geovisor.php" class="box">
                         <div class="page-box-image" style="background-image: url(./images/herramientas/3.png);"></div>
-                        <p class="page-box-title">GEOVISOR INTEGRAL</p>
-                        <p class="page-box-description">Visualizá, gestioná y accedé en línea a la totalidad de la información de este Observatorio y conectate e integrá información de otros organismos externos, tanto para consulta como para generar nueva información.</p>
-                    </a>
-                </div>
-                <div class="col-md-4 page-box">
-                    <a href="" class="box">
-                        <div class="page-box-image" style="background-image: url(./images/herramientas/4.png);"></div>
-                        <p class="page-box-title">GEOVISOR DE OBRA AHRSC</p>
-                        <p class="page-box-description">Visualizá y consultá las características técnicas de los proyectos de AHRSC, a través de la documentación ejecutiva y de los modelos tridimensionales con tecnología BIM que contiene toda la información técnica y física en una base de datos inteligente y dinámica.</p>
+                        <p class="page-box-title">GEOBI</p>
+                        <p class="page-box-description">Visualizá, gestioná y accedé en línea a la totalidad de la
+                            información de este Observatorio y conectate e integrá información de otros organismos
+                            externos, tanto para consulta como para generar nueva información.</p>
                     </a>
                 </div>
                 <div class="col-md-4 page-box">
                     <a href="http://hidrosantacruz.com.ar" class="box">
                         <div class="page-box-image" style="background-image: url(./images/herramientas/5.png);"></div>
-                        <p class="page-box-title">SIT</p>
-                        <p class="page-box-description">Accedé al geovisor del Sistema de Información Territorial de Santa Cruz (SIT) con información espacial pública de la provincia orientadas a la gestión y toma de decisiones.</p>
+                        <p class="page-box-title">SIG Santa Cruz</p>
+                        <p class="page-box-description">Accedé al geovisor del Sistema de Información Territorial de
+                            Santa Cruz (SIT) con información espacial pública de la provincia orientadas a la gestión y
+                            toma de decisiones.</p>
                     </a>
                 </div>
                 <div class="col-md-4 page-box">
                     <a href="" class="box">
                         <div class="page-box-image" style="background-image: url(./images/herramientas/6.png);"></div>
                         <p class="page-box-title">GEOVISOR AHRSC</p>
-                        <p class="page-box-description">Conocé los AHRSC,  sus características técnicas y toda la información ambiental de línea de base y monitoreos actuales. Indagá y generá tu propia información y vinculate a otros organismos externos para integrar nueva información.</p>
+                        <p class="page-box-description">Conocé los AHRSC, sus características técnicas y toda la
+                            información ambiental de línea de base y monitoreos actuales. Indagá y generá tu propia
+                            información y vinculate a otros organismos externos para integrar nueva información.</p>
                     </a>
                 </div>
             </div>
@@ -56,7 +55,10 @@
             <div class="row" style="padding-left: 20px; padding-right: 20px;">
                 <div class="col-md-12">
                     <p>
-                        A través de los siguientes geoservicios estándares visualizá, accedé y consultá la información espacial de IEASA. Estos geoservicios, podrán ser vinculados con diversas instancias de publicación, de acceso e intercambio de información que aplique estándares OGC (ISO 19111, 19115:2003).
+                        A través de los siguientes geoservicios estándares visualizá, accedé y consultá la información
+                        espacial de IEASA. Estos geoservicios, podrán ser vinculados con diversas instancias de
+                        publicación, de acceso e intercambio de información que aplique estándares OGC (ISO 19111,
+                        19115:2003).
                     </p>
                 </div>
 
@@ -64,30 +66,42 @@
                     <div class="geonetwork">
                         <a href="http://observatorio.ieasa.com.ar:8080/geonetwork" class="row">
                             <div class="col-md-4">
-                                <div class="page-box-image2" style="background-image: url(./images/herramientas/7.png);"></div>
+                                <div class="page-box-image2"
+                                    style="background-image: url(./images/herramientas/7.png);"></div>
                             </div>
                             <div class="col-md-8" style="padding: 20px;">
                                 <div style="padding-bottom: 10px; font-weight: bold;">GEONETWORK</div>
                                 <div>
-                                    Esta aplicación instalada en servidores de la empresa, de código abierto y diseñado para la catalogación de recursos georreferenciados, permite la búsqueda y acceso en múltiples formatos de la información geográfica y servicios de IEASA. Accedé a través del siguiente link.
+                                    Esta aplicación instalada en servidores de la empresa, de código abierto y diseñado
+                                    para la catalogación de recursos georreferenciados, permite la búsqueda y acceso en
+                                    múltiples formatos de la información geográfica y servicios de IEASA. Accedé a
+                                    través del siguiente link.
                                 </div>
                             </div>
                         </a>
                     </div>
                 </div>
-            
+
                 <div class="col-md-4 page-wbox">
                     <div class="wbox">
                         <p class="page-wbox-title">WMS</p>
                         <p class="page-wbox-link">
                             <div class="row">
-                                <div class="col-md-8">http://observatorio.ieasa.com.ar</div>
-                                <div class="col-md-4">
-                                    <a class="badge badge-secondary" style="color: #fff;">COPIAR</a>
+                                <div class="col-md-12">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control" readonly style="font-size: 12px;" value="http://observatorio.ieasa.com.ar:8080/geoserver/ows?">
+                                        <span class="input-group-btn">
+                                            <button class="clipboard-copy btn btn-default" style="color: #333;" data-clipboard-text="http://observatorio.ieasa.com.ar:8080/geoserver/ows?">
+                                                <i class="fa fa-clone"></i>
+                                            </button>
+                                        </span>
+                                    </div>
                                 </div>
                             </div>
                         </p>
-                        <p class="page-wbox-description">Copiá este link para usar en tu SIG o Cliente WEB, y visualizá información georreferenciada actualizada de la empresa en formato de imagen conocidos (GIF, JPG, etc.) </p>
+                        <p class="page-wbox-description">Copiá este link para usar en tu SIG o Cliente WEB, y visualizá
+                            información georreferenciada actualizada de la empresa en formato de imagen conocidos (GIF,
+                            JPG, etc.) </p>
                     </div>
                 </div>
                 <div class="col-md-4 page-wbox">
@@ -95,13 +109,22 @@
                         <p class="page-wbox-title">WFS</p>
                         <p class="page-wbox-link">
                             <div class="row">
-                                <div class="col-md-8">http://observatorio.ieasa.com.ar</div>
-                                <div class="col-md-4">
-                                    <a class="badge badge-secondary" style="color: #fff;">COPIAR</a>
+                                <div class="col-md-12">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control" readonly style="font-size: 12px;" value="http://observatorio.ieasa.com.ar:8080/geoserver/ows?">
+                                        <span class="input-group-btn">
+                                            <button class="clipboard-copy btn btn-default" style="color: #333;" data-clipboard-text="http://observatorio.ieasa.com.ar:8080/geoserver/ows?">
+                                                <i class="fa fa-clone"></i>
+                                            </button>
+                                        </span>
+                                    </div>
                                 </div>
                             </div>
                         </p>
-                        <p class="page-wbox-description">Copiá este link para acceder desde tu aplicación cliente SIG acceder a información georreferenciada. De esta manera se ponen a disposición los vectores y datos asociados de los distintos  proyectos en formatos vectoriales (Shapefile, geoJSON, GML, etc.)</p>
+                        <p class="page-wbox-description">Copiá este link para acceder desde tu aplicación cliente SIG
+                            acceder a información georreferenciada. De esta manera se ponen a disposición los vectores y
+                            datos asociados de los distintos proyectos en formatos vectoriales (Shapefile, geoJSON, GML,
+                            etc.)</p>
                     </div>
                 </div>
                 <div class="col-md-4 page-wbox">
@@ -109,13 +132,22 @@
                         <p class="page-wbox-title">CSW</p>
                         <p class="page-wbox-link">
                             <div class="row">
-                                <div class="col-md-8">http://observatorio.ieasa.com.ar</div>
-                                <div class="col-md-4">
-                                    <a class="badge badge-secondary" style="color: #fff;">COPIAR</a>
+                                <div class="col-md-12">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control" readonly style="font-size: 12px;" value="http://observatorio.ieasa.com.ar:8080/geonetwork/srv/spa/csw?">
+                                        <span class="input-group-btn">
+                                            <button class="clipboard-copy btn btn-default" style="color: #333;" data-clipboard-text="http://observatorio.ieasa.com.ar:8080/geonetwork/srv/spa/csw?">
+                                                <i class="fa fa-clone"></i>
+                                            </button>
+                                        </span>
+                                    </div>
                                 </div>
                             </div>
                         </p>
-                        <p class="page-wbox-description">Utilizá este link para acceder, a través de cliente SIG o aplicación de catalogación, a los metadatos de toda la información georreferenciada actualizada, buscar y consultar metadatos relacionados a datos, servicios presentes en esta plataforma.GML, etc.)</p>
+                        <p class="page-wbox-description">Utilizá este link para acceder, a través de cliente SIG o
+                            aplicación de catalogación, a los metadatos de toda la información georreferenciada
+                            actualizada, buscar y consultar metadatos relacionados a datos, servicios presentes en esta
+                            plataforma.GML, etc.)</p>
                     </div>
                 </div>
             </div>
@@ -123,8 +155,20 @@
     </div>
 </div>
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.4/clipboard.min.js"></script>
 <script type='text/javascript'>
 $(document).ready(function() {
+    var clipboard = new ClipboardJS('.clipboard-copy');
+
+    clipboard.on('success', function(e) {
+        //alert('Copiado!')
+
+/*         $('#my-other-tooltip')
+            .tooltipster({})
+            .tooltipster('content', 'My new content')
+            .tooltipster('open');
+ */    });
+
     $('.section-sticky a').on('click', function() {
         let selector = $(this).data('target');
         $('html, body').animate({
