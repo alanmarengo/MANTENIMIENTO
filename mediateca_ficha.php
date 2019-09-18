bccd73c4aea4888b007bb74a8684075f2aae1126
Martín Flores <martinmaximilianoflores@gmail.com>
2019-09-18 11:05:24 -0300

    mediateca_ficha agrega categoria_media

--- mediateca_ficha.php
+++ mediateca_ficha.php
@@ -14,6 +14,36 @@ $string_conn = "host=" . pg_server . " user=" . pg_user . " port=" . pg_portv . 
 	
 $conn = pg_connect($string_conn);
 
+/************************** CATEGORIA **********************************/
+/*
+	1 imagen 
+	2 video 
+	3 sonido
+* */
+
+$SQL_cat = "SELECT recurso_categoria_id FROM mod_catalogo.vw_catalogo_data WHERE origen_id=$origen_id AND origen_id_especifico=$id";
+
+$recordset = pg_query($conn,$SQL_cat);
+
+$row = pg_fetch_row($recordset);
+
+Switch($row[0])
+{
+	case 20: $cat = 1; break; //IMAGEN
+	case 39: $cat = 1; break; //IMAGEN
+	case 21: $cat = 2; break; //VIDEO
+	case 40: $cat = 2; break; //VIDEO
+	case 41: $cat = 2; break; //VIDEO
+	case 22: $cat = 2; break; //VIDEO
+	case 42: $cat = 3; break; //SONIDO
+	case 23: $cat = 3; break; //SONIDO
+	default: $cat = 1; break;
+	
+};
+
+
+/************************************************************/
+
 $SQL = "SELECT * FROM mod_mediateca.get_ficha_recurso($origen_id, $id) T limit 1";
 
 $recordset = pg_query($conn,$SQL);
@@ -35,7 +65,8 @@ echo "\"linkdescarga\":\"$row[8]\",";
 echo "\"fecha\":\"$row[9]\",";  
 echo "\"tema_subtema\":\"$row[10]\",";  
 echo "\"proyecto\":\"$row[11]\",";  
-echo "\"linkimagenes\":$row[12],";  
+echo "\"linkimagenes\":$row[12],";
+echo "\"categoria_media\":\"$cat\",";  
 echo "\"estudio_id\":\"$row[13]\""; 
 echo "}";
 /*
