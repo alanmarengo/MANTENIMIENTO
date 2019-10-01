40a18eb908b84251f60b6133e31fac4a72f0b3e1
Antoni Sanoguera <asanoguera@gmail.com>
2019-09-30 22:40:30 -0300

    ajustes sensor

--- section.index.php
+++ section.index.php
@@ -365,11 +365,14 @@
             let sensor = model.sensores[model.sensorIndex];
             let html = `
                 <div class='sensor-estacion'>${sensor.estacion}</div>
+                <div class='sensor-tipo'>${sensor.tipo}</div>
+                <div class='sensor-fecha'>Ultimo reg.: ${moment(sensor.ultima_act).format('DD [de] MMM [de] YYYY HH:mm:ss')}</div>
                 <div class='sensor-nombre'>${sensor.dato_nombre}</div>
                 <div class='sensor-dato'>${sensor.dato} <span class='sensor-unidad'>${sensor.unidad}</span></div>
-                <div class='sensor-min'>Min: <b>${sensor.minimo}</b></div>
-                <div class='sensor-med'>Med: <b>${sensor.media}</b></div>
-                <div class='sensor-max'>Max: <b>${sensor.maximo}</b></div>
+                <div class='sensor-min'>Min: <b>${sensor.minimo} *</b></div>
+                <div class='sensor-med'>Med: <b>${sensor.media} *</b></div>
+                <div class='sensor-max'>Max: <b>${sensor.maximo} *</b></div>
+                <div class='sensor-leyenda'>*Son valores medidos en los &uacute;ltimos 30 d&iacute;as</b></div>
             `
             $('#uxSensor').html(html);
 
