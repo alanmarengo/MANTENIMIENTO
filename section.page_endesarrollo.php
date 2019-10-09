0a73f753ebf126672a6529f5f4ecfe44cdfdd5f0
Antoni Sanoguera <asanoguera@gmail.com>
2019-10-09 20:03:35 -0300

    page_endesarrollo

---
+++ section.page_endesarrollo.php
@@ -0,0 +1,43 @@
+<link rel="stylesheet" type="text/css" href="./css/page_template.css" />
+
+<div id="page_endesarrollo" class="page page_template">
+    <div class="row">
+        <div class="col-md-12">
+            <p class="leyenda">
+                Estamos trabajando en el desarrollo de esta sección, que estará disponible próximamente.
+            </p>
+        </div>
+    </div>
+</div>
+
+<script type='text/javascript'>
+$(document).ready(function() {
+    $('.section-sticky a').on('click', function() {
+        let selector = $(this).data('target');
+        $('html, body').animate({
+            scrollTop: $(selector).offset().top - 200
+        }, 500)
+    });
+
+    let target = $.urlParam('target');
+    if (target)
+        $('#link-' + target).trigger('click');
+
+    $('body').on('click', '#uxCaminito', function(e) {
+        //TODO: LOAD DE JSON ARRAY BASADO EN CAMINITO
+        $('#uxFicha').modal('show');
+    });
+
+    $('.pop-button2').hover( 
+        function () {
+            let key=$(this).data('key');
+            $(this).css('background-image', 'url("./images/icono-' + key + '-relleno-hover.png")')
+        },
+        function () {
+            let key=$(this).data('key');
+            $(this).css('background-image', 'url("./images/icono-' + key + '-relleno.png")')
+        }
+    )
+
+});
+</script>
