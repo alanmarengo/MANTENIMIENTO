9096d7f421b7a23f762af4cf8e18fbda1042e872
Juan Pablo <rosset.juanpablo@gmail.com>
2019-10-14 16:09:12 -0300

    nuevo logo

--- html.nav.php
+++ html.nav.php
@@ -4,29 +4,47 @@
 			<a href="<?php echo startLink($_SERVER["SCRIPT_FILENAME"]); ?>" class="jump-link-death"><span>Inicio</span></a>
 		</li>
 		<li>
-			<a href="#">Acerca del Observatorio de proyectos IEASA</a>
+			<a href="#">Acerca del Observatorio de AHRSC</a>
 		</li>
 		<li>
 			<a href="javascript:void(0)" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onclick="flotant.toggle('#nav-herramientas-ieasa',true,false,this);">
-				Herramientas del Observatorio IEASA <i class="fa fa-angle-right nav-item-count"></i>
+				Herramientas del Observatorio <i class="fa fa-angle-right nav-item-count"></i>
 			</a>
 		</li>
 		<li>
-			<a href="#">Proyecto de Infraestructura Gasífera</a>
+			<a href="#">Proyecto - AHRSC</a>
 		</li>
 		<li>
-			<a href="javascript:void(0)" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onclick="flotant.toggle('#nav-aprovsc',true,false,this);">
-				Aprovechamientos Hidroeléctricos del Río Santa Cruz <i class="fa fa-angle-right nav-item-count"></i>
+			<a href="#">Plan de Gestión Ambiental - PGA</a>
+		</li>
+		<li>
+			<a href="#">Antecedentes y Marco Legal</a>
+		</li>
+		<li>
+			<a href="#">Participación Pública y Gestión Social</a>
+		</li>
+		<li>
+			<a href="#">Gestión Ambiental de Obra</a>
+		</li>
+		<li>
+			<a href="#">Patrimonio Natural y Cultural</a>
+		</li>
+		<li>
+			<a href="javascript:void(0)" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onclick="flotant.toggle('#nav-int-cuenca',true,false,this);">
+				Manejo Integrado de Cuenca <i class="fa fa-angle-right nav-item-count"></i>
 			</a>
 		</li>
 		<li>
-			<a href="./page_herramientas.php?target=geoservicios">Geoservicios</a>
+			<a href="#">Plan Gestión de Calidad</a>
+		</li>
+		<li>
+			<a href="#">Servicios Geográficos</a>
 		</li>
 		<li>
 			<a href="#">Datos Abiertos</a>
 		</li>
 		<li>
-			<a href="./contacto.php">Contacto</a>
+			<a href="#">Contacto</a>
 		</li>
 	</ul>
 </div>
--- html.nav.recursos_hidricos.php
+++ html.nav.recursos_hidricos.php
@@ -1,19 +1,16 @@
-<div id="nav-recursos-hidricos" data-visible="0" class="navmenu jump-flotant-nav jump-flotant-heightfill jump-flotant-heightfill-top jump-scroll nav-level-2 col col-nav col-xs-12 col-sm-12 col-md-3 col-lg-3">
+<div id="nav-int-cuenca" data-visible="0" class="navmenu jump-flotant-nav jump-flotant-heightfill jump-flotant-heightfill-top jump-scroll nav-level-2 col col-nav col-xs-12 col-sm-12 col-md-3 col-lg-3">
 	<ul>
 		<li>
-			<a href="#">Estadísticas</a>
+			<a href="#">Recursos Hídricos</a>
 		</li>
 		<li>
-			<a href="#">Monitoreo</a>
+			<a href="#">Riesgo</a>
 		</li>
 		<li>
-			<a href="#">EMAC (Estaciones de monitores ambiental costero)</a>
+			<a href="#">Desarrollo Regional</a>
 		</li>
 		<li>
-			<a href="#">Modelizaciones</a>
-		</li>
-		<li>
-			<a href="#">PLAHE</a>
+			<a href="#">Topografía y Geomorfología</a>
 		</li>
 	</ul>
 </div>
