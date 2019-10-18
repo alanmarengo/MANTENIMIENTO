f70b0f4eec5043d45b17a346de8629a7e90c4936
Antoni Sanoguera <asanoguera@gmail.com>
2019-10-18 13:00:56 -0300

    Agregado fecha inicial

--- section.index.php
+++ section.index.php
@@ -418,7 +418,8 @@ $(document).ready(function() {
         let html = `
                 <div class='sensor-estacion'>${sensor.estacion}</div>
                 <div class='sensor-tipo'>${sensor.tipo}</div>
-                <div class='sensor-fecha'>&Uacute;ltimo reg.: ${moment(sensor.ultima_act).format('DD [de] MMM [de] YYYY HH:mm:ss')}</div>
+                <div class='sensor-fecha'>Fecha inicial: ${moment(sensor.fecha_inicio).format('DD [de] MMM [de] YYYY HH:mm:ss')}</div>
+                <div class='sensor-fecha'>&Uacute;ltimo reg.: ${moment(sensor.fecha).format('DD [de] MMM [de] YYYY HH:mm:ss')}</div>
                 <div class='sensor-nombre'>${sensor.dato_nombre}</div>
                 <div class='sensor-dato'>${sensor.dato} <span class='sensor-unidad'>${sensor.unidad}</span></div>
                 <div class='sensor-min'>Min: <b>${sensor.minimo} *</b></div>
