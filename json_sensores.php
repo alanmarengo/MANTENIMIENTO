1713bca57528aa45a3f310972fe16989f8a3af64
Martín Flores <martinmaximilianoflores@gmail.com>
2019-10-22 13:27:57 -0300

    fecha inicial para sensores

--- json_sensores.php
+++ json_sensores.php
@@ -36,7 +36,7 @@ $conn = pg_connect($string_conn);
 
 //$conn = pg_connect("host=localhost port=5432 dbname=ahrsc user=postgres password=plahe100%");
 
-$SQL = "SELECT estacion, dato_nombre, dato, maximo, minimo, to_char(media::float, 'FM999999990.00') as media, ultima_act,unidad,tipo_sensor FROM mod_sensores.vw_sensores_data_index WHERE dato IS NOT NULL ORDER BY estacion ASC";
+$SQL = "SELECT estacion, dato_nombre, dato, maximo, minimo, to_char(media::float, 'FM999999990.00') as media, ultima_act,unidad,tipo_sensor,(ultima_act::date-30) AS fecha_menos_30 FROM mod_sensores.vw_sensores_data_index WHERE dato IS NOT NULL ORDER BY estacion ASC";
 
 $recordset = pg_query($conn,$SQL);
 
@@ -50,6 +50,7 @@ function draw_tupla($row)
        echo '"minimo":"'.$row[4].'",';
        echo '"media":"'.$row[5].'",';
        echo '"ultima_act":"'.$row[6].'",';
+       echo '"fecha_inicial":"'.$row[9].'",';
        echo '"unidad":"'.$row[7].'",';
        echo '"tipo":"'.$row[8].'"';
        echo '}';
