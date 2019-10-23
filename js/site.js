abbd116e03ddc9b52d8cc596f1c69227ad6bc551
Juan Pablo <rosset.juanpablo@gmail.com>
2019-10-23 15:04:26 -0300

    geovisor prev fix

--- js/site.js
+++ js/site.js
@@ -21,6 +21,24 @@ $(document).ready(function() {
 				$(flink).remove();
         }
     });
+
+    $('#main-search-btn').on('click', function(e) {
+        if (e.which == 13) {
+            //let url = "./mediateca.php?s=" + $(this).val();
+            //window.location.replace(url);
+			var pattern = $("#main-search").val();
+			var url = "./mediateca.php?s="+pattern;
+			var flink = document.createElement("a");
+				flink.href = url;
+				flink.target = "_blank";
+				document.body.appendChild(flink);
+				flink.click();
+				$(flink).remove();
+        }
+    });
+	
+	
+	
 });
 
 $.urlParam = function(name) {
