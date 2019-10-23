5419690218a2ee7e8e330bba8df88378a3f43056
Juan Pablo <rosset.juanpablo@gmail.com>
2019-10-23 12:10:54 -0300

    geovisor prev fix

---
+++ template-indicadores-empleo.php
@@ -0,0 +1,68 @@
+<div class="template-indicador-container" id="template-indicadores-gestion-pga">
+
+	<div class="row" >
+
+		<div class="resource-col col col-md-7 col-lg-7 col-xs-12 col-sm-12">
+		
+			<div class="resource-inner" id="indicador-col-pos-1" data-pos="1">
+			
+				
+			
+			</div>
+		
+		</div>
+
+		<div class="resource-col col col-md-5 col-lg-5 col-xs-12 col-sm-12">		
+		
+		
+			<div class="resource-inner" id="indicador-col-pos-2" data-pos="2">
+			
+				
+			
+			</div>
+		
+		</div>
+		
+	</div>
+	
+	<div class="row">
+
+		<div class="resource-col col col-md-5 col-lg-5 col-xs-12 col-sm-12">	
+		
+		
+			<div class="resource-inner" id="indicador-col-pos-3" data-pos="3">
+			
+				
+			
+			</div>
+		
+		</div>
+
+		<div class="resource-col col col-md-7 col-lg-7 col-xs-12 col-sm-12">
+		
+		
+			<div class="resource-inner" id="indicador-col-pos-4" data-pos="4">
+			
+				
+			
+			</div>
+		
+		</div>
+
+	</div>
+
+	<div class="row" >
+
+		<div class="resource-col col col-md-12 col-lg-12 col-xs-12 col-sm-12">		
+		
+			<div class="resource-inner" id="indicador-col-pos-5" data-pos="5">
+			
+				
+			
+			</div>
+		
+		</div>
+		
+	</div>
+	
+</div>
---
+++ template-indicadores-pga.php
@@ -0,0 +1,64 @@
+<div class="template-indicador-container" id="template-indicadores-gestion-pga">
+
+	<div class="row" >
+
+		<div class="resource-col col col-md-6 col-lg-6 col-xs-12 col-sm-12">
+		
+			<div class="resource-inner" id="indicador-col-pos-1" data-pos="1">
+			
+				
+			
+			</div>
+		
+		</div>
+
+		<div class="resource-col col col-md-6 col-lg-6 col-xs-12 col-sm-12">
+		
+			<div class="row">
+			
+				<div class="resource-col col col-md-12 col-lg-12 col-xs-12 col-sm-12">		
+		
+					<div class="resource-inner" id="indicador-col-pos-2" data-pos="2">
+					
+						
+					
+					</div>
+				
+				</div>
+			
+			</div>
+		
+			<div class="row">
+			
+				<div class="resource-col col col-md-12 col-lg-12 col-xs-12 col-sm-12">		
+		
+		
+					<div class="resource-inner" id="indicador-col-pos-3" data-pos="3">
+					
+						
+					
+					</div>
+				
+				</div>
+			
+			</div>
+		
+		</div>
+		
+	</div>
+	
+	<div class="row">		
+
+		<div class="resource-col col col-md-12 col-lg-12 col-xs-12 col-sm-12">			
+		
+			<div class="resource-inner" id="indicador-col-pos-4" data-pos="4">
+			
+				
+			
+			</div>
+		
+		</div>
+
+	</div>
+	
+</div>
