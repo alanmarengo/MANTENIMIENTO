413cd805b9592f3622b95cec0f369b332043af03
Antoni Sanoguera <asanoguera@gmail.com>
2019-10-23 23:40:00 -0300

    page acerca de

---
+++ section.page_acercade.php
@@ -0,0 +1,261 @@
+<link rel="stylesheet" type="text/css" href="./css/page_template.css" />
+
+<div id="page_acercade" class="page page_template">
+    <div class="row">
+        <div class="section-sticky">
+            <div class="col-md-12 page-title">
+                Acerca del Observatorio AHRSC
+            </div>
+
+            <div class="col-md-12 top-buttons">
+                <a href="#" data-target="#descripcion">Descripción</a>
+                <a href="#" data-target="#funcionalidades">FUNCIONALIDADES Y APLICACIONES</a>
+                <a href="#" data-target="#tratamiento">TRATAMIENTO DE LA INFORMACION</a>
+                <a href="#" data-target="#tecnologias">TECNOLOGIAS APLICADAS</a>
+            </div>
+        </div>
+
+        <div class="col-md-12">
+            <img src="./images/acercade_fondo.png" style="width: 100%; height: auto;" />
+        </div>
+
+        
+        <!---------------------------------------------->
+        <div class="col-md-12 section-a" style="padding-left: 8em; padding-right: 8em;">
+            <a id="descripcion"></a>
+            <h3>DESCRIPCIóN</h3>
+
+            <div class="row">
+                <div class="col-md-6 text-justify">
+El proyecto Aprovechamiento Hidroeléctrico del río Santa Cruz (AHRSC), es un emprendimiento hidroeléctrico estratégico para el desarrollo del país que se enmarca dentro de los preceptos de la energía renovable y se fundamenta bajo criterios de sostenibilidad.
+<br />
+IEASA, como organización pública que gestiona el proyecto, se compromete en un proceso de gestión del conocimiento abierto, de uso libre y gratuito, aportando visibilidad de sus acciones y acceso a la información para la aplicación en soluciones y toma de decisiones para el bien público.
+<br />
+En 2017 se presentó en Audiencia Pública en el Congreso de la Nación, prevista en la Ley Nº 23.879, el Estudio de Impacto Ambiental (EsIA) que permitió respaldar la factibilidad técnica, económica y ambiental del Proyecto. 
+<br />
+En ese marco, se instrumenta el Plan de Gestión Ambiental-PGA que identifica y organiza las recomendaciones del Estudio de Impacto Ambiental. Allí se instaura como compromiso el desarrollo de un Observatorio web para difundir la actuación, labor y la información de los planes y programas llevados a cabo. Posteriormente, este Observatorio,  se reestructura para contemplar una visión integral del proyecto hidroeléctrico y un entendimiento amplio  contribuyendo a la gestión de la sostenibilidad.
+<br />
+El Observatorio AHRSC es un espacio que reúne y articula herramientas basadas en conceptos propios de la gestión del conocimiento interdisciplinar, de la vigilancia estratégica y las redes de colaboración y uso compartido del conocimiento. Su estructura y componentes, concebidos como un sistema integral, permiten obtener una comprensión amplia de los procesos que hacen al estudio y construcción de una obra hidroeléctrica y a la ejecución de los compromisos ambientales en favor de la sociedad y de la naturaleza.
+<br />
+                </div>
+                <div class="col-md-6 text-justify">
+La gestión del conocimiento del proyecto se concibe como un proceso que va incrementando la escala de las intervenciones y del volumen de información. Se hace necesario promover una aplicación adaptada para las grandes escalas de información, contribuyendo a  facilitar su disponibilidad y el rápido acceso para su diligencia. Este es el caso de la generación de una suma de saberes que se acumula y que va adecuando en el tiempo los objetivos de su uso. La aplicación de la información se adapta a cada circunstancia en las etapas de construcción y de operación de un aprovechamiento hidroeléctrico. 
+<br />
+La gestión del AHRSC valoriza una gestión participativa del conocimiento. Es sensible al análisis y recopila, en forma sistemática y permanente, datos e información con el propósito de evaluar y seguir el complejo de acciones y procesos en torno a la construcción de un aprovechamiento hidroeléctrico. Esta actitud frente al conocimiento, se prolonga a su difusión pública, convirtiendo a la sociedad en el primer observador de esta gestión, y facilitando el medio para conocer los pareceres de los actores directa o indirectamente involucrados en su desarrollo.
+<br />
+El Observatorio, materializa un enfoque y una visión de la tutela y administración del proyecto AHRSC, manifiestando una clara posición a favor de la libre circulación de información.  Difunde el intercambio del conocimiento en red y lo capitaliza en la generación de nuevo conocimiento en apoyo a políticas especificas.
+<br />
+El Observatorio AHRSC desarrolla su capacidad con independencia en su tarea de investigación y análisis, y en la colaboración con órganos gubernamentales nacionales, provinciales, regionales y locales a los que destina el conocimiento para ser aplicados en políticas de desarrollo regional.
+<br />
+                </div>
+            </div>
+        </div>
+
+        
+        <!---------------------------------------------->
+        <div class="col-md-12 section-b cols4" style="padding-left: 8em; padding-right: 8em;">
+            <a id="funcionalidades"></a>
+            <h3 style="margin-bottom: 6px;">Funcionalidades y aplicaciones</h3>
+            <h4>El Observatorio AHRSC se asienta en cuatro aplicaciones centrales:</h4>
+            
+            <div class="row">
+                <div class="col-md-3" style="padding: 0px 10px 20px 0px;">
+                    <img src="./images/acercade_mediateca.png" style="width: 100%; height: auto;" />
+                    <h6>MEDIATECA</h6>
+                    <p>
+                        Es el centro de consulta de la información digital que permite la exploración por el laberinto del conocimiento donde se alberga todos los contenidos del Observatorio. Facilita la identificación de los recursos a través de un filtro personalizado y la visualización, consulta y descarga de todos los documentos: informes,noticias, fotografías, videos, planos, mapas temáticos, capas geográficas.
+                    </p>
+                </div>
+
+                <div class="col-md-3" style="text-align: center; padding: 0px 10px 20px 0px;">
+                    <img src="./images/acercade_geovisor.png" style="width: 100%; height: auto;" />
+                    <h6>Geovisor (GeObi)</h6>
+                    <p>
+                        Es la plataforma ofrece un sistema de información geográfica en línea, libre y gratuito.  Facilita la investigación y la participación de los usuarios en un entorno colaborativo en red. Promueve la indagación, producción, difusión y acceso de información espacial a través de su descarga. Las herramientas del GeObi conceden la selección, visualización, consulta, superposición de información y operaciones de análisis espacial.
+                    </p>
+                </div>
+
+                <div class="col-md-3" style="text-align: center; padding: 0px 10px 20px 0px;">
+                    <img src="./images/acercade_estadisticas.png" style="width: 100%; height: auto;" />
+                    <h6>Estadísticas</h6>
+                    <p>
+                        Esta aplicación posibilita en línea la realización de operaciones matemáticas y lógicas desde la base de datos, facilita la representación gráfica y mapeo de los resultados obtenidos, y acepta la visualización y descarga de los datasets.
+                    </p>
+                </div>
+
+                <div class="col-md-3" style="text-align: center; padding: 0px 10px 20px 0px;">
+                    <img src="./images/acercade_indicadores.png" style="width: 100%; height: auto;" />
+                    <h6>Indicadores</h6>
+                    <p>
+                        se implementa un sistema de indicadores de gestión e indicadores ambientales que pretende reflejar el proceso de generación información sobre fenómenos determinados. Los indicadores son considerados herramientas de gran valor para la evaluación del proceso de gestión del proyecto y las obras. Se visualizan diferentes paneles de indicadores categorizados temáticamente, relacionados con aspectos de gestión de los proyectos, del monitoreo de variables ambientales y de manejo de cuenca;
+                    </p>
+                </div>
+
+                <div class="col-md-6" style="padding: 0px 10px 20px 0px;">
+                    <p>
+                        Éstas aplicaciones son el “core” del Observatorio AHRSC. Desde cualquiera de ellas se puede navegar y acceder directamente a los recursos alojados en las demás. Funcionan como un sistema integral. Por caso, siendo el punto de partida el que elija el usuario, a través de Mediateca se buscan estudios, los cuales tienen información geográfica asociada con posibilidad de visualizarse en el Geovisor, a partir del cual se pueden consultar los Indicadores asociados a esa información y finalmente indagar los datos crudos que dan origen a esos indicadores en la aplicación Estadísticas. 
+                        <br />
+                        Para facilitar el acceso a la información, El Observatorio, cuenta con distintas páginas temáticas estructuradas para un acceso más directo a temas específicos y estratégicos de la gestión del proyecto. Se facilita el acceso a las aplicaciones con los contenidos relacionados ya filtrados. 
+                        <br />
+                    </p>
+                </div>
+
+                <div class="col-md-6" style="padding: 0px 10px 20px 0px;">
+                    <p>
+                        Cabe destacar que todos los recursos del sitio están catalogados en un motor de metadatos que ofrece la información detallada sobre la elaboración de cada uno, así brinda la posibilidad de reconocerla y obtenerla. A su vez, a través de servicios web, se ofrece toda la información geográfica adhiriendo a los protocolos de intercambio promovidos por la Infraestructura de datos espaciales de la Rep. Argentina (IDERA) y a los suscriptos por la Secretaria de Gobierno de Ambiente y Desarrollo Sustentable a través de la IDE Ambiental dependiente del Sistema de Información Ambiental Nacional - SIAN.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        
+        <!---------------------------------------------->
+        <div class="col-md-12 section-a">
+            <a id="tratamiento"></a>
+            <h3>Tratamiento de la información</h3>
+
+            <div class="row" style="padding-left: 2em; padding-right: 2em;">
+                <div class="col-md-6 parrafos">
+                    <p>
+                        El observatorio AHRSC se desarrolló a medida tomando como premisa generar una plataforma de código libre (open source) que se asiente en los estándares internacionales de publicación de datos y servicios para su visualización. 
+                        <br />
+                        <br />
+                        La plataforma integra investigaciones e informes generados por especialistas directamente vinculados al proyecto, además de aquellos de consultoría externa para temas específicos por su importancia estratégica, registros audiovisuales, información georreferenciada, noticias, encuestas, datos estadísticos, planos, datos de registro de fenómenos con sensores de medición en tiempo real, que se integraron y compatibilizaron en una base de datos para su acceso y tratamiento a los distintos niveles de información.                     
+                        <br />
+                        <br />
+                        <b>El elemento estructurador es el estudio-objeto, al que se vinculan e integran los recursos técnicos El estudio-objeto contiene los siguientes elementos:</b>
+                    </p>
+
+                    <img src="./images/bullet_blue.png" class="bullet" />
+                    <p class="bullet-parrafo">
+                        Disminución en los niveles del embalse de la presa Cóndor Cliff a los efectos de la desvinculación hidráulica en la operación de los Aprovechamientos del Río Santa Cruz con los niveles del Lago Argentino, asegurando la no afectación de los glaciares existentes en el mismo.
+                    </p>
+
+                    <img src="./images/bullet_blue.png" class="bullet" />
+                    <p class="bullet-parrafo">
+                        El o los documentos que aparecen como el producto parcial o final de un trabajo, 
+                    </p>
+
+                    <img src="./images/bullet_blue.png" class="bullet" />
+                    <p class="bullet-parrafo">
+                        La información geográfica que surge de los estudios documentados,
+                    </p>
+
+                    <img src="./images/bullet_blue.png" class="bullet" />
+                    <p class="bullet-parrafo">
+                        Los datos con y sin procesamiento relevados, 
+                    </p>
+
+                    <img src="./images/bullet_blue.png" class="bullet" />
+                    <p class="bullet-parrafo">
+                        Material audiovisual que se captura o genera en distintos momentos de los relevamientos,
+                    </p>
+
+                    <img src="./images/bullet_blue.png" class="bullet" />
+                    <p class="bullet-parrafo">
+                        Recursos técnicos específicos de los estudios como modelos, 
+                    </p>
+                </div>
+
+                <div class="col-md-6 parrafos">
+                    <p>
+                        Todos los documentos y recursos técnicos asociados tienen asignados una codificación para generar las relaciones entre ellos y permitir múltiples asociaciones. La codificación se aplicó según los siguientes criterios:
+                    </p>
+
+                    <img src="./images/bullet_blue.png" class="bullet" />
+                    <p class="bullet-parrafo2">
+                        Temporalidad
+                    </p>
+
+                    <img src="./images/bullet_blue.png" class="bullet" />
+                    <p class="bullet-parrafo2">
+                        Espacialidad
+                    </p>
+
+                    <img src="./images/bullet_blue.png" class="bullet" />
+                    <p class="bullet-parrafo2">
+                        Tema y subtema
+                    </p>
+
+                    <img src="./images/bullet_blue.png" class="bullet" />
+                    <p class="bullet-parrafo2">
+                        Matriz de asignación de responsables
+                    </p>
+
+                    <img src="./images/bullet_blue.png" class="bullet" />
+                    <p class="bullet-parrafo2">
+                        Autores e investigadores a cargo
+                    </p>
+
+                    <img src="./images/acercade_tratamiento.png" style="width: 100%; height: auto;" />
+
+                </div>
+            </div>
+        </div>
+
+        
+        <!---------------------------------------------->
+        <div class="col-md-12 section-b">
+            <a id="tecnologias"></a>
+            <h3 style="margin-bottom: 6px;">Tecnologías aplicadas</h3>
+            <h4>Descripción de cada componente</h4>
+            
+            <div class="row" style="padding-left: 2em; padding-right: 2em;">
+                <div class="col-md-6 parrafos">
+                    <h5>Base de datos espacial:</h5>
+                    <p>
+                        <b>- PostgresQL 9.2.24 version en Red Hat 4.8.5-28, modulo espacial PostGIS versión: 2.0.7:</b> Motor de base de datos relacional open source                     
+                    </p>
+
+                    <h5>Aplicaciones de Servidores web de mapas e información espacial:</h5>
+                    <p>
+                        <b>- GeoServer versión: 2.15.1:</b> Software open source que dispone de una interfaz para gestionar capas de información geográfica en el servidor y aplica protocolos de intercambio de la Open Geospatial Consortium (OGC): WMS, WFS, WCS y CSW.
+                        <br />
+                        <b>- MapServer:</b> Servidor de mapas con interface cgi-bin/mapscript (PHP, Python, Perl, Ruby, Java, y .NET) con soporte para las especificaciones Catálogo de Metadatos y aplica los estándares OGC.
+                        <br />
+                        <b>- GeoNetwork versión: 3.4.4.0:</b> Software catalogador de metadatos con capacidad de gestión en una interfaz web y almacenado en base de datos.                    
+                        <br />
+                    </p>
+
+                    <h5>Software de escritorio de sistemas de información Geográfica:</h5>
+                    <p>
+                        <b>- QGIS versión 3.4:</b>  Software open source que aplica estándares OGC y gestiona información vectorial y raster.
+                    </p>
+                </div>
+                <div class="col-md-6 parrafos">
+                    <img src="./images/acercade_tecnologias.png" style="padding-left: 40px; width: 80%; height: auto;" />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script type='text/javascript'>
+$(document).ready(function() {
+    $('.section-sticky a').on('click', function() {
+        $('.section-sticky a').removeClass('selected');
+        $(this).addClass('selected');
+
+        let selector = $(this).data('target');
+        $('html, body').animate({
+            scrollTop: $(selector).offset().top - 200
+        }, 500)
+    });
+
+    let target = $.urlParam('target');
+    if (target)
+        $('#link-' + target).trigger('click');
+
+    $('.section-footer-button2').hover( 
+        function () {
+            let key=$(this).data('key');
+            $(this).css('background-image', 'url("./images/icono-' + key + '-relleno-hover.png")')
+        },
+        function () {
+            let key=$(this).data('key');
+            $(this).css('background-image', 'url("./images/icono-' + key + '-relleno.png")')
+        }
+    )
+
+});
+</script>
