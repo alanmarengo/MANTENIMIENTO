dfecb59e72a91dbe25bd24e8368dd484e85cb5aa
Juan Pablo <rosset.juanpablo@gmail.com>
2019-10-24 11:46:32 -0300

    fixstats

--- php/get-layer-extent-mapeo.php
+++ php/get-layer-extent-mapeo.php
@@ -9,10 +9,10 @@ $string_conn = "host=" . pg_server . " user=" . pg_user . " port=" . pg_portv . 
 $conn = pg_connect($string_conn);
 
 $query_string = "SELECT 
-st_xmin(st_expand(st_extent(st_transform(T.geom, 3857)), 200::double precision)::box3d) AS minx,
-st_ymin(st_expand(st_extent(st_transform(T.geom, 3857)), 200::double precision)::box3d) AS miny,
-st_xmax(st_expand(st_extent(st_transform(T.geom, 3857)), 200::double precision)::box3d) AS maxx,
-st_ymax(st_expand(st_extent(st_transform(T.geom, 3857)), 200::double precision)::box3d) AS maxy
+st_xmin(st_expand(st_extent(st_transform(T.the_geom, 3857)), 200::double precision)::box3d) AS minx,
+st_ymin(st_expand(st_extent(st_transform(T.the_geom, 3857)), 200::double precision)::box3d) AS miny,
+st_xmax(st_expand(st_extent(st_transform(T.the_geom, 3857)), 200::double precision)::box3d) AS maxx,
+st_ymax(st_expand(st_extent(st_transform(T.the_geom, 3857)), 200::double precision)::box3d) AS maxy
 FROM mod_estadistica.get_mapeo($query_id) T";
 
 $extent = pg_fetch_assoc(pg_query($conn,$query_string));
