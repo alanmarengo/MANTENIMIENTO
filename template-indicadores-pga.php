22beb8e4fa0f41efcfd9d05198b1d69c0ac8ae58
Juan Pablo <rosset.juanpablo@gmail.com>
2019-10-25 17:11:32 -0300

    fix indicador

--- template-indicadores-pga.php
+++ template-indicadores-pga.php
@@ -14,9 +14,9 @@
 
 		<div class="resource-col col col-md-6 col-lg-6 col-xs-12 col-sm-12">
 		
-			<div class="row">
+			<div class="row jump-row">
 			
-				<div class="resource-col col col-md-12 col-lg-12 col-xs-12 col-sm-12">		
+				<div class="resource-col col col-md-12 col-lg-12 col-xs-12 col-sm-12 p0">		
 		
 					<div class="resource-inner" id="indicador-col-pos-2" data-pos="2">
 					
@@ -25,12 +25,8 @@
 					</div>
 				
 				</div>
-			
-			</div>
-		
-			<div class="row">
-			
-				<div class="resource-col col col-md-12 col-lg-12 col-xs-12 col-sm-12">		
+				
+				<div class="resource-col col col-md-12 col-lg-12 col-xs-12 col-sm-12 p0">		
 		
 		
 					<div class="resource-inner" id="indicador-col-pos-3" data-pos="3">
