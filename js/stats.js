afc43253d2c642fabdc02a588ab0f7373f7a025c
Juan Pablo <rosset.juanpablo@gmail.com>
2019-10-28 09:34:44 -0300

    fixstats

--- js/stats.js
+++ js/stats.js
@@ -6,11 +6,27 @@ function ol_stats() {
 	this.view = {};
 	this.dataset = {};
 		
-	$(".datepicker").datepicker({
+	$.datepicker.regional['es'] = {
+		closeText: 'Cerrar',
+		prevText: '< Ant',
+		nextText: 'Sig >',
+		currentText: 'Hoy',
+		monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+		monthNamesShort: ['Ene','Feb','Mar','Abr', 'May','Jun','Jul','Ago','Sep', 'Oct','Nov','Dic'],
+		dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+		dayNamesShort: ['Dom','Lun','Mar','Mié','Juv','Vie','Sáb'],
+		dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sá'],
+		weekHeader: 'Sm',
+		dateFormat: 'dd/mm/yy',
+		firstDay: 1,
+		isRTL: false,
+		showMonthAfterYear: false,
+		yearSuffix: ''
+	};
+		 
+	$.datepicker.setDefaults($.datepicker.regional['es']);
 		
-		dateFormat: 'dd/mm/yy'
-		
-	});
+	$(".datepicker").datepicker();
 	
 	this.panel.start = function() {
 		
