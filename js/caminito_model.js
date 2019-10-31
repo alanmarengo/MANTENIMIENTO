cb4d2837421731eb598a9a6c79fbfc630c619cd1
Antoni Sanoguera <asanoguera@gmail.com>
2019-10-31 00:30:26 -0300

    caminito model js

---
+++ js/caminito_model.js
@@ -0,0 +1,44 @@
+    var caminito = [{
+            id: 'aves',
+            titulo: 'Aves',
+            mode_id: 0,
+            texto: `
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.Quisque sit amet tincidunt sem.Etiam non augue diam.Donec at tempor ligula, ac rutrum felis.Nam molestie urna nec lacinia consectetur.Nulla egestas enim in velit interdum, sed pharetra diam convallis.Pellentesque sit amet neque in nibh finibus auctor.Aliquam eget elit sed odio sollicitudin pretium.In pharetra tristique sapien eu venenatis.Aliquam dictum, neque vitae pellentesque ornare, dolor sem fermentum risus, quis interdum diam libero nec urna.Duis nec magna vulputate lorem condimentum cursus.Praesent a lacus metus.Mauris venenatis felis eu euismod commodo.Aenean egestas eros eu sapien convallis semper.Integer nec velit erat.In maximus pellentesque turpis eu dignissim.
+		`
+        },
+        {
+            id: 'peces',
+            titulo: 'Peces',
+            mode_id: 0,
+            texto: `
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.Quisque sit amet tincidunt sem.Etiam non augue diam.Donec at tempor ligula, ac rutrum felis.Nam molestie urna nec lacinia consectetur.Nulla egestas enim in velit interdum, sed pharetra diam convallis.Pellentesque sit amet neque in nibh finibus auctor.Aliquam eget elit sed odio sollicitudin pretium.In pharetra tristique sapien eu venenatis.Aliquam dictum, neque vitae pellentesque ornare, dolor sem fermentum risus, quis interdum diam libero nec urna.Duis nec magna vulputate lorem condimentum cursus.Praesent a lacus metus.Mauris venenatis felis eu euismod commodo.Aenean egestas eros eu sapien convallis semper.Integer nec velit erat.In maximus pellentesque turpis eu dignissim.
+		`
+        },
+        {
+            id: 'humedales',
+            titulo: 'Humedales',
+            mode_id: 0,
+            texto: `
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.Quisque sit amet tincidunt sem.Etiam non augue diam.Donec at tempor ligula, ac rutrum felis.Nam molestie urna nec lacinia consectetur.Nulla egestas enim in velit interdum, sed pharetra diam convallis.Pellentesque sit amet neque in nibh finibus auctor.Aliquam eget elit sed odio sollicitudin pretium.In pharetra tristique sapien eu venenatis.Aliquam dictum, neque vitae pellentesque ornare, dolor sem fermentum risus, quis interdum diam libero nec urna.Duis nec magna vulputate lorem condimentum cursus.Praesent a lacus metus.Mauris venenatis felis eu euismod commodo.Aenean egestas eros eu sapien convallis semper.Integer nec velit erat.In maximus pellentesque turpis eu dignissim.
+		`
+        },
+        {
+            id: 'vegetacion',
+            titulo: '',
+            mode_id: 0,
+            texto: `
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.Quisque sit amet tincidunt sem.Etiam non augue diam.Donec at tempor ligula, ac rutrum felis.Nam molestie urna nec lacinia consectetur.Nulla egestas enim in velit interdum, sed pharetra diam convallis.Pellentesque sit amet neque in nibh finibus auctor.Aliquam eget elit sed odio sollicitudin pretium.In pharetra tristique sapien eu venenatis.Aliquam dictum, neque vitae pellentesque ornare, dolor sem fermentum risus, quis interdum diam libero nec urna.Duis nec magna vulputate lorem condimentum cursus.Praesent a lacus metus.Mauris venenatis felis eu euismod commodo.Aenean egestas eros eu sapien convallis semper.Integer nec velit erat.In maximus pellentesque turpis eu dignissim.
+		`
+        },
+        {
+            id: 'fauna',
+            titulo: 'Fauna silvestre',
+            mode_id: 0,
+            texto: `
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.Quisque sit amet tincidunt sem.Etiam non augue diam.Donec at tempor ligula, ac rutrum felis.Nam molestie urna nec lacinia consectetur.Nulla egestas enim in velit interdum, sed pharetra diam convallis.Pellentesque sit amet neque in nibh finibus auctor.Aliquam eget elit sed odio sollicitudin pretium.In pharetra tristique sapien eu venenatis.Aliquam dictum, neque vitae pellentesque ornare, dolor sem fermentum risus, quis interdum diam libero nec urna.Duis nec magna vulputate lorem condimentum cursus.Praesent a lacus metus.Mauris venenatis felis eu euismod commodo.Aenean egestas eros eu sapien convallis semper.Integer nec velit erat.In maximus pellentesque turpis eu dignissim.
+		`
+        },
+
+
+
+    ];
