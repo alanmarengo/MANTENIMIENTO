ddd30abe9ed0b42468b71846fd87886cfb7fb742
Juan Pablo <rosset.juanpablo@gmail.com>
2019-11-01 15:42:07 -0300

    fixstats

--- js/map.js
+++ js/map.js
@@ -150,7 +150,7 @@ function ol_map() {
 			
 				var iconStyle = new ol.style.Style({
 					image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
-					anchor: [0.5, 26],
+					anchor: [0,0],
 					anchorXUnits: 'fraction',
 					anchorYUnits: 'pixels',
 					opacity: 0.75,
