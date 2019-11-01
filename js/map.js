5bff60651ffd442783ee6398fc74ed5cb404e875
Juan Pablo <rosset.juanpablo@gmail.com>
2019-11-01 19:06:12 -0300

    relogin

--- js/map.js
+++ js/map.js
@@ -150,7 +150,7 @@ function ol_map() {
 			
 				var iconStyle = new ol.style.Style({
 					image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
-					anchor: [0.5, 0],
+					anchor: [0.5, 30],
 					anchorXUnits: 'fraction',
 					anchorYUnits: 'pixels',
 					opacity: 0.95,
