2dac70a852b71a6035391e1d2a0083eed7d3c810
Juan Pablo <rosset.juanpablo@gmail.com>
2019-11-01 15:40:55 -0300

    fixstats

--- js/map.js
+++ js/map.js
@@ -154,7 +154,8 @@ function ol_map() {
 					anchorXUnits: 'fraction',
 					anchorYUnits: 'pixels',
 					opacity: 0.75,
-					src: './images/icono-geovisores-br.png'
+					src: './images/icono-geovisores-br.png',
+					offset: [0, -50]
 					}))
 				});
 
