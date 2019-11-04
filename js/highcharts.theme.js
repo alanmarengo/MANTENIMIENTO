d25ad13aec64cfe4950fcec06c5775dd3eebd024
Juan Pablo <rosset.juanpablo@gmail.com>
2019-11-04 12:15:01 -0300

    fixstats

--- js/highcharts.theme.js
+++ js/highcharts.theme.js
@@ -1,23 +1,3 @@
-/* *
- *
- *  (c) 2010-2019 Torstein Honsi
- *
- *  License: www.highcharts.com/license
- *
- *  Grid-light theme for Highcharts JS
- *
- *  !!!!!!! SOURCE GETS TRANSPILED BY TYPESCRIPT. EDIT TS FILE ONLY. !!!!!!!
- *
- * */
-'use strict';
-/* global document */
-import Highcharts from '../parts/Globals.js';
-// Load the fonts
-Highcharts.createElement('link', {
-    href: 'https://fonts.googleapis.com/css?family=Dosis:400,600',
-    rel: 'stylesheet',
-    type: 'text/css'
-}, null, document.getElementsByTagName('head')[0]);
 Highcharts.theme = {
     colors: ['#7cb5ec', '#f7a35c', '#90ee7e', '#7798BF', '#aaeeee', '#ff0066',
         '#eeaaee', '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
--- scripts.highcharts.php
+++ scripts.highcharts.php
@@ -2,4 +2,5 @@
 <script src="https://code.highcharts.com/highcharts-more.js"></script>
 <script src="https://code.highcharts.com/modules/funnel.js"></script>
 <script src="https://code.highcharts.com/modules/exporting.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Dosis:400,600" rel="stylesheet">
 <script src="./js/highcharts.theme.js"></script>
