fc1d5caab2036f420f9b57bc6bbfc55b887b4b7e
Juan Pablo <rosset.juanpablo@gmail.com>
2019-11-04 12:11:29 -0300

    fixstats

--- scripts.highcharts.php
+++ scripts.highcharts.php
@@ -2,3 +2,4 @@
 <script src="https://code.highcharts.com/highcharts-more.js"></script>
 <script src="https://code.highcharts.com/modules/funnel.js"></script>
 <script src="https://code.highcharts.com/modules/exporting.js"></script>
+<script src="./js/highcharts.theme.js"></script>
