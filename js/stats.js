c03ab56e97cbca913f9926cc96a1caf71cede620
Juan Pablo <rosset.juanpablo@gmail.com>
2019-11-19 12:59:41 -0300

    bshd

--- js/stats.js
+++ js/stats.js
@@ -758,6 +758,12 @@ function ol_stats() {
 			});			
 			
 			$(".dataset-agroup").clone().attr("id","agroup-print").appendTo("#print-body");
+			
+			var agrupadoPor = $("#group-combo-view").text();
+			var fdesde = $("#dated-search").val();
+			var fhasta = $("#dateh-search").val();
+			
+			$("#print-body").append($("<p>Agrupado por: " + agrupadoPor + ". Período, Fecha Desde: " + fdesde + " - " + fhasta + "</p>"));
 			$("#dataset-header").clone().attr("id","dataset-header-print").appendTo("#print-body");
 			
 			document.getElementById("print-body").innerHTML += req.responseText;
