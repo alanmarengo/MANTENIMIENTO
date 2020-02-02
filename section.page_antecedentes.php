2a33cb6f95cb3ca4d72253f920ff9e3a3f16a2ba
Antoni Sanoguera <asanoguera@gmail.com>
2020-02-02 19:15:11 -0300

    nueva pagina de antecedentes

---
+++ section.page_antecedentes.php
@@ -0,0 +1,182 @@
+<link rel="stylesheet" type="text/css" href="./css/page_template.css" />
+
+<div id="page_antecedentes" class="page page_template">
+    <div class="row">
+        <div class="section-sticky">
+            <div class="col-md-12 page-title">
+                Antecedentes y Marco Legal
+            </div>
+
+            <div class="col-md-12 top-buttons">
+                <a id="link-historia" href="#" data-target="#historia">HISTORIA</a>
+                <a id="link-optimizacion" href="#" data-target="#optimizacion">OPTIMIZAC&Iacute;ON</a>
+                <a id="link-estudio" href="#" data-target="#estudio">ESTUDIO DE IMPACTO AMBIENTAL</a>
+                <a id="link-marcolegal" href="#" data-target="#marcolegal">MARCO LEGAL</a>
+            </div>
+        </div>
+
+        <div class="col-md-12 section-a historia">
+            <a id="historia"></a>
+            <h3>HISTORIA DEL PROYECTO</h3>
+
+            <p>Los Aprovechamientos Hidroeléctricos del río Santa Cruz (AHRSC) consisten en la construcción de las presas Cóndor Cliff (CC) y La Barrancosa (LB), con sus respectivas centrales de generación, sobre el río homónimo, que proveerán electricidad al Sistema Argentino de Interconexión (SADI).</p>
+            <p>El potencial energético del río Santa Cruz comenzó a estudiarse en la década de los 50 del siglo XX. En 1955, la empresa del estado Agua y Energía Eléctrica (AyEE) dispuso la habilitación de estaciones de aforo sobre el río, además de algunas estaciones meteorológicas en la zona, lo que hizo posible contar con más datos y establecer tendencias más confiables, a pesar de la discontinuidad en las mediciones.</p>
+            <p>A partir de 1975, AyEE comenzó a realizar estudios de prefactibilidad, incluyendo relevamientos geológicos y topográficos. Y en 1980 se realizaron investigaciones complementarias sobre perfiles geológicos de la zona de Cóndor Cliff.</p>
+            <p>Después de varias décadas –período en que el país discontinuó el plan de obras hidroeléctricas y se optó por la generación térmica como motor del crecimiento del parque generador–, en 2012 se licitaron las obras del río Santa Cruz y, paralelamente, se extendió la línea de trasmisión de alta tensión hasta la provincia con el objetivo de transportar la energía de las presas, además de permitir el trasporte de energía de los futuros parques eólicos de la región patagónica.</p>
+            <p>En agosto de 2012, la Secretaría de Obras Públicas del entonces Ministerio de Planificación Federal, Inversión Pública y Servicios de la Nación aprobó los pliegos licitatorios de la obra y se dispuso el llamado a Licitación Pública Nacional e Internacional para la contratación de los trabajos, bajo el régimen de la Ley 13064 y del Decreto 1381. Se presentaron cinco consorcios de empresas, adjudicándose la obra al constituido por las argentinas Electroingeniería e Hidrocuyo, y por la empresa asiática China Gezhouba Group Corporation, qu conformaron el consorcio Represas Patagonia (contratista).</p>
+            <p>A partir de 2016 se realizaron cambios sustanciales al proyecto, como la reducción de la cota en Cóndor Cliff y la reducción de turbinas en función de la modificación del diseño operativo en La Barrancosa. <a target="_blank" href="./media/Optimizacion-proyecto.pdf">VER OPTIMIZACIÓN</a></p>
+            <br />
+            <p>El 21 de diciembre de 2016, una medida cautelar dictaminada por la Corte Suprema de Justicia de la Nación (CSJN) suspendió la ejecución de las obras principales de los aprovechamientos (exceptuando las tareas preliminares del proyecto ejecutivo y obras complementarias), hasta que se implementase el proceso de evaluación de impacto ambiental y la audiencia pública prevista en la Ley Nacional 23879.</p>
+            <p>El Estudio cumplimentó lo solicitado en el mencionado dictamen de la Corte, conjuntamente con la realización de la Audiencia Pública en el marco del Congreso de la Nación, desarrollada el 20 y 21 de julio de 2017.</p>
+            <p>Producido el levantamiento de la medida cautelar de la Corte Suprema de Justicia de la Nación, el Comitente dio la orden de inicio en febrero de 2018, a partir de lo cual la Contratista pudo dar comienzo a la construcción de las obras principales, que están actualmente en ejecución. <a target="_blank" href="#">VER Proceso de Elaboración y Aprobación</a></p>
+        </div>
+
+        <div class="col-md-12 section-a optimizacion">
+            <a id="optimizacion"></a>
+            <h3>OPTIMIZACI&Oacute;N DEL PROYECTO</h3>
+
+            <div class="row">
+                <div class="col-md-6">
+                    <p>
+                        Los ajustes realizados al proyecto de los aprovechamientos hidroeléctricos del río Santa Cruz tienen dos premisas fundamentales desde el punto de vista ambiental, cuyo fin es garantizar la sostenibilidad sobre dos componentes principales del patrimonio natural durante toda la vida útil de las obras, incluido el periodo de llenado de los embalses:
+                        <br />
+                        <span class="bullet">•</span>Garantizar el desacople hidráulico del futuro embalse de la presa Cóndor Cliff respecto del lago Argentino.
+                        <br />
+                        <span class="bullet">•</span>Operar la central La Barrancosa de forma tal de preservar aguas abajo el régimen natural del río y del estuario.
+                    </p>
+                    <p>
+                        De esta manera, el proyecto logra su sostenibilidad en el tiempo, aprovechando una fuente de energía renovable para la generación eléctrica. 
+                    </p>
+                </div>
+                <div class="col-md-6">
+                    <p>
+                        Para esto, se han introducido las siguientes modificaciones al proyecto original:
+                        <br />
+                        <span class="bullet">•</span>Reducción en 2,40m, del nivel del embalse de Cóndor Cliff con el objetivo de garantizar el desacople hidráulico respecto al lago Argentino. Adicionalmente se ajustó el cálculo del oleaje. En este nuevo escenario, la altura de la presa en CC se bajó 6,50 m
+                        <br />
+                        <span class="bullet">•</span>La cantidad de turbinas totales para ambos aprovechamientos pasó de 11 a 8, motivado por el cambio de modo de operación de LB.
+                        <br />
+                        <span class="bullet">•</span>Se sextuplicó la capacidad de erogación de los descargadores de fondo, asegurando que aun durante el desvío del río, llenado del embalse y en épocas de aguas bajas se garantice la regularidad del ciclo hidrológico para la sostenibilidad ambiental.
+                    </p>
+                </div>
+            </div>
+
+            <br />
+            <h3 style="font-size: 14px;">OPERACI&Oacute;N DE "BASE" DE LA BARRANCOSA</h3>
+
+            <div class="row">
+                <div class="col-md-6">
+                    <p>La operación de “base” de una central hidroeléctrica consiste en un funcionamiento permanente de la misma, de modo tal que su embalse no cumple la función de reservar el agua para generar electricidad en los momentos de mayor demanda.</p> 
+                    <p>En el caso de las obras sobre el río Santa Cruz, la función de “base” la cumple la central La Barrancosa (situada aguas abajo de la presa Cóndor Cliff), que también tendrá un criterio de operación adicional: la central debe erogar el mismo caudal diario que ingresa al río Santa Cruz desde el lago Argentino. Por lo cual el tramo fluvial que quedará aguas abajo de La Barrancosa no tendrá las fluctuaciones típicas de los ríos con aprovechamientos hidroeléctricos, sino que respetará los caudales naturales del río.</p>
+                    <p>De esta manera, si nos ubicamos en cualquier punto aguas abajo de la presa La Barrancosa durante la etapa de operación (incluyendo la zona del estuario) el río se comportará de la misma forma que si ambas obras no existieran. Es decir que en esta zona el río tendrá las mismas fluctuaciones a lo largo del día y de las estaciones que tendría si las obras no hubieran sido construidas, evitando que se produzcan alteraciones hidrológicas.</p>
+                    <p>Desde el punto de vista energético, ello constituye una restricción a la generación, priorizando la preservación ambiental de la porción inferior del río, del estuario y su fauna. Por lo tanto, La Barrancosa podrá generar sólo la cantidad de energía que le permita el caudal medio del río.</p>
+                    <p>La reducción del número de turbinas en La Barrancosa, de 5 a 3, es una modificación que acompaña al nuevo régimen de operación de la central.</p> 
+                </div>
+                <div class="col-md-6">
+                    <p>
+                        <b>Reducción del nivel de embalse de la presa Cóndor Cliff</b>
+                        <img src="./images/base1.png" style="margin: 1em 0em 1em 0em; width: 90%; height: auto;" />
+
+                        <b>Operación de “base” de La Barrancosa</b>
+                        <img src="./images/base2.png" style="margin: 1em 0em 1em 0em; width: 90%; height: auto;" />
+                    </p> 
+                </div>
+                <div class="col-md-12 text-center">
+                    <a target="_blank" href="./media/Optimizacion-proyecto.pdf" class="boton">OPTIMIZACI&Oacute;N DEL PROYECTO – DESCRIPCI&Oacute;N T&Eacute;CNICA</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-12 section-a estudio">
+            <a id="estudio"></a>
+            <h3>ESTUDIO DE IMPACTO AMBIENTAL</h3>
+
+            <p>Los Aprovechamientos Hidroeléctricos del río Santa Cruz (AHRSC) consisten en la construcción de las presas Cóndor Cliff (CC) y La Barrancosa (LB), con sus respectivas centrales de generación, sobre el río homónimo, que proveerán electricidad al Sistema Argentino de Interconexión (SADI).</p>
+            <p>El potencial energético del río Santa Cruz comenzó a estudiarse en la década de los 50 del siglo XX. En 1955, la empresa del estado Agua y Energía Eléctrica (AyEE) dispuso la habilitación de estaciones de aforo sobre el río, además de algunas estaciones meteorológicas en la zona, lo que hizo posible contar con más datos y establecer tendencias más confiables, a pesar de la discontinuidad en las mediciones.</p>
+            <p>A partir de 1975, AyEE comenzó a realizar estudios de prefactibilidad, incluyendo relevamientos geológicos y topográficos. Y en 1980 se realizaronEl Estudio de Impacto Ambiental (EsIA) es parte del proceso de Evaluación de Impacto Ambiental (EIA) y proporciona una metodología sistemática para encarar la identificación y caracterización de las posibles alteraciones del ambiente y, de acuerdo con ese estudio, proponer modificaciones, alternativas o mitigaciones en pos de que el impacto negativo del proyecto sobre el ambiente sea el menor posible. A su vez, se identifican los impactos positivos para potenciar sus efectos. </p>
+            
+            <p style="font-weight: bolder;">Proceso de elaboración y aprobación</p>
+            <p>El EsIA de los aprovechamientos hidroeléctricos del río Santa Cruz fue elaborado por Ebisa –ahora denominada Integración Energética Argentina S.A., luego de su fusión con Enarsa– bajo la encomienda de la Secretaría de Energía Eléctrica, a partir de una medida cautelar de la Corte Suprema de Justicia de la Nación de diciembre de 2016. El estudio es el resultado de la recopilación, integración y revisión del EsIA de Santa Cruz, la consideración de los requerimientos del Dictamen Técnico de la provincia y de las Especificaciones Técnicas Ambientales (ETA) del Ministerio de Ambiente y Desarrollo Sustentable de la Nación (MAyDS), junto con otros estudios especiales realizados por el contratista y por expertos independientes, altamente calificados y con experiencia en los principales ejes temáticos del proyecto, que fueron contratados por Ebisa.</p>
+            <p>El enfoque metodológico adoptado en este Estudio está orientado por el marco conceptual del Manual de Gestión Ambiental de Obras Hidráulicas con Aprovechamiento Energético, según los requerimientos de la Ley 23.879 </p>
+            <p>El EsIA fue aprobado a nivel nacional por el Ministerio de Energía (actualmente con rango de Secretaría) y por el Ministerio de Ambiente y Desarrollo Sustentable (MAyDS) y, a nivel provincial, por el Ministerio de Producción, Comercio e Industria y por la Secretaría de Estado de Ambiente, dependiente del Ministerio de Salud y Ambiente de la provincia de Santa Cruz.</p>
+            <p>El Estudio cumplimentó lo solicitado en el mencionado dictamen de la Corte, conjuntamente con la realización de la Audiencia Pública en el marco del Congreso de la Nación, desarrollada el 20 y 21 de julio de 2017.</p>
+            <p>
+                Buenos Aires, 20 y 21 de julio de 2017
+                Congreso de la Nación
+            </p>
+        </div>
+
+        <div class="col-md-12 section-a antecedentes">
+            <h3>ESTUDIO ANTECEDENTES</h3>
+
+            <div class="row">
+                <div class="col-md-8">
+                    <div style="background-color: #ccc; width: 100%; height: 400px;"></div>
+                </div>
+                <div class="col-md-4">
+                    <p>
+                        El EsIA se basó sobre los siguientes documentos previos:
+                        <br />
+                        <span class="bullet">•</span>Estudio de Impacto Ambiental presentado por el contratista a la provincia de Santa Cruz en agosto de 2015.
+                        <br />
+                        <span class="bullet">•</span>Declaración de Impacto Ambiental (DIA) y Dictamen Técnico con requerimientos específicos emitidos por la Comisión Evaluadora de la provincia de Santa Cruz en diciembre de 2015.
+                        <br />
+                        <span class="bullet">•</span>Auditoría Ambiental de Cumplimiento presentada por el contratista para obtener la renovación de la Declaración de Impacto Ambiental de2017.
+                        <br />
+                        <span class="bullet">•</span>Actualización del EIA presentado a la provincia de Santa Cruz el 30 de marzo de 2017, que incluye la información relevada en campo desde agosto de 2015 a marzo 2017 y se aplica sobre el proyecto ejecutivo modificado.
+                        <br />
+                        <span class="bullet">•</span>Estudios complementarios propuestos por el MAyDS en agosto 2016 y realizados por el contratista en el periodo 2016-2017. 
+                    </p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="./images/time-line.png" style="" />
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-12 section-a marcolegal">
+            <a id="marcolegal"></a>
+            <h3>MARCO LEGAL</h3>
+
+            <div class="row">
+                <div class="col-md-6">
+                    <p>El Estudio de Impacto Ambiental incluyó un análisis jurídico enmarcado en el sistema federal, de acuerdo con las competencias de cada una de las jurisdicciones involucradas, que comprendió la normativa jurídica de los ámbitos internacional, nacional, provincial y municipal.</p>
+                    <p>Del ámbito internacional, se analizaron los acuerdos ratificados por la República Argentina que condicionan el ordenamiento jurídico interno y se utilizan de guía para la política ambiental.</p>
+                    <p>A nivel nacional, se consideraron los artículos de la Constitución Nacional, la Ley General del Ambiente, la participación ciudadana, el acceso a la información pública y los instrumentos procedimentales que tienen los ciudadanos para accionar. Se realizó un análisis de las exigencias ambientales del sector energético teniendo en cuenta la generación y distribución de energía. Se contempló la Ley de Obras Hidráulicas que establece un procedimiento para llevar a cabo la realización de los estudios ambientales en forma conjunta con las provincias.</p>
+                </div>
+                <div class="col-md-6">
+                    <p>Desde la órbita provincial, se consideró la evaluación de impacto ambiental, la ley de expropiación, los permisos, las autoridades de aplicación involucradas en la emisión de estos y toda la normativa municipal a cumplimentar.</p>
+                    <p>Asimismo, se contempló el nuevo paradigma en relación al régimen de responsabilidad por daño ambiental previsto por el Código Civil y Comercial de la Nación, en lo relevante para el proyecto.</p>
+                    <p>La evaluación jurídica se realizó desde un enfoque que permitió una interpretación integral, transversal y sistémica del marco normativo, a fin de considerar todos los aspectos ambientales y sectoriales de un proyecto de esta importancia.</p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script type='text/javascript'>
+$(document).ready(function() {
+    $('.section-sticky a').on('click', function() {
+        $('.section-sticky a').removeClass('selected');
+        $(this).addClass('selected');
+
+        let selector = $(this).data('target');
+        $('html, body').animate({
+            scrollTop: $(selector).offset().top - 200
+        }, 500)
+    });
+
+    let target = $.urlParam('target');
+    if (target)
+        $('#link-' + target).trigger('click');
+
+});
+</script>
