b9de94a1d0591077262ed5b5457b35675fe2542d
Juan Pablo <rosset.juanpablo@gmail.com>
2020-02-10 13:48:09 -0300

    pga capas

--- php/get-layer-info-pdga.php
+++ php/get-layer-info-pdga.php
@@ -5,10 +5,7 @@ include("../tools.php");
 
 $results = $_POST["results"];
 
-$layer_names = array();
-$layer_desc = array();
-$estudios_id = array();
-$gids = array();
+$layer_id_arr = array();
 
 $string_conn = "host=" . pg_server . " user=" . pg_user . " port=" . pg_portv . " password=" . pg_password . " dbname=" . pg_db;
 	
@@ -18,35 +15,24 @@ for ($i=0; $i<sizeof($results); $i++) {
 	
 	$sep = explode(";",$results[$i]);
 	
-	$qs_name = "SELECT layer_desc,cod_oficial FROM mod_geovisores.vw_layers WHERE layer_wms_layer = '" . $sep[0] . "' LIMIT 1";
+	$qs_name = "SELECT layer_id FROM mod_geovisores.vw_layers WHERE layer_wms_layer = '" . $sep[0] . "' LIMIT 1";
 	$qs_query = pg_query($conn,$qs_name);
 	$qs_name_data = pg_fetch_assoc($qs_query);
-	$layer_d = $qs_name_data["layer_desc"];
-	$cod_oficial = $qs_name_data["cod_oficial"];
+	$layer_id = $qs_name_data["layer_id"];
 	
-	array_push($layer_desc,$layer_d);
-	array_push($layer_names,$sep[0]);
-	array_push($estudios_id,$cod_oficial);
-	
-	if (!$gids[$sep[0]]) {
-		
-		$gids[$sep[0]] = array();
-		
-	}
-	
-	array_push($gids[$sep[0]],$sep[1]);
+	array_push($layer_id_arr,$layer_id);
 	
 }
 
-$layer_names = array_unique($layer_names);
+$layer_id = array_unique($layer_names);
 
-$layer_names = array_values($layer_names);
+$layer_id = array_values($layer_names);
 
 $html = "";
 
-for ($i=0; $i<sizeof($layer_names); $i++) {
+for ($i=0; $i<sizeof($layer_id); $i++) {
 	
-	$query_string = "SELECT DISTINCT layer_id FROM mod_geovisores.vw_layers WHERE layer_wms_layer = '" . $layer_names[$i] . "' LIMIT 1";
+	$query_string = "SELECT DISTINCT layer_id FROM mod_geovisores.vw_layers WHERE layer_id = '" . $layer_id[$i] . "' LIMIT 1";
 	
 	$query = pg_query($conn,$query_string);
 
