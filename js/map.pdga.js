83fce97fce5e24ebf8e3dfce9f9b7f5386744c08
Juan Pablo <rosset.juanpablo@gmail.com>
2020-02-10 13:16:14 -0300

    legend

--- js/map.pdga.js
+++ js/map.pdga.js
@@ -345,7 +345,7 @@ function ol_map() {
 						}
 						
 						map.gfiAddedLayers.push(layer.layer_id);
-						console.log(i + " :: " + (len+1));
+						console.log(i + " :: " + (len-1));
 						if (i==(len-1)) {
 		
 							this.parseGFI();
