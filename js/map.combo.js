beb532c4c1d9c3b535931f99cf85073bba28210f
Juan Pablo <rosset.juanpablo@gmail.com>
2020-02-11 12:08:44 -0300

    pga capas

--- js/map.combo.js
+++ js/map.combo.js
@@ -201,7 +201,7 @@ function ol_map() {
 								
 								}
 								
-								$("#popup-combo").css("left",(pixel[0]-200)+"px");
+								$("#popup-combo").css("left",(pixel[0]-210)+"px");
 								$("#popup-combo").css("top",(pixel[1]-300)+"px");
 
 							
