4dda9564095c1a654f5e946d9c8b32b05a8db44b
Juan Pablo <rosset.juanpablo@gmail.com>
2020-02-11 19:13:09 -0300

    responsive charts

--- js/indicadores.graficofn.js
+++ js/indicadores.graficofn.js
@@ -1091,8 +1091,8 @@ function draw_grafico_12(container,config) { // PIE WITH DRILLDOWN
 	
 	container = document.getElementById(container);
 	
-	container.innerHTML = "<h3 style='font-size:144px; margin:0; color:#0088cc;'>"+config.data[0].y + "</h3>";
-	container.innerHTML += "<h4 style='font-size:24px; margin:0;'>"+ config.unidad + "</h4>";
-	container.innerHTML += "<h5 style='font-size:18px; margin:10px 0;'>"+config.titulo+"</h5>";
+	container.innerHTML = "<h3 style='font-size:20vw; margin:0; color:#0088cc;'>"+config.data[0].y + "</h3>";
+	container.innerHTML += "<h4 style='font-size:5vw; margin:0;'>"+ config.unidad + "</h4>";
+	container.innerHTML += "<h5 style='font-size:5vw; margin:10px 0;'>"+config.titulo+"</h5>";
 
 }
