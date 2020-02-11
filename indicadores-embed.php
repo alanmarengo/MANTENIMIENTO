6e7fbaca176aebfbbf6d4ade92f0241467b92555
Juan Pablo <rosset.juanpablo@gmail.com>
2020-02-11 17:52:56 -0300

    indicadores embed

---
+++ indicadores-embed.php
@@ -0,0 +1,68 @@
+<?php include("pgconfig.php"); ?>
+<?php include("geovisor.fn.php"); ?>
+<?php include("indicadores.fn.php"); ?>
+<?php include("./fn.php"); ?>
+<?php include("./login.php"); ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+	<title>Indicadores</title>
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+	<?php include("./scripts.default-indicadores.php"); ?>	
+	<?php include("./scripts.indicadores.php"); ?>	
+	<?php include("./scripts.openlayers.php"); ?>	
+	<?php include("./scripts.highcharts.php"); ?>	
+	<?php include("./scripts.onresize.php"); ?>	
+	<?php include("./scripts.document_ready.indicadores.php"); ?>
+	
+	<?php// include("./scripts.default.php"); ?>
+	
+	
+	
+</head>
+<body style="overflow:hidden;">
+
+	<div style="display:none;">
+		<?php include("./html.panel-indicadores.php"); ?>
+	</div>
+	
+	<div id="page">
+	
+		<?php include("./html.navbar-main.php"); ?>
+		<?php include("./html.navbar-tools-indicadores.php"); ?>
+		
+		<div class="page-container">
+		
+			<?php //include("./section.index.php"); ?>
+			<div style="display:none;">
+				<?php include("./html.navs.php"); ?>
+				
+				<?php include("./html.nav.popup.php"); ?>
+			</div>
+			<div class="row jump-row">
+				
+				<div id="template-wrapper" class="jump-flotant-heightfill jump-scroll col-md-12 col-lg-12 col-sm-12 col-xs-12">
+				
+					
+				
+				</div>
+				
+			</div>
+			
+		</div>
+		
+	</div>
+	
+	<div class="jump-alert-modal"></div>
+	
+	<?php include("./popup.share.php"); ?>	
+	<?php include("./popup.fmetodologica.php"); ?>	
+	<?php // include("./widget-links.php"); ?>
+	<?php include("./html.jalert.php"); ?>
+
+</body>
+</html>
