675c7ce3ff08fbd4b92e61713f24026019fbe755
Juan Pablo <rosset.juanpablo@gmail.com>
2020-02-11 17:45:04 -0300

    graficos nuevos

---
+++ template-pdga-1.php
@@ -0,0 +1,27 @@
+<div class="template-indicador-container" id="template-indicadores-pdga-1">
+
+	<div class="row" >
+
+		<div class="resource-col col col-md-6 col-lg-6 col-xs-12 col-sm-12">
+		
+			<div class="resource-inner" id="indicador-col-pos-1" data-pos="1">
+			
+				
+			
+			</div>
+		
+		</div>
+
+		<div class="resource-col col col-md-6 col-lg-6 col-xs-12 col-sm-12">
+		
+			<div class="resource-inner" id="indicador-col-pos-2" data-pos="2">
+			
+				
+			
+			</div>
+		
+		</div>
+		
+	</div>
+	
+</div>
---
+++ template-pdga-2.php
@@ -0,0 +1,37 @@
+<div class="template-indicador-container" id="template-indicadores-pdga-1">
+
+	<div class="row" >
+
+		<div class="resource-col col col-md-4 col-lg-4 col-xs-12 col-sm-12">
+		
+			<div class="resource-inner" id="indicador-col-pos-1" data-pos="1">
+			
+				
+			
+			</div>
+		
+		</div>
+
+		<div class="resource-col col col-md-4 col-lg-4 col-xs-12 col-sm-12">
+		
+			<div class="resource-inner" id="indicador-col-pos-2" data-pos="2">
+			
+				
+			
+			</div>
+		
+		</div>
+
+		<div class="resource-col col col-md-4 col-lg-4 col-xs-12 col-sm-12">
+		
+			<div class="resource-inner" id="indicador-col-pos-3" data-pos="3">
+			
+				
+			
+			</div>
+		
+		</div>
+		
+	</div>
+	
+</div>
