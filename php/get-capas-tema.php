4c81833b3626810225b6e8b9c4c216a756c14d66
Juan Pablo Rosset <rosset.juanpablo@gmail.com>
2020-02-12 22:56:21 -0300

    dataextra json

---
+++ php/get-capas-tema.php
@@ -0,0 +1,46 @@
+<?php
+
+include("../pgconfig.php");
+
+$tema_id = $_POST["tema_id"];
+
+$layers_id = array();
+
+$string_conn = "host=" . pg_server . " user=" . pg_user . " port=" . pg_portv . " password=" . pg_password . " dbname=" . pg_db;
+	
+$conn = pg_connect($string_conn);
+
+$query_string = "SELECT * FROM mod_catalogo.temas_capas WHERE tema_id = " . $tema_id;
+
+$query = pg_query($conn,$query_string);
+
+while($r = pg_fetch_assoc($query)) {
+	
+	array_push($layers_id,$r["layer_id"]);
+	
+}
+
+$query_string = "SELECT layer_id,layer_desc,layer_wms_server,layer_wms_layer,layer_schema,layer_table FROM mod_geovisores.vw_layers WHERE layer_id IN(" . implode(",",$layers_id) . ")";
+
+$query = pg_query($conn,$query_string);
+
+$json = "{\"layers\":[";
+
+while ($data = pg_fetch_assoc($query)) {
+
+	$json .= "{";
+	$json .= "\"layer_id\":" . $data["layer_id"] . ",";
+	$json .= "\"layer_desc\":\"" . $data["layer_desc"] . "\",";
+	$json .= "\"layer_wms_server\":\"" . $data["layer_wms_server"] . "\",";
+	$json .= "\"layer_wms_layer\":\"" . $data["layer_wms_layer"] . "\",";
+	$json .= "\"layer_schema\":\"" . $data["layer_schema"] . "\",";
+	$json .= "\"layer_table\":\"" . $data["layer_table"] . "\"";
+	$json .= "},";
+
+}
+
+$json = substr($json,0,strlen($json)-1) . "]";
+
+echo $json;
+
+?>
