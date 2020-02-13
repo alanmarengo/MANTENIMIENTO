37f3d3470d33c8c7cb3ec0bb764fca7a4ec9aa14
Juan Pablo Rosset <rosset.juanpablo@gmail.com>
2020-02-13 00:52:29 -0300

    dataextra json

--- php/get-programas.php
+++ php/get-programas.php
@@ -24,8 +24,23 @@ $interCount = 0;
 $first = true;
 $pretsp = false;
 
-while($r = pg_fetch_assoc($query)) {
+while($r = pg_fetch_assoc($query)) {			
+		
+	$tema_json = "";
+	$query_tema_id_string = "SELECT tema_id,tema_nombre FROM mod_catalogo.temas WHERE tema_nombre IN('" . implode("','",explode(",",$r["temas"])) . "')";
+	$query_tema_id = pg_query($conn,$query_tema_id_string);
+		
+	while ($t = pg_fetch_assoc($query_tema_id)) {
+		
+		$tema_json .= "{";
+		$tema_json .= "\"id\":" . $t["tema_id"] . ",";
+		$tema_json .= "\"nombre\":\"" . $t["tema_nombre"] . "\"";
+		$tema_json .= "},";
+		
+	}
 	
+	$tema_json = substr($tema_json,0,strlen($tema_json)-1);
+		
 	if ($split_part != $r["split_part"]) {
 		
 		if ((!$first) && ($pretsp>1)) { $json = substr($json,0,strlen($json)-1); }
@@ -33,23 +48,6 @@ while($r = pg_fetch_assoc($query)) {
 		$progCount ++;
 		
 		$split_part = $r["split_part"];
-		
-		$query_tema_id_string = "SELECT tema_id,tema_nombre FROM mod_catalogo.temas WHERE tema_nombre IN('" . implode("','",explode(",",$r["temas"])) . "')";
-		echo $query_tema_id_string;
-		$query_tema_id = pg_query($conn,$query_tema_id_string);
-		
-		$tema_json = "";
-		
-		while ($t = pg_fetch_assoc($query_tema_id)) {
-			
-			$tema_json .= "{";
-			$tema_json .= "\"id\":" . $t["tema_id"] . ",";
-			$tema_json .= "\"nombre\":\"" . $t["tema_nombre"] . "\"";
-			$tema_json .= "},";
-			
-		}
-		
-		$tema_json = substr($tema_json,0,strlen($tema_json)-1);
 		
 		if (!$first) { $json .= "]},{"; } else { $json .= "{"; }
 		$json .= "\"id\":\"" . $r["id"] . "\",";
@@ -76,7 +74,7 @@ while($r = pg_fetch_assoc($query)) {
 		
 		$json .= "\"id\":\"" . $r["id"] . "\",";
 		$json .= "\"name\":\"" . $r["programa"] . "\",";
-		$json .= "\"temas\":[\"" . implode("\",\"",explode(",",$r["temas"])) . "\"],";
+		$json .= "\"temas\":[" . $tema_json . "],";
 		$json .= "\"data\":{";
 			$json .= "\"rubro\":\"" . $r["rubro"] . "\",";
 			$json .= "\"categoria\":\"" . $r["categoria"] . "\",";
