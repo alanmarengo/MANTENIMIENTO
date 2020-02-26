be4be750aa05449e05e300d418fa91d8fe04287e
Juan Pablo Rosset <rosset.juanpablo@gmail.com>
2020-02-26 19:55:41 -0300

    botones mini popup

--- js/map.pdga.visor.js
+++ js/map.pdga.visor.js
@@ -121,8 +121,6 @@ function ol_map(tema_id) {
 		
 		var js = JSON.parse(req.responseText);
 		
-		alert(js.geovisor_link);
-		console.log(document.getElementById("veg-btn"));
 		document.getElementById("veg-btn").href = js.geovisor_link;
 		
 		for (var i=0; i<js.layers.length; i++) {
