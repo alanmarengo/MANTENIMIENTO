400c549b3a0769994defc9b630a1250e26e34f51
Martín Flores <martinflores@atic.com.ar>
2020-05-03 15:58:26 -0300

    Agregar rec asociados a ficha de programas subprogramas

--- php/get-programas.php
+++ php/get-programas.php
@@ -102,7 +102,12 @@ while($r = pg_fetch_assoc($query)) {
 			$json .= "\"Responsable&nbsp;del&nbsp;estudio&nbsp;más&nbsp;reciente\":\"" . $r["respons_nom"] . "\",";
 			$json .= "\"Estado\":\"" . $r["estado"] . "\",";
 			$json .= "\"Descripción\":\"" . $r["descripcion"] . "\",";
-			$json .= "\"Recursos Asociados\":\"<a href='" . $r["recursos_asociados"] . "' target='_blank' ><img height='24' width='24' src='./images/icono-mediateca-relleno.png' alt='Ver recursos asociados'/> </a> \"";
+			
+
+			if ($r["recursos_asociados"]!='')
+			{
+				$json .= "\"Recursos Asociados\":\"<a href='" . $r["recursos_asociados"] . "' target='_blank' ><img height='24' width='24' src='./images/icono-mediateca-relleno.png' alt='Ver recursos asociados'/> </a> \"";
+			};
 			//$json .= "\"Rubro\":\"" . $sp["rubro"] . "\",";
 			//$json .= "\"Categoría\":\"" . $sp["categoria"] . "\",";
 			//$json .= "\"Etapa\":\"" . $sp["etapa"] . "\",";
