9911fa6c3a61ae18168a66a8a2ced6c653cd38d0
Juan Pablo Rosset <rosset.juanpablo@gmail.com>
2020-05-19 20:36:20 -0300

    ieasa titulo

--- section.page_geovisor_pdga_visor.php
+++ section.page_geovisor_pdga_visor.php
@@ -2,8 +2,8 @@
 
 <div class="row h100p">
 			
-	<!---------------------------------------------->
-	<div class="col-md-12 col-lg-12 section-b h100p p0">
+	<!----------------------sda------------------------>
+	<div class="col-md-12 col-lg-12 section-b h100p p0 jump-posrel">
 
 		<div id="map">
 		
@@ -14,6 +14,11 @@
 		<div class="jus-left row jump-row" id="btn-ver-geovisor" style="max-width:200px; display: flex; left:30px;">
 			<div style="width:200px;">
 				<a href="#" class="black-button" id="veg-btn" target="_blank">VER EN GEOVISOR</a>
+			</div>
+		</div>
+		<div class="jus-left row jump-row" style="position:absolute; top:20px; right:35px; max-width:350px; display: flex;">
+			<div style="width:350px;">
+				<span>Sitios de Observación y Medición del PDGA</span>
 			</div>
 		</div>
 		
