65b371ef51bd1d797cf28a0272a67c5580cb5171
Martín Flores <martinflores@atic.com.ar>
2020-05-22 12:06:14 -0300

    Olcultar columnas descarga datos gfi

--- tools.php
+++ tools.php
@@ -11,7 +11,7 @@ function parseSQLToCSV($recordset)
                         $fflag_delimitador = false;
  
                         $omitir_index = array();
-			$omitir_column = array("the_geom","geom","cod_","origen","id","fec_bbdd");
+			$omitir_column = array("the_geom","geom","cod_","origen","id","fec_bbdd","cod_estudio","cod_esia","cod_temp","cod_ob","cod_unsubclase");
 			
   			for ($index = 0; $index < $TotalColum; $index++) 
   			{
