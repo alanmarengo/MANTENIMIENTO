f22b71ae536c2f552d25c9486890474a029e511a
Martín Flores <martinflores@atic.com.ar>
2020-05-22 19:43:14 -0300

    http -> https

--- js/stats.js
+++ js/stats.js
@@ -1182,7 +1182,7 @@ function ol_stats() {
 						//'VERSION': '1.1.1',
 						'FORMAT': 'image/png',
 						'TILED': false,
-						'SLD':'http://'+window.location.hostname+'/sld/'+query_id+'.sld' /* EL SLD CREADO ES SIEMPRE EL ID_MAPEO.SLD */,
+						'SLD':'https://'+window.location.hostname+'/sld/'+query_id+'.sld' /* EL SLD CREADO ES SIEMPRE EL ID_MAPEO.SLD */,
 						'viewparams':'id:'+query_id
 					}
 				})
