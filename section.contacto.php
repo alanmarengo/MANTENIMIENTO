9ca05f237aabd55383ef247cd608ab8f8ca7f72f
Martín Flores <martinflores@atic.com.ar>
2020-05-29 11:40:23 -0300

    rechapach

--- section.contacto.php
+++ section.contacto.php
@@ -31,10 +31,7 @@
                                 data-sitekey="6LejbP0UAAAAAEtu681Yypx4F2kHrkQOpxl3JmhA" 
                                 data-callback='onSubmit' 
                                 data-action='submit'>Enviar</button>
-                       <small>This site is protected by reCAPTCHA and the Google 
-							<a href="https://policies.google.com/privacy">Privacy Policy</a> and
-							<a href="https://policies.google.com/terms">Terms of Service</a> apply.
-						</small>
+                       
 
                     </div>
                 </div>
