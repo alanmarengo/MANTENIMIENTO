d62b7a34bb2e72fa3c1366ee558ec2e201b87257
Martín Flores <martinflores@atic.com.ar>
2020-06-02 17:38:58 -0300

    iframe restrictions

--- scripts.document_ready.indicadores.php
+++ scripts.document_ready.indicadores.php
@@ -71,7 +71,7 @@
 		
 		indicadores.loadIndicador(<?php echo $_GET["ind_id"]; ?>,'<?php echo $_GET["t"]; ?>','<?php echo $_GET["cid"]; ?>');
 		
-		$('.layer-label').removeClass('layer-label-active'); 
+		//$('.layer-label').removeClass('layer-label-active'); 
 		//$(this).addClass('layer-label-active'); 
 		$('#nav-panel-arrow-a').trigger('click');"
 		
