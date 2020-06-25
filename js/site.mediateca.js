224ac3a3953db579db9cb17f289a283f07b74926
Martín Flores <martinflores@atic.com.ar>
2020-06-25 16:19:36 -0300

    editar link de pie

--- js/site.mediateca.js
+++ js/site.mediateca.js
@@ -721,7 +721,8 @@ $(document).ready(function() {
                     format: "dd/mm/yyyy",
                     language: "es",
                     autoclose: true,
-                    clearBtn: true,
+                    clearBtn: true,,
+                    todayHighlight:true,
                     toggleActive: true
                 });
 
