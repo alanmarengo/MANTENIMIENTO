7f9346300f9ea9ebcdb4702bd99041ea2f174eb7
Martín Flores <martinflores@atic.com.ar>
2020-06-28 20:06:09 -0300

    wms tools

--- php/wms_tools.php
+++ php/wms_tools.php
@@ -4,6 +4,9 @@ define("tools_wms_server",'https://observatorio.ieasa.com.ar/geoserver/ows?');
 
 function wms_get_layer_extent($str_layer_name)
 {
+	$index_layers = 0;/* Indice de capa, hay que buscar la capa $str_layer_name */
+	$index_boundind = 0; /* Indice de extent, solo aceptamos 4326 */
+
 	$wms_request = tools_wms_server."REQUEST=GetCapabilities&SERVICE=WMS";
 
 	$con = curl_init($wms_request);
@@ -21,9 +24,26 @@ function wms_get_layer_extent($str_layer_name)
 
 	$capacidades = simplexml_load_string($response);
 
-	echo $capacidades->Capability->Layer->Layer[0]->Name;
-	echo $capacidades->Capability->Layer->Layer[0]->BoundingBox[0]["CRS"]."<br>";
-	var_dump( $capacidades->Capability->Layer->Layer[0]->BoundingBox);
+	//echo $capacidades->Capability->Layer->Layer[0]->Name;
+	//echo $capacidades->Capability->Layer->Layer[0]->BoundingBox[0]["CRS"]."<br>";
+	//var_dump( $capacidades->Capability->Layer->Layer[0]->BoundingBox);
+
+	for($index_layers=0;$index_layers<sizeof($capacidades->Capability->Layer);$index_layers++)
+	{
+		if($capacidades->Capability->Layer->Layer[$index_layers]->Name==$str_layer_name)
+		{
+			for($index_boundind=0;$index_boundind<sizeof($capacidades->Capability->Layer[$index_layers]->BoundingBox);$index_boundind++)
+			{
+				if($capacidades->Capability->Layer[$index_layers]->BoundingBox[$index_boundind]["CRS"]=='EPSG:4326')
+				{
+					echo "maxx: ".$capacidades->Capability->Layer[$index_layers]->BoundingBox[$index_boundind]["maxx"];
+					echo "maxy: ".$capacidades->Capability->Layer[$index_layers]->BoundingBox[$index_boundind]["maxy"];
+					echo "minx: ".$capacidades->Capability->Layer[$index_layers]->BoundingBox[$index_boundind]["minx"];
+					echo "miny: ".$capacidades->Capability->Layer[$index_layers]->BoundingBox[$index_boundind]["miny"];
+				};	
+			};	
+		};	
+	};
 
 	return 0;
 };
