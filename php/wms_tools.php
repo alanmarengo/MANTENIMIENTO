c5d6c283db7c339a36968ad50be8a505f1fdeb08
Martín Flores <martinflores@atic.com.ar>
2020-06-28 18:58:44 -0300

    wms tools

--- php/wms_tools.php
+++ php/wms_tools.php
@@ -19,7 +19,7 @@ function wms_get_layer_extent($str_layer_name)
 
 	//echo $responde;
 
-	$capacidades = new SimpleXMLElement($response);
+	$capacidades = simplexml_load_string($response);
 
 	echo $capacidades->Layer[0]->Name;
 
