92108af82eba2dc25b42e21f03bdd1e1f5ce72b3
Martín Flores <martinflores@atic.com.ar>
2020-06-28 21:00:11 -0300

    Debuging extent wms

--- php/wms_tools.php
+++ php/wms_tools.php
@@ -24,9 +24,11 @@ function wms_get_layer_extent($str_layer_name)
 
 	for($index_layers=0;$index_layers<sizeof($capacidades->Capability->Layer);$index_layers++)
 	{
+		echo $capacidades->Capability->Layer->Layer[$index_layers]->Name;
+		
 		if($capacidades->Capability->Layer->Layer[$index_layers]->Name==$str_layer_name)
 		{
-			echo $capacidades->Capability->Layer->Layer[$index_layers]->Name;
+			
 			
 			for($index_boundind=0;$index_boundind<sizeof($capacidades->Capability->Layer->Layer[$index_layers]->BoundingBox);$index_boundind++)
 			{
