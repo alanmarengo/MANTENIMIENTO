2a3bf75960f42ea63e6bf2473691b26e188a5f71
Antoni Sanoguera <asanoguera@gmail.com>
2020-08-02 22:52:01 -0300

    page_participacion

---
+++ section.page_participacion.php
@@ -0,0 +1,190 @@
+<link rel="stylesheet" type="text/css" href="./css/page_template.css" />
+
+<div id="page_participacion" class="page page_template">
+    <div class="row">
+        <div class="section-sticky">
+            <div class="col-md-12 page-title">
+                Participaci&oacute;n p&uacute;blica y gesti&oacute;n social
+            </div>
+
+            <div class="col-md-12 top-buttons">
+                <a id="link-informacion" href="#" data-target="#informacion">ACCESO A LA INFORMACI&Oacute;N</a>
+                <a id="link-reuniones" href="#" data-target="#reuniones">REUNIONES CON LA COMUNIDAD</a>
+                <a id="link-pueblos" href="#" data-target="#pueblos">PUEBLOS ORIGINARIOS</a>
+                <a id="link-articulaciones" href="#" data-target="#articulaciones">ARTICULACIONES INSTITUCIONALES</a>
+                <a id="link-eventos" href="#" data-target="#eventos">LOCALIZACI&Oacute;N DE EVENTOS DE PARTICIPA..</a>
+            </div>
+        </div>
+
+        <!---------------------------------------------->
+        <div class="col-md-12 section-a" style="padding-left: 8em; padding-right: 8em;">
+            <a id="informacion"></a>
+            <h3 style="margin-bottom: 1em; color: #333;">ACCESO A LA INFORMACI&Oacute;N</h3>
+
+            <div class="row">
+                <div class="col-md-6 text-justify">
+                    Una de las características destacadas de los aprovechamientos hidroeléctricos del río Santa Cruz es el amplio acceso a la información del proyecto para todos los públicos. 
+                    <br />
+                    <br />
+                    Además de los instrumentos usuales, como el <a target="_blank" href="http://hidrosantacruz.com.ar/">sitio web</a>, las relaciones con la comunidad, la interacción con distintos actores públicos y privados del territorio y la información brindada a la prensa, entre otros, IEASA ha desarrollado una herramienta específica y muy potente para la gestión de la información: El <a target="_blank" href="https://observatorio.ieasa.com.ar/page_acercade.php">Observatorio AHRSC</a>, una plataforma web multidimensional que integra la información de los aspectos técnicos de la obra y de las disciplinas ambientales involucradas en el proceso, aportando además herramientas para el tratamiento y manejo de la información.
+                    <br />
+                    <br />
+                    La plataforma es un instrumento moderno de apoyo a la gestión del proyecto. Organiza y resguarda la información, facilita su seguimiento y control y permite realizar análisis integrales y multidimensionales para la generación de nuevo conocimiento. A través de esta plataforma se fortalece la difusión de las acciones y resultados vinculados al proyecto y la generación de redes colaborativas con personas e instituciones.
+                    <br />
+                    <br />
+                </div>
+                <div class="col-md-6 text-justify">
+                    La plataforma integra investigaciones e informes generados por los especialistas de las distintas temáticas, consultores externos vinculados con el proyecto, incluyendo imágenes, registros audiovisuales, información georreferenciada, noticias, datos estadísticos, planos, imágenes satelitales, modelos, datos de registro de fenómenos con sensores de medición en tiempo real, entre otros. Toda estos recursos hacen a la base de datos del Proyecto estructurada  bajo ciertos protocolos que vuelve más eficiente la administración de la información y facilita su acceso a través de la web.
+                    <br />
+                    <br />
+                    El Observatorio es una poderosa herramienta de acceso a la información, que concreta el compromiso de IEASA en la transparencia de la gestión y en la promoción de conocimiento en forma abierta, libre y gratuita, como base para los procesos participativos de toma de decisiones. 
+                    <br />
+                    <br />
+                </div>
+            </div>
+        </div>
+
+        <!---------------------------------------------->
+        <div class="col-md-12 section-b">
+            <a id="reuniones"></a>
+            <h3 style="margin-bottom: 1em; color: #333;">REUNIONES CON LA COMUNIDAD</h3>
+
+            <div class="row">
+                <div class="col-md-6 text-justify">
+                    La interacción del proyecto con diferentes actores en la sociedad, tanto estatales como privados o de la sociedad civil es constante. 
+                    <br />
+                    <br />
+                    Para el tratamiento de temas o inquietudes particulares el proyecto realiza encuentros abiertos con la comunidad, en locaciones específicas. 
+                    <br />
+                    <br />
+                 </div>
+                <div class="col-md-6 text-justify">
+                    Dentro de los encuentros abiertos a la participación pública se consideran las instancias formales previstas por normas nacionales o provinciales, como las Audiencias Públicas.
+                    <br />
+                    <br />
+                    <a id="link-visor1" href="#" data-target="#eventos" data-url="#">Ver la localización de las reuniones con la comunidad.</a>
+                    <br />
+                    <a target="_blank" href="#">Acceda al listado de reuniones con la comunidad.</a>
+                    <br />
+                </div>
+            </div>
+
+            {{SLIDE_1}}
+
+        </div>
+
+        <!---------------------------------------------->
+        <div class="col-md-12 section-a">
+            <a id="pueblos"></a>
+            <h3 style="margin-bottom: 1em; color: #333;">PUEBLOS ORIGINARIOS</h3>
+
+            <div class="row">
+                <div class="col-md-6 text-justify">
+                    La Constitución Nacional reconoce la preexistencia étnica y cultural de los pueblos indígenas argentinos y reconoce la personería jurídica de las comunidades. A su vez, el Convenio 169 de la Organización Internacional del Trabajo sobre Pueblos Indígenas (Ley Nacional 24.017) contempla la consulta a los pueblos interesados cada vez que se prevén medidas legislativas o administrativas susceptibles de afectarles directamente.
+                    <br />
+                    <br />
+                    A partir de una sentencia judicial del 20 de octubre de 2017 se constituye la mesa de diálogo para hacer efectivo el proceso de consulta para los aprovechamientos hidroeléctricos del río Santa Cruz, en la que participan las 15 comunidades de pueblos originarios de la provincia, los Estados Nacional y Provincial, el Instituto Nacional de Asuntos Indígenas y la empresa contratista. 
+                    <br />
+                    <br />
+                </div>
+                <div class="col-md-6 text-justify">
+                    En ese marco, se realizan reuniones plenarias con diferente periodicidad, reuniones de las comunidades con sus asesores, se firmaron convenios de cooperación y se llevan adelante actividades conjuntas, en un proceso abierto que tiene como objetivo construir una mirada intercultural sobre el proyecto, la naturaleza y las ideas de desarrollo, y el reconocimiento de los derechos indígenas. 
+                    <br />
+                    <br />
+                    <a id="link-visor2" href="#" data-target="#eventos" data-url="#">Ver localización de las comunidades originarias.</a>
+                    <br />
+                    <a target="_blank" href="#">Acceda al listado de reuniones con las comunidades originarias.</a>
+                    <br />
+                </div>
+            </div>
+
+            {{SLIDE_2}}
+
+        </div>
+
+        <!---------------------------------------------->
+        <div class="col-md-12 section-b">
+            <a id="articulaciones"></a>
+            <h3 style="margin-bottom: 1em; color: #333;">ARTICULACIONES INSTITUCIONALES</h3>
+
+            <div class="row">
+                <div class="col-md-6 text-justify">
+                    Dala la importancia, la escala y la complejidad del proyecto, los Aprovechamientos Hidroeléctricos del Río Santa Cruz se desarrollan en un contexto de articulación con diversas instituciones, empresas y actores a nivel local, provincial, nacional e internacional.
+                    <br />
+                    <br />
+                    Esta dimensión global del proyecto es propia de los emprendimientos de estas características, aunque suele quedar oculta por el hecho de que las obras se ejecutan en un espacio acotado del territorio. 
+                    <br />
+                    <br />
+                    En primer lugar, esta articulación se verifica en las competencias legales e institucionales más inmediatas: a) La Nación, a través de IEASA, como comitente del proyecto; b) La Provincia de Santa Cruz, como titular de los recursos naturales y de la gestión territorial y ambiental; c) La Inspección del proyecto; d) La Unión Transitoria de Empresas (UTE) Represas Patagonia, como contratista de las obras. 
+                    <br />
+                    <br />
+                    Las articulaciones que sustentan la ejecución del proyecto operan también en diversos niveles. Además de los indicados, podemos mencionar: 
+                    <br />
+                    <br />
+                </div>
+                <div class="col-md-6 text-justify">
+                    - La provisión de tecnología, conocimiento y experiencias de gestión de diversas industrias y expertos de todo el mundo; 
+                    <br />
+                    - La gestión de programas ambientales con la participación del sistema científico argentino y de expertos internacionales; 
+                    <br />
+                    - La provisión de insumos, materiales y tecnologías para la obra de distintos puntos del país, incluyendo la propia provincia de Santa Cruz;
+                    <br />
+                    - La interacción con organismos y agencias nacionales con competencia en el área de proyecto o en temas relacionados; 
+                    <br />
+                    - El otorgamiento de permisos para gestionar distintos aspectos de la obra o del Plan de Gestión Ambiental por parte de organismos nacionales y provinciales. 
+                    <br />
+                    - La articulación con agencias y organismos provinciales para el monitoreo de diversas variables sociales y ambientales. 
+                    <br />
+                    - La interacción –ya sea a nivel de espacios de diálogo o en ámbito judicial con actores u organizaciones que cuestionan el proyecto o alguno de sus aspectos. 
+                    <br />
+                    - La relación con instituciones encargadas de la gestión de la energía, el agua, la seguridad de presas y la gestión de riesgos. 
+                    <br />
+                    <br />
+                    <a id="link-visor3" href="#" data-target="#eventos" data-url="#">Ver la localización de las instituciones vinculadas al Proyecto.</a>
+                    <br />
+                    <a target="_blank" href="#">Acceda al tablero de Indicadores.</a>
+                    <br />
+                </div>
+            </div>
+        </div>
+
+        <!---------------------------------------------->
+        <div class="col-md-12 section-a" style="padding-bottom: 40px;">
+            <a id="eventos"></a>
+            <h3 style="margin-bottom: 1em; color: #333;">LOCALIZACI&Oacute;N DE EVENTOS DE PARTICIPACI&Oacute;N P&Uacute;BLICA Y GESTI&Oacute;N SOCIAL</h3>
+
+            <div class="embed-responsive" style="padding-top: 40%;">
+                <iframe id="uxVisor" src="./geovisor.combo.php" frameborder="0"></iframe>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script type='text/javascript'>
+$(document).ready(function() {
+    $('.section-sticky a').on('click', function() {
+        $('.section-sticky a').removeClass('selected');
+        $(this).addClass('selected');
+
+        let selector = $(this).data('target');
+        $('html, body').animate({
+            scrollTop: $(selector).offset().top - 200
+        }, 500)
+    });
+
+    $('#link-visor1, #link-visor2, #link-visor3').on('click', function() {
+        let selector = $(this).data('target');
+        let url = $(this).data('url');
+        $('#uxVisor').attr('src', url);
+
+        $('html, body').animate({
+            scrollTop: $(selector).offset().top - 200
+        }, 500)
+    });
+
+    let target = $.urlParam('target');
+    if (target)
+        $('#link-' + target).trigger('click');
+
+});
+</script>
