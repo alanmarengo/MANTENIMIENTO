00507f1553c82949e543d22a765837cd7cb78fa6
Martín Flores <martinflores@atic.com.ar>
2020-08-09 21:48:46 -0300

    slider participacion add tag

--- slider_participacion.php
+++ slider_participacion.php
@@ -6,7 +6,7 @@ $string_conn = "host=" . pg_server . " user=" . pg_user . " port=" . pg_portv . 
 
  $con = pg_connect($string_conn);
 
-	$SQL  = "SELECT r.recurso_path_url,f.desc,f.tema,r.recurso_id FROM  mod_mediateca.recurso r INNER JOIN mod_mediateca.fotos_participacion f ";
+	$SQL  = "SELECT r.recurso_path_url,f.desc,f.tema,r.recurso_id,upper(right(r.recurso_path_url,3))AS extension FROM  mod_mediateca.recurso r INNER JOIN mod_mediateca.fotos_participacion f ";
 	$SQL .= "ON f.recurso_id::bigint=r.recurso_id ";
 	$SQL .= "WHERE f.tema='Participación y gestión pública' ";
 	$SQL .= "ORDER BY f.tema ASC";
@@ -17,9 +17,11 @@ $string_conn = "host=" . pg_server . " user=" . pg_user . " port=" . pg_portv . 
 	{
 		   echo '{';
 		   echo '"media_url":"'.$row[0].'",';
-		   echo '"media_tipo":"jpg",';
+		   //echo '"media_tipo":"jpg",';
+		   echo '"media_tipo":"'.$row[4].'",';
 		   echo '"texto":"'.$row[1].'",';
 		   echo '"url":"#",';
+		   echo '"tag":"",';
 		   echo '"tema":"'.$row[2].'"';
 		   echo '}';
 		   
@@ -52,7 +54,7 @@ $string_conn = "host=" . pg_server . " user=" . pg_user . " port=" . pg_portv . 
 
 
 
-	$SQL  = "SELECT r.recurso_path_url,f.desc,f.tema,r.recurso_id FROM  mod_mediateca.recurso r INNER JOIN mod_mediateca.fotos_participacion f ";
+	$SQL  = "SELECT r.recurso_path_url,f.desc,f.tema,r.recurso_id,upper(right(r.recurso_path_url,3))AS extension FROM  mod_mediateca.recurso r INNER JOIN mod_mediateca.fotos_participacion f ";
 	$SQL .= "ON f.recurso_id::bigint=r.recurso_id ";
 	$SQL .= "WHERE f.tema='Pueblos Originarios' ";
 	$SQL .= "ORDER BY f.tema ASC";
