40d5b40c11c4686e5768ed69ba39233a36a58860
Martín Flores <martinflores@atic.com.ar>
2020-08-10 12:00:22 -0300

    play preview para audio y video

--- js/site.mediateca.js
+++ js/site.mediateca.js
@@ -239,8 +239,9 @@ $(document).ready(function() {
                      * */
                     
                     preview_media = '';
+                    console.log(model.ficha.categoria_media);
                     
-                    if (model.ficha.categoria_media in [2,3])//audio o video
+                    if (model.ficha.categoria_media in ['2','3'])//audio o video
                     {
 						preview_media = './images/play.png';
 					}
