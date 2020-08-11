956a41c38d8ea7aaeed61f84fd3ad08e3247be85
Juan Pablo Rosset <rosset.juanpablo@gmail.com>
2020-08-10 21:12:05 -0300

    visor com

--- php/get-popup-comunidad.php
+++ php/get-popup-comunidad.php
@@ -45,7 +45,7 @@ switch($table) {
 	case "socio.vp_geo_msrpo_reunionespueblosoriginarios_pga1":
 	
 	$title = "Pueblos Originarios";
-	$html = "<p style='color:#31cbfd; margin-top:20px; text-transform:uppercase;'>" . $title . "</p>";
+	$html = "<p style='color:#31cbfd; margin-top:20px; font-weight:700; text-transform:uppercase;'>" . $title . "</p>";
 	
 	while ($r = pg_fetch_assoc($query)) {
 		
