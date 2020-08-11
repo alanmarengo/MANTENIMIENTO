421421c57e4113b2ed5525446cb4a57e49bc573e
Juan Pablo Rosset <rosset.juanpablo@gmail.com>
2020-08-11 00:44:13 -0300

    com

--- js/map.comunidad.js
+++ js/map.comunidad.js
@@ -268,9 +268,9 @@ function ol_map() {
 				"EPSG:3857", "EPSG:3857"
 			);
 			
-			this.map.ol_object_mini.getView().fit(extent,{duration:1000});
-			this.map.ol_object_mini.updateSize();
-			this.map.ol_object_mini.render();
+			this.ol_object_mini.getView().fit(extent,{duration:1000});
+			this.ol_object_mini.updateSize();
+			this.ol_object_mini.render();
 			
 		}.bind(this));
 		
