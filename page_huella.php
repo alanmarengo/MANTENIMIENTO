96ea4f94919f5c3363ce793a7d0b8ffb4ceafc5f
Antoni Sanoguera <asanoguera@gmail.com>
2020-08-11 23:03:16 -0300

    Inicio maquetado huella

---
+++ page_huella.php
@@ -0,0 +1,40 @@
+<?php include("./fn.php"); ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	
+	<?php include("./scripts.analytics.php"); ?>
+
+	<title>Huellas del pasado</title>
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<?php include("./scripts.default.php"); ?>
+	<?php include("./scripts.onresize.php"); ?>		
+	
+	<?php include("./scripts.document_ready.php"); ?>
+	
+</head>
+<body>
+
+	<div id="page">
+	
+		<?php include("./html.navbar-main.php"); ?>
+		
+		<div class="page-container">
+		
+			<?php include("./section.page_huella.php"); ?>
+			
+			<?php include("./html.navs.php"); ?>
+			
+			<?php include("./footer.php"); ?>
+			
+		</div>
+		
+	</div>
+	
+	<?php include("./widget-links.php"); ?>	
+
+</body>
+</html>
---
+++ section.page_huella.php
@@ -0,0 +1,244 @@
+<link rel="stylesheet" type="text/css" href="./css/page_template.css" />
+
+<div id="page_huella" class="page page_template">
+    <div class="row">
+        <div class="section-sticky">
+            <div class="col-md-12 page-title">
+                Huellas del pasado
+            </div>
+
+            <div class="col-md-12 top-buttons">
+                <a id="link-areas" href="#" data-target="#areas">&Aacute;REAS DE IMPORTANCIA CIENT&Iacute;FICA Y REGISTROS VISUALES</a>
+                <a id="link-arqueologia" href="#" data-target="#arqueologia">ARQUEOLOGIA</a>
+                <a id="link-paleontologia" href="#" data-target="#paleontologia">PALEONTOLOGIA</a>
+                <a id="link-geologia" href="#" data-target="#geologia">GEOLOGIA</a>
+            </div>
+        </div>
+
+        <!---------------------------------------------->
+        <img src="./images/huellas.png" style="width: 100%;" />
+        <div class="col-md-12 section-a">
+            <div class="row">
+                <div class="col-md-6 text-justify">
+La cuenca del río Santa Cruz tiene una gran importancia para el concimiento del patrimonio natural y cultural. La región fue escenario de varias glaciaciones, habitad de abundante fauna y escenario de los primeros asentamientos del país. A su vez, en la zona de La Barrancosa tuvieron lugar hechos históricos de relevancia.
+<br />
+<br />
+                </div>
+                <div class="col-md-6 text-justify">
+En el marco del Programa de Gestión Ambiental se ejecutan programas que tienen como objetivos evitar la afectación del patrimonio en el entorno del proyecto, incluyendo protocolos de acción ante casos de hallazgos fortuitos. Se realizan relevamientos y rescates del material paleontológico, geológico y arqueológico, trabajando en este último caso en conjunto con las autoridades provinciales y las comunidades originarias en el tratamiento del material.                </div>
+<br />
+<br />
+            </div>
+        </div>
+
+        <!---------------------------------------------->
+        <div class="col-md-12 section-b">
+            <a id="areas"></a>
+            <h3 style="margin-bottom: 1em; color: #333;">&Aacute;REAS DE IMPORTANCIA CIENT&Iacute;FICA Y REGISTROS VISUALES</h3>
+
+            <div class="embed-responsive" style="padding-top: 40%;">
+                <iframe id="uxVisor" src="./geovisor.combo.php" frameborder="0"></iframe>
+            </div>
+        </div>
+
+<hr />
+<hr />
+<hr />
+<hr />
+<hr />
+
+        <!---------------------------------------------->
+        <div class="col-md-12 section-a">
+            <a id="pueblos"></a>
+            <h3 style="margin-bottom: 1em; color: #333;">PUEBLOS ORIGINARIOS</h3>
+
+            <div class="row">
+                <div class="col-md-6 text-justify">
+                    La Constitución Nacional reconoce la preexistencia étnica y cultural de los pueblos indígenas argentinos y reconoce la personería jurídica de las comunidades. A su vez, el Convenio 169 de la Organización Internacional del Trabajo sobre Pueblos Indígenas (Ley Nacional 24.017) contempla la consulta a los pueblos interesados cada vez que se prevén medidas legislativas o administrativas susceptibles de afectarles directamente.
+                    <br />
+                    <br />
+                    A partir de una sentencia judicial del 20 de octubre de 2017 se constituye la mesa de diálogo para hacer efectivo el proceso de consulta para los aprovechamientos hidroeléctricos del río Santa Cruz, en la que participan las 15 comunidades de pueblos originarios de la provincia, los Estados Nacional y Provincial, el Instituto Nacional de Asuntos Indígenas y la empresa contratista. 
+                    <br />
+                    <br />
+                </div>
+                <div class="col-md-6 text-justify">
+                    En ese marco, se realizan reuniones plenarias con diferente periodicidad, reuniones de las comunidades con sus asesores, se firmaron convenios de cooperación y se llevan adelante actividades conjuntas, en un proceso abierto que tiene como objetivo construir una mirada intercultural sobre el proyecto, la naturaleza y las ideas de desarrollo, y el reconocimiento de los derechos indígenas. 
+                    <br />
+                    <br />
+                    <a id="link-visor2" href="#" data-target="#eventos" data-url="#">Ver localización de las comunidades originarias.</a>
+                    <br />
+                    <a target="_blank" href="#">Acceda al listado de reuniones con las comunidades originarias.</a>
+                    <br />
+                </div>
+            </div>
+
+            <div id="slider-participacion2" class="carousel slide slider2" data-ride="carousel" data-interval="5000">
+                <div class="carousel-inner">
+                </div>
+                <div class="carousel-prev" href="#slider-participacion2" role="button" data-slide="prev">
+                    <i class="fa fa-chevron-left"></i>
+                </div>
+                <div class="carousel-next" href="#slider-participacion2" role="button" data-slide="next">
+                    <i class="fa fa-chevron-right"></i>
+                </div>
+            </div>
+
+        </div>
+
+        <!---------------------------------------------->
+        <div class="col-md-12 section-b">
+            <a id="articulaciones"></a>
+            <h3 style="margin-bottom: 1em; color: #333;">ARTICULACIONES INSTITUCIONALES</h3>
+
+            <div class="row">
+                <div class="col-md-6 text-justify">
+                    Dala la importancia, la escala y la complejidad del proyecto, los Aprovechamientos Hidroeléctricos del Río Santa Cruz se desarrollan en un contexto de articulación con diversas instituciones, empresas y actores a nivel local, provincial, nacional e internacional.
+                    <br />
+                    <br />
+                    Esta dimensión global del proyecto es propia de los emprendimientos de estas características, aunque suele quedar oculta por el hecho de que las obras se ejecutan en un espacio acotado del territorio. 
+                    <br />
+                    <br />
+                    En primer lugar, esta articulación se verifica en las competencias legales e institucionales más inmediatas: a) La Nación, a través de IEASA, como comitente del proyecto; b) La Provincia de Santa Cruz, como titular de los recursos naturales y de la gestión territorial y ambiental; c) La Inspección del proyecto; d) La Unión Transitoria de Empresas (UTE) Represas Patagonia, como contratista de las obras. 
+                    <br />
+                    <br />
+                    Las articulaciones que sustentan la ejecución del proyecto operan también en diversos niveles. Además de los indicados, podemos mencionar: 
+                    <br />
+                    <br />
+                </div>
+                <div class="col-md-6 text-justify">
+                    - La provisión de tecnología, conocimiento y experiencias de gestión de diversas industrias y expertos de todo el mundo; 
+                    <br />
+                    - La gestión de programas ambientales con la participación del sistema científico argentino y de expertos internacionales; 
+                    <br />
+                    - La provisión de insumos, materiales y tecnologías para la obra de distintos puntos del país, incluyendo la propia provincia de Santa Cruz;
+                    <br />
+                    - La interacción con organismos y agencias nacionales con competencia en el área de proyecto o en temas relacionados; 
+                    <br />
+                    - El otorgamiento de permisos para gestionar distintos aspectos de la obra o del Plan de Gestión Ambiental por parte de organismos nacionales y provinciales. 
+                    <br />
+                    - La articulación con agencias y organismos provinciales para el monitoreo de diversas variables sociales y ambientales. 
+                    <br />
+                    - La interacción –ya sea a nivel de espacios de diálogo o en ámbito judicial con actores u organizaciones que cuestionan el proyecto o alguno de sus aspectos. 
+                    <br />
+                    - La relación con instituciones encargadas de la gestión de la energía, el agua, la seguridad de presas y la gestión de riesgos. 
+                    <br />
+                    <br />
+                    <a id="link-visor3" href="#" data-target="#eventos" data-url="#">Ver la localización de las instituciones vinculadas al Proyecto.</a>
+                    <br />
+                    <a target="_blank" href="#">Acceda al tablero de Indicadores.</a>
+                    <br />
+                </div>
+            </div>
+        </div>
+
+        <!---------------------------------------------->
+        <div class="col-md-12 section-a" style="padding-bottom: 40px;">
+            <a id="eventos"></a>
+            <h3 style="margin-bottom: 1em; color: #333;">LOCALIZACI&Oacute;N DE EVENTOS DE PARTICIPACI&Oacute;N P&Uacute;BLICA Y GESTI&Oacute;N SOCIAL</h3>
+
+            <div class="embed-responsive" style="padding-top: 40%;">
+                <iframe id="uxVisor" src="./geovisor.combo.php" frameborder="0"></iframe>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="./js/slide.js"></script>
+<script type='text/javascript'>
+$(document).ready(function() {
+    var data1 = [
+        {
+            media_url: './images/index/s1.jpg',
+            media_tipo: 'jpg',
+            texto: 'Jornada informativa en Río Gallegos, previa a la Audiencia pública provincial. 16 de octubre de 2015',
+            url: '#',
+            tag: '',
+        },
+        {
+            media_url: 'https://mdbootstrap.com/img/video/Agua-natural.mp4',
+            media_tipo: 'mp4',
+            texto: 'VIDEO',
+            url: '#',
+            tag: '',
+        },
+        {
+            media_url: './images/index/s4.jpg',
+            media_tipo: 'jpg',
+            texto: 'ITEM 3',
+            url: '#',
+            tag: '',
+        },
+        {
+            media_url: './images/index/s5.jpg',
+            media_tipo: 'jpg',
+            texto: 'ITEM 4',
+            url: '#',
+            tag: '',
+        }
+    ];
+
+    var data2 = [
+        {
+            media_url: './images/index/s6.jpg',
+            media_tipo: 'jpg',
+            texto: 'Jornada informativa en Río Gallegos, previa a la Audiencia pública provincial. 16 de octubre de 2015',
+            url: '#',
+            tag: '',
+        },
+        {
+            media_url: './images/index/s1.jpg',
+            media_tipo: 'jpg',
+            texto: 'ITEM 2',
+            url: '#',
+            tag: '',
+        },
+        {
+            media_url: './images/index/s4.jpg',
+            media_tipo: 'jpg',
+            texto: 'ITEM 3',
+            url: '#',
+            tag: '',
+        },
+        {
+            media_url: './images/index/s3.jpg',
+            media_tipo: 'jpg',
+            texto: 'ITEM 4',
+            url: '#',
+            tag: '',
+        }
+    ];
+    //-----------------------------------------------
+    
+    <?php /*include("./slider_participacion.php");*/ ?>
+
+
+    $('.section-sticky a').on('click', function() {
+        $('.section-sticky a').removeClass('selected');
+        $(this).addClass('selected');
+
+        let selector = $(this).data('target');
+        $('html, body').animate({
+            scrollTop: $(selector).offset().top - 200
+        }, 500)
+    });
+
+    $('#link-visor1, #link-visor2, #link-visor3').on('click', function() {
+        let selector = $(this).data('target');
+        let url = $(this).data('url');
+        $('#uxVisor').attr('src', url);
+
+        $('html, body').animate({
+            scrollTop: $(selector).offset().top - 200
+        }, 500)
+    });
+
+    let target = $.urlParam('target');
+    if (target)
+        $('#link-' + target).trigger('click');
+
+    //------------------------------------------------------
+    slide('#slider-participacion1', data1, '');
+    slide('#slider-participacion2', data2, '');
+
+});
+</script>
