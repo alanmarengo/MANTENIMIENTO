a15664b1a66cccff2f6683581d5e2df46f67933a
Martín Flores <martinflores@atic.com.ar>
2020-08-17 11:57:59 -0300

    Agregar slider huellas, faltan categorias en los recursos

--- section.page_huella.php
+++ section.page_huella.php
@@ -207,7 +207,7 @@ $(document).ready(function() {
 
     //-----------------------------------------------
     
-    <?php /*include("./slider_participacion.php");*/ ?>
+    <?php /*include("./slider_huellas.php");*/ ?>
 
 
     $('.section-sticky a').on('click', function() {
