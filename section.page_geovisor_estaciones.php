2c86fee64562cfd43423e243bd5826402a7d2dd1
phortos <rosset.juanpablo@gmail.com>
2020-10-31 20:36:38 -0300

    autocommit

---
+++ section.page_geovisor_estaciones.php
@@ -0,0 +1,39 @@
+<link rel="stylesheet" type="text/css" href="./css/page_template.css" />
+
+<!--<div id="page_proyecto" class="page page_template h100p">-->
+    <div class="row h100p">
+                
+        <!---------------------------------------------->
+        <div class="col col-xs-12 col-sm-12 col-md-12 col-lg-12 section-b h100p p0">     
+		
+            <div style="border: solid 1px #666; margin: 0; height:100%; width: 100%;">
+				
+				<div class="col col-xs-12 col-sm-12 col-md-12 col-lg-12" style="text-align: left; padding: 0px; height:0px;">
+                    <select id="uxVisor" class="selectpicker" data-width="300">
+                       <!--<option value="">LA BARRANCOSA</option>-->
+                    </select>
+                    <div id="uxCapa" class="jump-scroll">
+                        <!--<option value="">ESCALA DE PECES</option>-->
+                    </div>
+                </div>
+
+                <div id="map">
+				
+				
+					
+                </div>
+
+            </div>
+			
+        </div>
+		
+    </div>
+<!--</div>-->
+
+<div style="width:200px; bottom:15px; left:10px; font-weight:bolder;" class="jump-posabs">
+			
+	<a href="./geovisor.php?geovisor=2" target="_blank" class="black-button">
+		<span>VER EN GEOVISOR</span>
+	</a>
+
+</div>
