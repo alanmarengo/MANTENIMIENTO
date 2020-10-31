65ddc6b625abf3e37889031b701a774578b92fa1
phortos <rosset.juanpablo@gmail.com>
2020-10-31 20:23:23 -0300

    autocommit

--- scripts.map.estaciones.php
+++ scripts.map.estaciones.php
@@ -1,6 +1,6 @@
 <!-- MAP -->
 
 <script src="./js/config.js" type="text/javascript"></script>
-<script src="./js/map.combo.js" type="text/javascript"></script>
+<script src="./js/map.estaciones.js" type="text/javascript"></script>
 <script src="./js/chart.js" type="text/javascript"></script>
 <script src="./js/sldlib.js" type="text/javascript"></script>
