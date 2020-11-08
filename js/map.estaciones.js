a8804dbd8f789ed664b132eb4b50da43279b20dc
phortos <rosset.juanpablo@gmail.com>
2020-11-08 17:28:55 -0300

    autocommit

--- js/map.estaciones.js
+++ js/map.estaciones.js
@@ -175,7 +175,7 @@ function ol_map() {
             var reqGFI = $.ajax({
 
                 async: false,
-                data: { url: newurl.encodeURI() },
+                data: { url: encodeURIComponent(newurl) },
                 type: "get",
                 url: "./red_gfi.php",
                 success: function(d) {}
