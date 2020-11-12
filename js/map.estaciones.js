d34409d5594502d6db4520fe56ed12e36f30a59c
phortos <rosset.juanpablo@gmail.com>
2020-11-12 17:05:40 -0300

    autocommit

--- js/map.estaciones.js
+++ js/map.estaciones.js
@@ -267,7 +267,6 @@ function ol_map() {
                 $("#tab-aforo-ha-1").trigger("click");
 
             } else {
-                alert(1);
 
                 $("#popup-aforo").hide();
                 $("#popup").show();
