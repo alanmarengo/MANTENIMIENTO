78f238201f19dcca9c5895f3c3a144b40eaf2592
phortos <rosset.juanpablo@gmail.com>
2020-11-12 23:48:06 -0300

    autocommit

---
+++ geovisor.redesbox.php
@@ -0,0 +1,32 @@
+<?php include("./fn.php"); ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	
+	<?php include("./scripts.analytics.php"); ?>
+
+	<style></style>
+
+	<title>PROYECTO</title>
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<?php include("./scripts.default.php"); ?>
+	<?php include("./scripts.openlayers.php"); ?>	
+	<?php include("./scripts.highcharts.php"); ?>	
+	<?php include("./scripts.map.estaciones.php"); ?>
+	<?php include("./scripts.onresize.php"); ?>
+
+	<link rel="stylesheet" href="./css/geovisorredesbox.css" type="text/css">
+	
+</head>
+<body style="overflow:hidden;">
+
+	<div class="container">
+		<p class="title">REGISTRO ESTADÍSTICO Y GRÁFICO DE LA RED DE MONITOREO HÍDRICO DE LA CUENCA DEL RÍO SANTA CRUZ</p>
+	</div>
+
+
+</body>
+</html>
