03f9d033357fd9094c860248db54825aa8e09958
phortos <rosset.juanpablo@gmail.com>
2020-11-12 21:11:46 -0300

    autocommit

--- js/map.estaciones.js
+++ js/map.estaciones.js
@@ -867,7 +867,7 @@ function ol_map() {
 
         this.popupTabAforo3 = function() {
 
-            let url = this.apiUrl + "?estacion_id=" + estacion_id + "&tipo_estacion_id=" + tipo_estacion_id + "&mode=0";
+            let url = this.apiUrl + "?estacion_id=" + estacion_id + "&mode=7";
             let js = this.requestApi(url);
 
         }
