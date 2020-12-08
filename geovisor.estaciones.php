d3bcf55137c9c054a4142f43f075b38e25ff9c0f
phortos <rosset.juanpablo@gmail.com>
2020-12-08 18:52:22 -0300

    autocommit

--- geovisor.estaciones.php
+++ geovisor.estaciones.php
@@ -207,30 +207,14 @@
 				<div id="panel-ha-4" class="tab-pane fade in">
 
 					<ul class="nav nav-tabs type2">
-						<li class="active"><a class="btn-tab active show" data-toggle="tab" id="tab-ha4-1" href="#panel-ha4-1" aria-expanded="true">VALORES MEDIOS MENSUALES</a></li>
-						<li class="m0"><a class="btn-tab" data-toggle="tab" id="tab-ha4-2" href="#panel-ha4-2" aria-expanded="true">VALORES MEDIOS ANUALES</a></li>
+						<li class="active"><a onclick="geomap.popupTab4();" class="btn-tab active show" data-toggle="tab" id="tab-ha4-1" href="#panel-ha4-1" aria-expanded="true">VALORES MEDIOS MENSUALES</a></li>
+						<li class="m0"><a onclick="geomap.popupTab4();" class="btn-tab" data-toggle="tab" id="tab-ha4-2" href="#panel-ha4-2" aria-expanded="true">VALORES MEDIOS ANUALES</a></li>
 					</ul>
 					
 					<div class="tab-content p20" id="popup-tab-content-ha4">
 						<div id="panel-ha4-1" class="tab-pane fade in active show">
-							<ul class="ico-list">
-								<?php for($i=0; $i<8; $i++) { ?>
-								<li>
-									<img src="./images/panel-h4-ico.png">
-									<span>MENSUAL ITEM: Lorem ipsum is simply dummy text of the printing any typesetting industry</span>
-								</li>
-								<?php } ?>
-							</ul>
 						</div>
 						<div id="panel-ha4-2" class="tab-pane fade in">
-							<ul class="ico-list">
-								<?php for($i=0; $i<8; $i++) { ?>
-								<li>
-									<img src="./images/panel-h4-ico.png">
-									<span>ANUAL ITEM: Lorem ipsum is simply dummy text of the printing any typesetting industry</span>
-								</li>
-								<?php } ?>
-							</ul>
 						</div>
 					</div>
 
