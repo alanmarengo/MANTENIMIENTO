e2c593285130eb64688bd07a6231c31faa22c581
phortos <rosset.juanpablo@gmail.com>
2020-12-08 18:59:48 -0300

    autocommit

--- js/map.estaciones.js
+++ js/map.estaciones.js
@@ -22,8 +22,8 @@ function ol_map() {
 
     let estacionLayersHtml = `
         <div class="tooltip-white-list">
-            <p class="text-center">CAPAS BASE</p>
-            <hr style="margin:7px 0;">
+            <p class="text-center m0">CAPAS BASE</p>
+            <hr style="margin:10px 0;">
             <ul>
                 <li><a href="javascrit:void(0);" data-state="0" onclick="geomap.map.setEstacionesLayer(0,this);" class="alphalink estacion-layer">CUENCAS PROVINCIA</a></li>
                 <li><a href="javascrit:void(0);" data-state="0" onclick="geomap.map.setEstacionesLayer(1,this);" class="alphalink estacion-layer">RÍOS PROVINCIA</a></li>
