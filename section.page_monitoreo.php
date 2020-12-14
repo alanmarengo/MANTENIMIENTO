6b4271c955d0defaa3e6842dc2af7f8f97fef80b
phortos <rosset.juanpablo@gmail.com>
2020-12-14 18:18:11 -0300

    autocommit

--- section.page_monitoreo.php
+++ section.page_monitoreo.php
@@ -125,7 +125,7 @@
 
         <!---------------------------------------------->
         <div class="col-md-12 section-b">
-            <!--<a id="registro"></a>-->
+            <a id="registro"></a>
             <!--<h3 style="margin-bottom: 1em; color: #333;">REGISTRO ESTAD&Iacute;STICO Y GR&Aacute;FICO DE LA RED DE MONITOREO H&Iacute;DRICO DE LA CUENCA DEL R&Iacute;O SANTA CRUZ</h3>-->
 			
 			<iframe id="box4" src="./geovisor.redesbox.php" frameborder="0" style="overflow:hidden;height:150vh;" height="100%" width="100%"></iframe>
