30ebb93eadde35dbf1ba5040c9601fde7f56118b
Martín Flores <martinflores@atic.com.ar>
2021-01-07 13:21:59 -0300

    cambio logo backend

--- backend/header.php
+++ backend/header.php
@@ -3,7 +3,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light nav-top">
         <a class="navbar-brand" href="#">
-            <img class="logo" src="./images/logo_observatorio_ieasa.png" />
+            <img class="logo" style="height:auto;" src="./images/logo_observatorio_ieasa.png" />
         </a>
 
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
