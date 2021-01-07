221719277558908af161a59feafdbc43a7cb471a
Juan Pablo Rosset <rosset.juanpablo@gmail.com>
2021-01-07 11:19:14 -0300

    autocommit

--- js/map.estaciones.js
+++ js/map.estaciones.js
@@ -393,7 +393,7 @@ function ol_map() {
 
         this.popupTab1 = function() {		
 			
-            
+            console.log(this.requestApi(url)[0]);
 
             let url = this.apiUrl + "?estacion_id=" + this.current_estacion_id + "&tipo_estacion_id=" + this.current_tipo_estacion_id + "&mode=0";
             let js = this.requestApi(url)[0];
