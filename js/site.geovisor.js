4ce7bfd4aeab1fe951f8c058a5a56f4d6a292802
Juan Pablo Rosset <rosset.juanpablo@gmail.com>
2021-01-07 09:20:09 -0300

    autocommit

--- js/site.geovisor.js
+++ js/site.geovisor.js
@@ -24,14 +24,15 @@ $(document).ready(function() {
 				
         }
     });
-	$(document)
-	  .ajaxStart(function () {
-		HoldOn.open({ theme: "sk-rect" });
-	  })
-	  .ajaxStop(function () {
-		HoldOn.close();
-	  });
 });
+	
+$(document)
+  .ajaxStart(function () {
+	HoldOn.open({ theme: "sk-rect" });
+  })
+  .ajaxStop(function () {
+	HoldOn.close();
+  });
 
 $.urlParam = function(name) {
     var results = new RegExp('[\?&]' + name + '=([^&#]*)')
