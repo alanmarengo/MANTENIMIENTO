1e0bcfca4fbfdf4d13e7b566af6cfd0d967e41ba
Juan Pablo Rosset <rosset.juanpablo@gmail.com>
2021-01-15 19:35:34 -0300

    tpls

--- template-ieasa-3.php
+++ template-ieasa-3.php
@@ -74,69 +74,29 @@
 	
 	<div class="row" >
 
-		<div class="resource-col col col-md-6 col-lg-6 col-xs-12 col-sm-12">
+		<div class="resource-col col col-md-12 col-lg-12 col-xs-12 col-sm-12">
 		
 			<div class="resource-inner" id="indicador-col-pos-8" data-pos="8">			
 			
 			</div>
 		
 		</div>
+		
+	</div>
+	
+	<div class="row">
 
-		<div class="resource-col col col-md-6 col-lg-6 col-xs-12 col-sm-12">
+		<div class="resource-col col col-md-6 col-lg-6 col-xs-12 col-sm-12">				
 		
-			<div class="row">
-			
-				<div class="resource-col col col-md-4 col-lg-4 col-xs-12 col-sm-12">				
-		
-					<div class="resource-inner" id="indicador-col-pos-9" data-pos="9">					
-					
-					</div>
-					
-				</div>
-			
-				<div class="resource-col col col-md-4 col-lg-4 col-xs-12 col-sm-12">				
-		
-					<div class="resource-inner" id="indicador-col-pos-10" data-pos="10">					
-					
-					</div>
-				
-				</div>
-			
-				<div class="resource-col col col-md-4 col-lg-4 col-xs-12 col-sm-12">				
-		
-					<div class="resource-inner" id="indicador-col-pos-11" data-pos="11">					
-					
-					</div>
-				
-				</div>
+			<div class="resource-inner" id="indicador-col-pos-9" data-pos="9">					
 			
 			</div>
-		
-			<div class="row">
 			
-				<div class="resource-col col col-md-4 col-lg-4 col-xs-12 col-sm-12">				
-		
-					<div class="resource-inner" id="indicador-col-pos-12" data-pos="12">					
-					
-					</div>
-					
-				</div>
-			
-				<div class="resource-col col col-md-4 col-lg-4 col-xs-12 col-sm-12">				
-		
-					<div class="resource-inner" id="indicador-col-pos-13" data-pos="13">					
-					
-					</div>
-				
-				</div>
-			
-				<div class="resource-col col col-md-4 col-lg-4 col-xs-12 col-sm-12">				
-		
-					<div class="resource-inner" id="indicador-col-pos-14" data-pos="14">					
-					
-					</div>
-				
-				</div>
+		</div>
+
+		<div class="resource-col col col-md-6 col-lg-6 col-xs-12 col-sm-12">				
+
+			<div class="resource-inner" id="indicador-col-pos-10" data-pos="10">					
 			
 			</div>
 		
--- template-ieasa-5.php
+++ template-ieasa-5.php
@@ -2,43 +2,56 @@
 
 	<div class="row" >
 
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
+		<div class="resource-col col col-md-4 col-lg-4 col-xs-12 col-sm-12">
 		
-			<div class="resource-inner" id="indicador-col-pos-1" data-pos="1">			
+			<div class="row">
+			
+				<div class="resource-col col col-md-12 col-lg-12 col-xs-12 col-sm-12">				
+		
+					<div class="resource-inner" id="indicador-col-pos-1" data-pos="1">					
+					
+					</div>
+					
+				</div>
+				
+			</div>
+			
+			<div class="row">
+			
+				<div class="resource-col col col-md-12 col-lg-12 col-xs-12 col-sm-12">				
+		
+					<div class="resource-inner" id="indicador-col-pos-2" data-pos="2">					
+					
+					</div>
+				
+				</div>
 			
 			</div>
-		
+			
 		</div>
 
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
+		<div class="resource-col col col-md-8 col-lg-8 col-xs-12 col-sm-12">					
 		
-			<div class="resource-inner" id="indicador-col-pos-2" data-pos="2">			
+			<div class="resource-inner" id="indicador-col-pos-3" data-pos="3">					
 			
 			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-3" data-pos="3">			
 			
-			</div>
-		
 		</div>
+			
+		
+	</div>
 
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
+	<div class="row" >
+
+		<div class="resource-col col col-md-4 col-lg-4 col-xs-12 col-sm-12">
 		
 			<div class="resource-inner" id="indicador-col-pos-4" data-pos="4">			
 			
 			</div>
 		
 		</div>
-		
-	</div>
 
-	<div class="row" >
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
+		<div class="resource-col col col-md-4 col-lg-4 col-xs-12 col-sm-12">
 		
 			<div class="resource-inner" id="indicador-col-pos-5" data-pos="5">			
 			
@@ -46,15 +59,19 @@
 		
 		</div>
 
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
+		<div class="resource-col col col-md-4 col-lg-4 col-xs-12 col-sm-12">
 		
 			<div class="resource-inner" id="indicador-col-pos-6" data-pos="6">			
 			
 			</div>
 		
 		</div>
+		
+	</div>
 
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
+	<div class="row" >
+
+		<div class="resource-col col col-md-6 col-lg-6 col-xs-12 col-sm-12">
 		
 			<div class="resource-inner" id="indicador-col-pos-7" data-pos="7">			
 			
@@ -62,477 +79,9 @@
 		
 		</div>
 
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
+		<div class="resource-col col col-md-6 col-lg-6 col-xs-12 col-sm-12">
 		
 			<div class="resource-inner" id="indicador-col-pos-8" data-pos="8">			
-			
-			</div>
-		
-		</div>
-		
-	</div>
-
-	<div class="row" >
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-9" data-pos="9">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-10" data-pos="10">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-11" data-pos="11">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-12" data-pos="12">			
-			
-			</div>
-		
-		</div>
-		
-	</div>
-
-	<div class="row" >
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-13" data-pos="13">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-14" data-pos="14">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-15" data-pos="15">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-16" data-pos="16">			
-			
-			</div>
-		
-		</div>
-		
-	</div>
-
-	<div class="row" >
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-17" data-pos="17">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-18" data-pos="18">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-19" data-pos="19">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-20" data-pos="20">			
-			
-			</div>
-		
-		</div>
-		
-	</div>
-
-	<div class="row" >
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-21" data-pos="21">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-22" data-pos="22">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-23" data-pos="23">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-24" data-pos="24">			
-			
-			</div>
-		
-		</div>
-		
-	</div>
-
-	<div class="row" >
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-25" data-pos="25">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-26" data-pos="26">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-27" data-pos="27">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-28" data-pos="28">			
-			
-			</div>
-		
-		</div>
-		
-	</div>
-
-	<div class="row" >
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-29" data-pos="29">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-30" data-pos="30">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-31" data-pos="31">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-32" data-pos="32">			
-			
-			</div>
-		
-		</div>
-		
-	</div>
-
-	<div class="row" >
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-33" data-pos="33">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-34" data-pos="34">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-35" data-pos="35">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-36" data-pos="36">			
-			
-			</div>
-		
-		</div>
-		
-	</div>
-
-	<div class="row" >
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-37" data-pos="37">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-38" data-pos="38">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-39" data-pos="39">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-40" data-pos="40">			
-			
-			</div>
-		
-		</div>
-		
-	</div>
-
-	<div class="row" >
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-41" data-pos="41">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-42" data-pos="42">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-43" data-pos="43">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-44" data-pos="44">			
-			
-			</div>
-		
-		</div>
-		
-	</div>
-
-	<div class="row" >
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-45" data-pos="45">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-46" data-pos="46">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-47" data-pos="47">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-48" data-pos="48">			
-			
-			</div>
-		
-		</div>
-		
-	</div>
-
-	<div class="row" >
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-49" data-pos="49">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-50" data-pos="50">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-51" data-pos="51">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-52" data-pos="52">			
-			
-			</div>
-		
-		</div>
-		
-	</div>
-
-	<div class="row" >
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-53" data-pos="53">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-54" data-pos="54">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-55" data-pos="55">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-56" data-pos="56">			
-			
-			</div>
-		
-		</div>
-		
-	</div>
-
-	<div class="row" >
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-57" data-pos="57">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-58" data-pos="58">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-59" data-pos="59">			
-			
-			</div>
-		
-		</div>
-
-		<div class="resource-col col col-md-3 col-lg-3 col-xs-12 col-sm-12">
-		
-			<div class="resource-inner" id="indicador-col-pos-60" data-pos="60">			
 			
 			</div>
 		
