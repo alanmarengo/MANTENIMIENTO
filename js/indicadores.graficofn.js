617366c631951c64f954d6c4c9ab639b4bdc2b5e
Juan Pablo <rosset.juanpablo@gmail.com>
2021-02-24 18:36:48 -0300

    autocommit

--- js/indicadores.graficofn.js
+++ js/indicadores.graficofn.js
@@ -1376,10 +1376,9 @@ function draw_grafico_17(container,config) {
 		subtitle: {
 			text: config.subtitle
 		},
-		xAxis: [{
-			categories: config.etiquetasUnique,
-			crosshair: true
-		}],
+		xAxis: {
+			categories: config.etiquetasUnique
+		},
 		yAxis:axis,
 		tooltip: {
 			shared: true
@@ -1401,7 +1400,109 @@ function draw_grafico_17(container,config) {
 }
 
 function draw_grafico_18(container,config) {
-	
+
+	console.log(config);
+
+	var axis = [];
+	var series = [];
+
+	for (var i=0; i<config.data.length; i++) {
+
+		var o = {
+			name: config.data[i].name,
+			type: config.data[i].type,
+			data: config.data[i].data,
+			tooltip:{
+				valueSuffix:config.data[i].unit
+			}
+		}
+
+		var y = { // Primary yAxis
+			labels: {
+				format: '{value}'+config.data[i].unit,
+				style: {
+					color: Highcharts.getOptions().colors[i]
+				}
+			},
+			title: {
+				text: config.data[i].name,
+				style: {
+					color: Highcharts.getOptions().colors[i]
+				}
+			}
+		}
+
+		series.push(o);
+		axis.push(y);
+
+	}
+
+	Highcharts.chart(container, {
+		title: {
+			text: 'Combination chart'
+		},
+		xAxis: {
+			categories: ['Apples', 'Oranges', 'Pears', 'Bananas', 'Plums']
+		},
+		labels: {
+			items: [{
+				html: 'Total fruit consumption',
+				style: {
+					left: '50px',
+					top: '18px',
+					color: ( // theme
+						Highcharts.defaultOptions.title.style &&
+						Highcharts.defaultOptions.title.style.color
+					) || 'black'
+				}
+			}]
+		},
+		series: [{
+			type: 'column',
+			name: 'Jane',
+			data: [3, 2, 1, 3, 4]
+		}, {
+			type: 'column',
+			name: 'John',
+			data: [2, 3, 5, 7, 6]
+		}, {
+			type: 'column',
+			name: 'Joe',
+			data: [4, 3, 3, 9, 0]
+		}, {
+			type: 'spline',
+			name: 'Average',
+			data: [3, 2.67, 3, 6.33, 3.33],
+			marker: {
+				lineWidth: 2,
+				lineColor: Highcharts.getOptions().colors[3],
+				fillColor: 'white'
+			}
+		}, {
+			type: 'pie',
+			name: 'Total consumption',
+			data: [{
+				name: 'Jane',
+				y: 13,
+				color: Highcharts.getOptions().colors[0] // Jane's color
+			}, {
+				name: 'John',
+				y: 23,
+				color: Highcharts.getOptions().colors[1] // John's color
+			}, {
+				name: 'Joe',
+				y: 19,
+				color: Highcharts.getOptions().colors[2] // Joe's color
+			}],
+			center: [100, 80],
+			size: 100,
+			showInLegend: false,
+			dataLabels: {
+				enabled: false
+			}
+		}]
+	});
+
 }
 
 function draw_grafico_19(container,config) {
