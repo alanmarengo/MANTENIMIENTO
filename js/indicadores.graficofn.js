c7ec9bf30acdb62fddfdc9640f9e755358216b4b
Juan Pablo <rosset.juanpablo@gmail.com>
2021-02-24 22:18:53 -0300

    autocommit

--- js/indicadores.graficofn.js
+++ js/indicadores.graficofn.js
@@ -1399,36 +1399,7 @@ function draw_grafico_16(container,config) { // MULTIPLE AXES
 				Highcharts.defaultOptions.legend.backgroundColor || // theme
 				'rgba(255,255,255,0.25)'
 		},
-		series: [{
-			name: 'Rainfall',
-			type: 'column',
-			yAxis: 1,
-			data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
-			tooltip: {
-				valueSuffix: ' mm'
-			}
-	
-		}, {
-			name: 'Sea-Level Pressure',
-			type: 'spline',
-			yAxis: 2,
-			data: [1016, 1016, 1015.9, 1015.5, 1012.3, 1009.5, 1009.6, 1010.2, 1013.1, 1016.9, 1018.2, 1016.7],
-			marker: {
-				enabled: false
-			},
-			dashStyle: 'shortdot',
-			tooltip: {
-				valueSuffix: ' mb'
-			}
-	
-		}, {
-			name: 'Temperature',
-			type: 'spline',
-			data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6],
-			tooltip: {
-				valueSuffix: ' °C'
-			}
-		}],
+		series: series,
 		responsive: {
 			rules: [{
 				condition: {
