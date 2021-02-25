ae9567e2278831c1e37d43393759d50ae3c36a57
Juan Pablo <rosset.juanpablo@gmail.com>
2021-02-24 21:00:40 -0300

    autocommit

--- js/indicadores.graficofn.js
+++ js/indicadores.graficofn.js
@@ -1514,28 +1514,28 @@ function draw_grafico_20(container,config) {
 		xAxis: {
 			gridLineWidth: 1,
 			title: {
-				text: 'Daily fat intake'
+				text: config.etiquetas.x_titulo
 			},
 			labels: {
-				format: '{value} gr'
+				format: '{value} ' + config.etiquetas.x_unidad
 			},
 			plotLines: [{
 				color: 'black',
 				dashStyle: 'dot',
 				width: 2,
-				value: 65,
+				value:config.etiquetas.x_valor_ref,
 				label: {
 					rotation: 0,
 					y: 15,
 					style: {
 						fontStyle: 'italic'
 					},
-					text: 'Safe fat intake 65g/day'
+					text: config.etiquetas.x_valor_ref_texto
 				},
 				zIndex: 3
 			}],
 			accessibility: {
-				rangeDescription: 'Range: 60 to 100 grams.'
+				rangeDescription: config.etiquetas.x_titulo_abcisa
 			}
 		},
 	
@@ -1543,32 +1543,32 @@ function draw_grafico_20(container,config) {
 			startOnTick: false,
 			endOnTick: false,
 			title: {
-				text: 'Daily sugar intake'
+				text: config.etiquetas.y_titulo
 			},
 			labels: {
-				format: '{value} gr'
+				format: '{value} ' + config.etiquetas.y_unidad
 			},
 			maxPadding: 0.2,
 			plotLines: [{
 				color: 'black',
 				dashStyle: 'dot',
 				width: 2,
-				value: 50,
+				value: config.etiquetas.y_valor_ref,
 				label: {
 					align: 'right',
 					style: {
 						fontStyle: 'italic'
 					},
-					text: 'Safe sugar intake 50g/day',
+					text: config.etiquetas.y_valor_ref_texto,
 					x: -10
 				},
 				zIndex: 3
 			}],
 			accessibility: {
-				rangeDescription: 'Range: 0 to 160 grams.'
+				rangeDescription: config.etiquetas.y_titulo_abcisa
 			}
 		},
-	
+		
 		tooltip: {
 			useHTML: true,
 			headerFormat: '<table>',
@@ -1590,7 +1590,23 @@ function draw_grafico_20(container,config) {
 		},
 	
 		series: [{
-			data: config.valor
+			data: [
+				{ x: 95, y: 95, z: 13.8, name: 'BE', country: 'Belgium' },
+				{ x: 86.5, y: 102.9, z: 14.7, name: 'DE', country: 'Germany' },
+				{ x: 80.8, y: 91.5, z: 15.8, name: 'FI', country: 'Finland' },
+				{ x: 80.4, y: 102.5, z: 12, name: 'NL', country: 'Netherlands' },
+				{ x: 80.3, y: 86.1, z: 11.8, name: 'SE', country: 'Sweden' },
+				{ x: 78.4, y: 70.1, z: 16.6, name: 'ES', country: 'Spain' },
+				{ x: 74.2, y: 68.5, z: 14.5, name: 'FR', country: 'France' },
+				{ x: 73.5, y: 83.1, z: 10, name: 'NO', country: 'Norway' },
+				{ x: 71, y: 93.2, z: 24.7, name: 'UK', country: 'United Kingdom' },
+				{ x: 69.2, y: 57.6, z: 10.4, name: 'IT', country: 'Italy' },
+				{ x: 68.6, y: 20, z: 16, name: 'RU', country: 'Russia' },
+				{ x: 65.5, y: 126.4, z: 35.3, name: 'US', country: 'United States' },
+				{ x: 65.4, y: 50.8, z: 28.5, name: 'HU', country: 'Hungary' },
+				{ x: 63.4, y: 51.8, z: 15.4, name: 'PT', country: 'Portugal' },
+				{ x: 64, y: 82.9, z: 31.3, name: 'NZ', country: 'New Zealand' }
+			]
 		}]
 	
 	});
