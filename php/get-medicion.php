104960951c3965a544476937c75cda31eeceeede
Martín Flores <martinflores@atic.com.ar>
2021-03-09 11:25:44 -0300

    Medición de distancia a geograficas

--- php/get-medicion.php
+++ php/get-medicion.php
@@ -12,6 +12,7 @@ $conn = pg_connect($string_conn);
 if ($type == "LineString") {
 
 	$query_string = "SELECT ST_Length(ST_GeomFromText('".$wkt."')::geography,true) / 1000 AS km;";
+	echo $query_string;
 
 }else{
 	
