d6ac51cee413f9f6e36c8d49a8875ad810158185
Martín Flores <martinflores@atic.com.ar>
2021-03-10 13:44:34 -0300

    templates 8 y 9

---
+++ template-ieasa-8.php
@@ -0,0 +1,69 @@
+<div class="template-indicador-container" id="template-indicadores-ieasa-8">
+
+	<div class="row" >
+
+		<div class="resource-col col-12 col-md-12 col-lg-12 col-xs-12 col-sm-12">
+		
+			<div class="resource-inner" id="indicador-col-pos-1" data-pos="1">			
+
+			</div>
+		
+		</div>
+        
+
+		<div class="resource-col col-12 col-md-4 col-lg-4 col-xs-12 col-sm-12">
+		
+			<div class="resource-inner" id="indicador-col-pos-2" data-pos="2">			
+
+			</div>
+		
+		</div>
+        <div class="resource-col col-12 col-md-4 col-lg-4 col-xs-12 col-sm-12">
+
+            <div class="resource-inner" id="indicador-col-pos-3" data-pos="3">			
+
+            </div>
+    
+        </div>
+        <div class="resource-col col-12 col-md-4 col-lg-4 col-xs-12 col-sm-12">
+            
+            <div class="resource-inner" id="indicador-col-pos-4" data-pos="4">			
+
+            </div>
+        
+        </div>
+
+        <div class="resource-col col-12 col-md-6 col-lg-6 col-xs-12 col-sm-12">
+            
+            <div class="resource-inner" id="indicador-col-pos-5" data-pos="5">			
+
+            </div>
+        
+        </div>
+        <div class="resource-col col-12 col-md-6 col-lg-6 col-xs-12 col-sm-12">
+            
+            <div class="resource-inner" id="indicador-col-pos-6" data-pos="6">			
+
+            </div>
+        
+        </div>
+
+        <div class="resource-col col-12 col-md-6 col-lg-6 col-xs-12 col-sm-12">
+            
+            <div class="resource-inner" id="indicador-col-pos-7" data-pos="7">			
+
+            </div>
+        
+        </div>
+        <div class="resource-col col-12 col-md-6 col-lg-6 col-xs-12 col-sm-12 ">
+            
+            <div class="resource-inner" id="indicador-col-pos-8" data-pos="8">			
+
+            </div>
+        
+        </div>
+
+
+
+    </div>
+</div>
