bc435c17ccdfeb51d01467f7c1a9f4139b6e0667
Luciano Ghelfi <nanoghelfi2@gmail.com>
2021-03-29 12:56:02 -0300

    fix popup de carga monitoreo

--- page_monitoreo.php
+++ page_monitoreo.php
@@ -1,7 +1,15 @@
 <?php include("./fn.php"); ?>
 <!DOCTYPE html>
 <html lang="es">
-<head>
+
+<!-- Script del modal  -->
+<script type="text/javascript">
+	function cerrarPopup(){
+			$('.modalMonitoreo').remove()
+	}
+</script>
+
+<head></head>
 
 	<link href="./css/popupMonitoreo.css" rel="stylesheet" type="text/css">
 	<?php include("./scripts.analytics.php"); ?>
@@ -19,7 +27,21 @@
 <body>
 
 	<div class="p">
-		<div class="modalMonitoreo"> </div>
+		<div class="modalMonitoreo">
+		<div id="basePopup"> 
+			<div id="contenedorPopup"> 
+				<div id="tituloPopup"> <h5> Atención </h5></div>
+				<div id="contenidoPopup"> 
+					Se recuerda a los potenciales usuarios de esta información que en todo momento se debe tener cuidado con las conclusiones que puedan derivarse del análisis de las series de datos hidroambientales pues son el resultado de una reconstrucción y por ende no son mediciones reales en el sentido estrictamente académico. <b>Los datos de los últimos dos meses de estas estaciones son datos en tiempo real y pueden mostrar resultados afectados por problemas técnicos.</b>
+				</div>
+				<div id="footerPopup">
+					<button type="button" class="btn btn-primary" id="botonPopup" onclick="cerrarPopup()">
+						Aceptar
+					</button>
+				</div>
+			</div>
+		</div>
+		 </div>
 		<?php include("./html.navbar-main.php"); ?>
 		<div class="page-container">
 		
@@ -37,32 +59,10 @@
 	<?php include("./widget-links.php"); ?>	
 
 </body>
+<script type="text/javascript"> 
 
-<!-- Script del modal  -->
-<script type="text/javascript">
-    $(window).on('load', function() {		
-		let html = `
-		<div id="basePopup"> 
-			<div id="contenedorPopup"> 
-				<div id="tituloPopup"> <h5> Atención </h5></div>
-				<div id="contenidoPopup"> 
-					Se recuerda a los potenciales usuarios de esta información que en todo momento se debe tener cuidado con las conclusiones que puedan derivarse del análisis de las series de datos hidroambientales pues son el resultado de una reconstrucción y por ende no son mediciones reales en el sentido estrictamente académico. <b>Los datos de los últimos dos meses de estas estaciones son datos en tiempo real y pueden mostrar resultados afectados por problemas técnicos.</b>
-				</div>
-				<div id="footerPopup">
-					<button type="button" class="btn btn-primary" id="botonPopup">
-						Aceptar
-					</button>
-				</div>
-			</div>
-		</div>
-		`
-		$('.modalMonitoreo').html(html)
-
-		$('#botonPopup').on('click', function(){
-			$('.modalMonitoreo').remove()
-		})
-    });
 </script>
+
 
 </html>
 
